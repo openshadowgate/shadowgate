37330437be236df8eca4ac841f482f714a0d0ae6
shadowgate <law@shadowgate.org>
2019-07-14 00:17:01 +0000

    autocommit 201907141700

--- d/magic/temples/shaundakul.c
+++ d/magic/temples/shaundakul.c
@@ -1,5 +1,6 @@
+//updated by ~Circe~ 7/13/19 following the god changes
 #include <std.h>
-inherit "/std/temple";
+inherit ROOM;
 
 void create(){
    ::create();
@@ -7,46 +8,41 @@ void create(){
    set_travel(DIRT_ROAD);
    set_light(2);
    set_indoors(0);
-   set_temple("shaundakul");
+//   set_temple("shaundakul");
    set_smell("default","The crisp scents of the forest is carried in on the winds.");
    set_listen("default","The wind whistles through the stone pillars.");
-   set_short("%^MAGENTA%^Temple of Shaundakul%^RESET%^");
-   set_long("%^MAGENTA%^The Temple of Shaundakul\n%^RESET%^"+
-      "%^BOLD%^%^WHITE%^Nestled in a small grove lays the ancient open air temple of"+
-      " %^RESET%^%^MAGENTA%^Shaundakul%^BOLD%^%^WHITE%^.  Surrounded by a ring of dense"+
-      " trees, the grassy hill that makes up the temple rises upwards to the sky.  "+
-      "On top of the hill a weathered and ancient %^BOLD%^%^BLACK%^granite throne"+
-      "%^BOLD%^%^WHITE%^ rests.  The high back massive throne has been carved with an"+
-      " intricate wind like %^RESET%^%^CYAN%^r%^MAGENTA%^i%^BOLD%^%^BLACK%^p%^RESET%^"+
-      "%^CYAN%^p%^MAGENTA%^l%^BOLD%^%^BLACK%^e%^RESET%^%^CYAN%^s%^RESET%^%^BOLD%^%^WHITE%^"+
-      " that radiates outwards from an image of an upright left hand.  Before the "+
-      "throne a %^RESET%^weathered%^BOLD%^%^WHITE%^ dais has been constructed out of "+
-      "granite slabs.  Carved deep into the stone is the charge of Shaundakul for all "+
-      "to read.  Three pillars of %^RESET%^s%^BOLD%^%^BLACK%^t%^RESET%^o%^BOLD%^%^BLACK%^"+
-      "n%^RESET%^e%^BOLD%^%^WHITE%^ rise upwards to the sky.  Each pillar is pierced with"+
-      " holes which allows the wind to whistle through.%^RESET%^"
+   set_short("%^MAGENTA%^An abandoned temple%^RESET%^");
+   set_long("%^BOLD%^%^WHITE%^Nestled in a small grove lays an ancient open air temple in ruins.  "
+      "Surrounded by a ring of dense %^GREEN%^trees%^WHITE%^, a grassy hill rises "
+      "upwards to the %^CYAN%^sky%^WHITE%^.  "
+      "On top of the hill, a weathered and ancient %^BOLD%^%^BLACK%^granite throne"
+      "%^BOLD%^%^WHITE%^ rests.  The high back massive throne has been carved with"
+      " intricate wind-like %^RESET%^%^CYAN%^r%^MAGENTA%^i%^BOLD%^%^BLACK%^p%^RESET%^"
+      "%^CYAN%^p%^MAGENTA%^l%^BOLD%^%^BLACK%^e%^RESET%^%^CYAN%^s%^RESET%^%^BOLD%^%^WHITE%^"
+      " that radiate outwards from an unrecognizable image.  Before the "
+      "throne is a ring of concave earth where it seems something once stood.  "
+      "Three crumbling pillars of %^RESET%^s%^BOLD%^%^BLACK%^t%^RESET%^o%^BOLD%^%^BLACK%^"
+      "n%^RESET%^e%^BOLD%^%^WHITE%^ rise upwards to the sky.  Each pillar is pierced with"
+      " holes which allows the wind to whistle through.\n%^RESET%^"
    );
    set_exits(([
       "out":"/d/shadow/room/kildare/rooms/roads/eroad06"
    ]));
    set_items(([
-     "dais":"%^CYAN%^The weathered dais is flecked with moss.  Carved "+
-		"into the surface of the slab is the charge of Shaundakul.%^RESET%^",
-      "charge":"%^MAGENTA%^Maybe you should read it.%^RESET%^",
-      "throne":"%^RESET%^The tall and ancient throne is common for each "+
-		"shrine or temple devoted to Shaundakul.  The throne is built "+
-		"at each temple so that as Shaundakul travels the lands he will"+
-		" always find a seat to rest on.  The faithful typically cover the"+
-		" seat of the throne with offerings to The Helping Hand.%^RESET%^",
-	"pillars":"%^BOLD%^%^BLACK%^Three weather beaten pillars rise up to the sky."+
-		"  Each pillar is pierced with numerous holes from various angles to "+
-		"allow the wind to freely blow through creating a variety of whistles.",
-	"trees":"%^GREEN%^A dense forest surrounds three quarters of the temple, "+
-		"giving it a feeling of privacy.  At one time it looks as if the temple"+
+     ({"ring","earth","concave earth","concave ring"}) : "A slightly irregular dip in "
+        "the ground indicates that something large once dominated the top of the hill, "
+        "but all that reminds are slabs of granite strewn here and there.",
+     "throne":"%^RESET%^The tall and ancient throne stands stoically against the sky.  "
+        "Whatever purpose it might have had, those secrets have been lost to the wind.",
+	"pillars":"%^BOLD%^%^BLACK%^Three weather-beaten pillars rise up to the sky."
+		"  Each pillar is pierced with numerous holes from various angles to "
+		"allow the wind to freely blow through, creating a variety of whistles.",
+	"trees":"%^GREEN%^A dense forest surrounds three-quarters of the abandoned temple, "
+		"giving it a feeling of privacy.  At one time it looks as if the temple"
 		" might have been more accessible to travelers.",
    ]));
 }
-
+/*
 void init(){
    ::init();
    add_action("read_charge","read");
@@ -81,3 +77,4 @@ EXTRA
    );
    return 1;
 }
+*/
