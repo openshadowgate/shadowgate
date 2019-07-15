e7aac232556c2e0b6879bcce4d3db7d77e21e12b
shadowgate <law@shadowgate.org>
2019-07-15 01:17:01 +0000

    autocommit 201907151701

--- d/magic/temples/nilith/nilith.c
+++ d/magic/temples/nilith/nilith.c
@@ -1,3 +1,4 @@
+//added items to look at ~Circe~ 7/14/19
 #include <std.h>
 inherit "/std/temple";
 
@@ -11,33 +12,77 @@ void create() {
    set_name("%^ORANGE%^Temple of Nilith%^WHITE%^");
    set_short("%^ORANGE%^Temple of Nilith%^WHITE%^");
    set_long("%^ORANGE%^Temple of Nilith%^WHITE%^\n"
-   "%^BOLD%^%^WHITE%^Towering vaulted ceilings adorned with murals draw your eyes "+
-   "immediately upon entry, each arch of the ceiling a flying buttress of "+
-   "%^BOLD%^%^BLACK%^b%^RESET%^l%^BOLD%^%^BLACK%^a%^RESET%^c%^BOLD%^%^BLACK%^k "+
-   "%^BOLD%^%^WHITE%^%^marble and inlaid with perfectly identical %^MAGENTA%^s%^BLUE%^p"+
-   "%^CYAN%^i%^GREEN%^r%^ORANGE%^a%^RED%^l%^BOLD%^%^BLACK%^s %^BOLD%^%^WHITE%^%^of "+
-   "silver and %^RED%^g%^BOLD%^e%^BOLD%^%^WHITE%^%^m%^BOLD%^s%^RESET%^%^GREEN%^t%^BOLD%^o"+
-   "%^RESET%^%^BLUE%^n%^MAGENTA%^e%^BOLD%^s%^BOLD%^%^WHITE%^%^. Each mural shows a step "+
-   "within the evolution of undead, with a single central mural showing a archlich, with its "+
-   "parchment white skin and nigh bloodless expression placid. As you return your eyes to ground "+
-   "level, mentally boggling over the upper arrangement, you are instantly brought into shock at "+
-   "the lovely %^BOLD%^%^BLACK%^g%^WHITE%^r%^RESET%^a%^BOLD%^%^BLACK%^y %^BOLD%^%^WHITE%^%^and "+
-   "%^BOLD%^%^BLACK%^black %^BOLD%^%^WHITE%^%^inlays of the floor, geometric and gem-riddled, the "+
-   "pattern is intense and mesmerizing. Pews have been laid out on both sides of a central isle, "+
-   "thirty in all, some having undead and living worshipers seated silently. At the apex of the "+
-   "central isle is a sarcophagus. This incredible and immense detail, this stone burial dressing "+
-   "possesses an image of a %^BOLD%^%^BLACK%^black-skinned %^BOLD%^%^WHITE%^%^and %^BOLD%^%^WHITE%^"+
-   "white-haired %^BOLD%^%^WHITE%^%^woman of incredible beauty embossed upon its lid. Precious "+
-   "stones are set in the base of the sarcophagus, and piles of %^BOLD%^c%^BOLD%^%^WHITE%^%^o"+
-   "%^BOLD%^i%^BOLD%^%^WHITE%^%^n%^BOLD%^s %^BOLD%^%^WHITE%^%^are scattered at it's feet. There are "+
-   "goblets of fine brass and plates of complex potions of unknown use. Behind the altar on the wall "+
-   "is is a shield inscribed with a skeletal hand holding a calla lily. On an elegant, %^WHITE%^s"+
-   "%^BOLD%^i%^RESET%^l%^BOLD%^v%^RESET%^e%^BOLD%^r %^BOLD%^%^WHITE%^%^scroll is engraved the charge "+
-   "of Nilith.%^WHITE%^\n");
-   set_smell("default","The scent of blood and decay assaults your senses");
-   set_listen("default","You hear the faint sounds of distant battles");
+      "%^RESET%^%^BLUE%^Towering vaulted ceilings adorned with murals draw your eyes "+
+      "immediately upon entry, each arch of the ceiling a flying buttress of "+
+      "%^BOLD%^%^BLACK%^b%^RESET%^l%^BOLD%^%^BLACK%^a%^RESET%^c%^BOLD%^%^BLACK%^k "+
+      "m%^RESET%^a%^BOLD%^%^BLACK%^r%^RESET%^b%^BOLD%^%^BLACK%^l%^RESET%^e %^BLUE%^"
+      "and inlaid with perfectly identical %^RESET%^s%^RED%^p%^BOLD%^i%^BLACK%^r%^RED%^a"
+      "%^RESET%^%^RED%^l%^RESET%^s %^BLUE%^of %^RESET%^s%^BOLD%^i%^BLACK%^l%^WHITE%^v%^RESET%^er "
+      "%^BLUE%^and %^RED%^g%^BOLD%^e%^BOLD%^%^BLACK%^%^m%^RESET%^%^RED%^s%^BOLD%^t%^BLACK%^o"+
+      "%^RESET%^%^RED%^n%^BOLD%^e%^BLACK%^s%^RESET%^%^BLUE%^. Each %^RED%^mural %^BLUE%^shows a step "+
+      "within the evolution of %^BOLD%^%^BLACK%^undead%^RESET%^%^BLUE%^, with a single "
+      "central mural showing an %^RESET%^archlich%^BLUE%^ with its "+
+      "parchment white skin and nigh bloodless expression placid. The floor is covered in "
+      "%^BOLD%^%^BLACK%^g%^WHITE%^r%^RESET%^a%^BOLD%^%^BLACK%^y %^RESET%^%^BLUE%^and "+
+      "%^BOLD%^%^BLACK%^black %^RESET%^%^BLUE%^inlays, geometric and gem-riddled, in an "
+      "intense, mesmerizing pattern. Pews have been laid out on both sides of a central aisle, "+
+      "thirty in all, some having undead and living worshippers seated silently. At the apex of the "+
+      "central aisle is a %^RESET%^sarcophagus%^BLUE%^. The stone burial dressing "+
+      "possesses an image of a %^BOLD%^%^BLACK%^black-skinned %^RESET%^%^BLUE%^and %^BOLD%^%^WHITE%^"+
+      "white-haired %^RESET%^%^BLUE%^woman of incredible beauty embossed upon its lid. Precious "+
+      "%^RED%^s%^BOLD%^t%^BLACK%^on%^RED%^e%^RESET%^%^RED%^s %^BLUE%^are set in the base "
+      "of the sarcophagus, and piles of %^ORANGE%^c%^YELLOW%^o%^WHITE%^"
+      "i%^YELLOW%^n%^RESET%^%^ORANGE%^s %^BLUE%^are scattered at its feet. There are "+
+      "goblets of fine brass and plates of complex potions of unknown use. Behind the altar on the wall "+
+      "is a %^BOLD%^%^WHITE%^shield %^RESET%^%^BLUE%^inscribed with a %^RESET%^skeletal hand "
+      "%^BLUE%^holding a %^BOLD%^%^WHITE%^calla lily%^RESET%^%^BLUE%^. On an elegant %^WHITE%^s"+
+      "%^BOLD%^i%^RESET%^l%^BOLD%^v%^RESET%^e%^BOLD%^r %^BOLD%^%^WHITE%^%^scroll %^RESET%^%^BLUE%^is engraved the charge "+
+      "of Nilith.%^WHITE%^\n");
+   set_smell("default","The scent of blood and decay assaults your senses.");
+   set_listen("default","You hear the faint sounds of distant battles.");
+   set_items(([
+      ({"ceiling","marble","black marble","buttresses","buttress"}) : "The arched buttresses "
+         "carved from %^BOLD%^%^BLACK%^b%^RESET%^l%^BOLD%^%^BLACK%^a%^RESET%^c%^BOLD%^%^BLACK%^k "
+         "m%^RESET%^a%^BOLD%^%^BLACK%^r%^RESET%^b%^BOLD%^%^BLACK%^l%^RESET%^e tower high overhead, " 
+         "adding to the awed hush of the temple. Decorative %^RESET%^s%^RED%^p%^BOLD%^i%^BLACK%^r%^RED%^a"
+         "%^RESET%^%^RED%^l%^RESET%^s of %^RESET%^s%^BOLD%^i%^BLACK%^l%^WHITE%^v%^RESET%^er "
+         "and %^RED%^g%^BOLD%^e%^BOLD%^%^BLACK%^%^m%^RESET%^%^RED%^s%^BOLD%^t%^BLACK%^o"
+         "%^RESET%^%^RED%^n%^BOLD%^e%^BLACK%^s %^RESET%^provide artistic relief to the otherwise somber marble.",
+      ({"mural","murals"}) : "The murals are works of macabre art. Each painting "
+         "depicts a single stage of the transformation from life to undeath, and "
+         "the skeletal, sometimes bloody images are presented in painstaking detail. "
+         "While the murals to either side show lesser undead, the pride of the "
+         "masterpiece is clearly the central panel. Here, an archlich reigns in all "
+         "her glory, with her bone white skin and burning eyes offset by the pitch "
+         "black robe she wears and the ebony circlet atop her skull.",
+      "floor" : "%^BOLD%^%^BLACK%^A master craftsman must have spent quite some "
+         "time on the floor, which is smooth and perfect despite the countless inlaid "
+         "designs. The %^RESET%^%^RED%^g%^BOLD%^e%^BLACK%^m%^RESET%^%^RED%^s %^BOLD%^%^BLACK%^"
+         "have been polished until they glitter like the burning red eyes of a lich in the "
+         "cautionary tales told to children.%^RESET%^",
+      ({"pew","pews","worshipper","worshippers"}) : "%^BOLD%^%^WHITE%^Bone white pews "
+         "line both sides of the central aisle, curving to create a pattern reminiscent "
+         "of a rib cage. Worshippers, both living and undead, can be found scattered "
+         "among the pews at all hours, though most keep to themselves.%^RESET%^",
+      ({"sarcophagus","woman","lid","sarcophagus lid"}) : "This stone burial dressing "
+         "has been carved in the likeness of a woman and painted to bring her to "
+         "breathtaking un-life. Her %^BOLD%^white hair %^RESET%^contrasts her "
+         "%^BOLD%^%^BLACK%^dusky skin%^RESET%^, and the artist has captured her "
+         "unearthly beauty. The base of the sarcophagus is set with %^RED%^g%^BOLD%^e"
+         "%^BOLD%^%^BLACK%^%^m%^RESET%^%^RED%^s%^BOLD%^t%^BLACK%^o%^RESET%^%^RED%^n"
+         "%^BOLD%^e%^BLACK%^s%^RESET%^, and petitioners have left piles of "
+         "%^ORANGE%^c%^YELLOW%^o%^WHITE%^i%^YELLOW%^n%^RESET%^%^ORANGE%^s %^RESET%^at her feet.",
+      ({"shield","hand","skeletal hand","calla lily"}) : "A %^WHITE%^steel tower shield "
+         "%^RESET%^on the wall bears the holy symbol of Nilith: a %^BOLD%^skeletal hand "
+         "%^RESET%^holding a single w%^BOLD%^i%^RESET%^l%^BOLD%^t%^RESET%^e%^BOLD%^d "
+         "%^RESET%^c%^BOLD%^a%^BLACK%^l%^WHITE%^l%^RESET%^a %^BOLD%^l%^RESET%^i%^BOLD%^%^BLACK%^l%^RESET%^y.",
+      ({"charge","scroll","silver scroll"}) : "A thin sheet of %^BOLD%^s%^RESET%^i"
+         "%^BOLD%^l%^RESET%^v%^BOLD%^e%^RESET%^r fashioned into a %^BOLD%^%^BLACK%^scroll "
+         "%^RESET%^bears the charge of %^BOLD%^%^BLACK%^Nilith %^RESET%^to her followers. "
+         "Perhaps you should read it."
+   ]));
    set_exits(([
-  "out" : "/d/dagger/road/p8"
+      "out" : "/d/dagger/road/p8"
    ]));
 }
 
@@ -50,7 +95,7 @@ int read_charge(string str){
    if(!str || str != "charge") return __Read_me(str);
    write(
 @CHARGE
-%^ORANGE%^Seek immortal undeath at all costs. Mortals are born to serve the unliving gods of this realm, and to the elite, become them. Trample all who stand in your way. Ravage the lands and take all you desire. Avoid the path of light, for the eternal shadow of undeath is your only prize, and seek to undo the lives of others upon the battlefield, every fallen foe is a step on the path to your eternal stride in the world of undeath.%^RESET%^
+%^ORANGE%^Seek immortal undeath at all costs. Mortals are born to serve the unliving gods of this realm, and the elite are destined to become them. Trample all who stand in your way. Ravage the lands and take all you desire. Avoid the path of light, for the eternal shadow of undeath is your only prize. Seek to undo the lives of others upon the battlefield, for every fallen foe is a step on the path to your eternal stride in the world of undeath.%^RESET%^
 
 CHARGE
    );
