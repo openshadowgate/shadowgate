81af9b68aa3f332a78dfab425315d1de5056bc5c
shadowgate <law@shadowgate.org>
2019-07-17 19:17:01 +0000

    autocommit 201907171719

--- d/magic/obj/window_viewer.c
+++ d/magic/obj/window_viewer.c
@@ -3,38 +3,39 @@ inherit "/d/magic/obj/shadowlistener.c";
 string room_filename;
 string win_location;
 
-
 void create(){
-  ::create();
-  set_id(({"window_viewer"}));
-  set_name("window viewer");
+    ::create();
+    set_id(({"window_viewer"}));
+    set_name("window viewer");
 }
 
 void heart_beat()
 {
-   if(!objectp(TO)) { return; }
+    if(!objectp(TO)) { return; }
 }
 
-catch_tell(string str){
-   if(!objectp(caster)){
-      dest_me();
-      return;
-   }
-   tell_object(caster,"%^CYAN%^Through the window you see:%^RESET%^   "+str);
+catch_tell(string str)
+{
+    if(!objectp(caster)){
+        dest_me();
+        return;
+    }
+    tell_object(caster,"%^CYAN%^Through the window you see:%^RESET%^   "+str);
 }
 
 string query_room_filename(){
-  return room_filename;
+    return room_filename;
 }
 
 void set_room_filename(string str){
-  room_filename = str;
+    room_filename = str;
 }
 
 string query_win_location(){
-  return win_location;
+    return win_location;
 }
 
 void set_win_location(string str){
-  win_location = str;
+    win_location = str;
 }
+
