46a99b12269608a25195676de66c262f23dfb647
shadowgate <law@shadowgate.org>
2019-07-17 18:17:01 +0000

    autocommit 201907171718

--- d/magic/obj/window_viewer.c
+++ d/magic/obj/window_viewer.c
@@ -1,5 +1,14 @@
 #include <std.h>
 inherit "/d/magic/obj/shadowlistener.c";
+string room_filename;
+string win_location;
+
+
+void create(){
+  ::create();
+  set_id(({"window_viewer"}));
+  set_name("window viewer");
+}
 
 void heart_beat()
 {
@@ -14,3 +23,18 @@ catch_tell(string str){
    tell_object(caster,"%^CYAN%^Through the window you see:%^RESET%^   "+str);
 }
 
+string query_room_filename(){
+  return room_filename;
+}
+
+void set_room_filename(string str){
+  room_filename = str;
+}
+
+string query_win_location(){
+  return win_location;
+}
+
+void set_win_location(string str){
+  win_location = str;
+}
