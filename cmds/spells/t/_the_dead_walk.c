f44f1ffb7e4c3ed5508700cac43cebf32781fd24
shadowgate <law@shadowgate.org>
2019-07-23 06:17:01 +0000

    autocommit 201907231706

--- cmds/spells/t/_the_dead_walk.c
+++ cmds/spells/t/_the_dead_walk.c
@@ -11,17 +11,19 @@ void create() {
     set_author("nienne");
     set_spell_name("the dead walk");
     set_spell_level(([ "warlock" : 2 ]));
-    set_syntax("cast CLASS the dead walk (on TARGET)");
+    set_syntax("cast CLASS the dead walk [on TARGET]");
     set_description("This invocation draws upon the power of the Plane of Shadows, infusing the recently dead with
 negative energy to reanimate their bodies as undead. You can raise a total number of hit dice worth of zombies and skeletons up to your casting level. They will follow you and defend you from your enemies. Unlike most common summoning spells, these monsters will not go away unless the caster quits. They are undead, so they can be turned, and so forth, but they cannot be dispelled. Many faiths and cultures condemn this spell and alike powers as it serves caster's selfish, often evil, motives and frequent users of the spell are known to be inherently evil.
 
 To command undead, use <command undead to ACTION>
 To dismiss undead, use <dismiss undead>
-To command lost undead to follow you, use <command undead to follow>");
+To command lost undead to follow you, use <command undead to follow>
+To check how many levels you have rised use <poolsize>");
     set_verbal_comp();
     set_somatic_comp();
     set_non_living_ok(1);
     set_helpful_spell(1);
+    evil_spell(1);
 }
 
 string query_cast_string() {
