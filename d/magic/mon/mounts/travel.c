13dd34ecfa06a71d312b3a3c335d1b013f8f3e53
shadowgate <law@shadowgate.org>
2019-07-27 03:17:01 +0000

    autocommit 201907271703

--- d/magic/mon/mounts/travel.c
+++ d/magic/mon/mounts/travel.c
@@ -1,9 +1,35 @@
 // selune: travel domain, faithful mount. Nienne, 05/07.
 #include <std.h>
-inherit "/d/magic/mon/mounts/travelinherit.c";
+inherit "/std/riding_animal.c";
 
 void create(){
     ::create();
+    ::create();
+    set_name("horse");
+    set_short("horse");
+    set_id( ({"horse"}) );
+    set_long("It's a horse. lawl.");
+    set_level(10);
+    set_value(0);
+    set_race("horse");
+    if(random(2))
+        set_gender("female");
+    else
+        set_gender("male");
+    set_body_type("equine");
+    set_attack_limbs( ({"right forehoof", "left forehoof"}) );
+    set_size(3);
+    set_max_hp(100);
+    set_hd(10,1);
+    set_hp(100);
+    set_overall_ac(3);
+    set_exp(1);
+    set_max_distance(10);
+    set_enter_room("rides in on a horse.");
+    set_exit_room("leaves riding a horse.");
+    set_vehicle_short("A horse");
+    set_attacks_num(2);
+    set_damage(2,6);
     set_name("gray mare");
     set_short("%^BOLD%^%^BLACK%^A gentle gray mare%^RESET%^");
     set_id( ({"horse","mare","gray mare","gentle mare","gentle gray mare","faithful mount"}) );
@@ -23,3 +49,35 @@ void create(){
     set_exit_room("leaves riding a gentle mare.");
     set_vehicle_short("%^BOLD%^%^BLACK%^A gentle gray mare%^RESET%^");
 }
+
+int enter(string str){
+    int lev, temp;
+
+    lev = TP->query_guild_level("cleric");
+    temp = TP->query_max_hp();
+    if(query_owner() != this_player()) {
+        tell_room(ETP, ""+TPQCN+" seems unable to mount the horse!", TP);
+        return notify_fail("The horse won't let you ride!\n");
+    }
+
+    set_level(lev*1/3);
+    set_mlevel("fighter", lev*1/3);
+    set_max_hp(temp*1/3);
+    set_damage(2,6);
+    set_hp(query_max_hp());
+    return ::enter(str);
+    return 1;
+}
+
+int handle_critter(string str) {
+    tell_room(ETP,"The horse seems uninterested in "+TPQCN+"'s attempts to handle it.", TP);
+    return notify_fail("You cannot handle this enchanted beast!\n");
+}
+
+int give_me_away(string str){
+    return notify_fail("Your mount refuses to leave you!\n");
+    return 0;
+}
+int save_me(string file) {return 0;}
+int restore_me(string file) { TO->remove(); return 0;}
+
