c50438bd5bdf4d07f067b7e21ec2ae87b69416c9
shadowgate <law@shadowgate.org>
2019-07-28 04:55:23 +0000

    autocommit 201907285504

--- d/common/obj/misc/backpack.c
+++ d/common/obj/misc/backpack.c
@@ -1,19 +1,14 @@
 #include <std.h>
-
-inherit PARMOUR;
+inherit "/std/bag_logic";
 
 void create() {
-	::create();
     set_id( ({"backpack","pack","bag"}) );
     set_name("backpack");
     set("short", "%^RESET%^%^ORANGE%^A leather backpack%^RESET%^");
     set_weight(5);
-    set("long", "This is a simple leather backpack that you could put things in. It has rope stripes to wear.\n");
+    set("long", "This is a simple leather backpack that you could put things in.\n");
     set_value(2);
-    set_max_internal_encumbrance(25);
-    set_type("ring");
-    set_limbs(({"torso"}));    
-    set_value(250);
-    set_property("repairtype",({ "leatherwork", "tailor" }));
+    set_property("treasure_type", "5A");
+    set_max_internal_encumbrance(40);
 }
 
