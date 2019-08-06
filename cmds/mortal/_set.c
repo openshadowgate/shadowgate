6fd6d1cbed89ad6142a7d3293ceb92ebfd031063
shadowgate <law@shadowgate.org>
2019-08-06 20:17:01 +0000

    autocommit 201908061720

--- cmds/mortal/_set.c
+++ cmds/mortal/_set.c
@@ -1,6 +1,6 @@
 #include <std.h>
 
-#define VALID_SETTINGS ({"simpleinv"})
+#define VALID_SETTINGS ({"simpleinv","hints"})
 
 int cmd_set(string args)
 {
@@ -17,12 +17,12 @@ int cmd_set(string args)
     }
     if(!call_other(TO,"set_"+setting,value))
     {
-        write("Unable to set "+setting+" to "+value+".");
+        write("%^WHITE%^%^BOLD%^Unable to set %^RED%^"+setting+"%^WHITE%^ to %^RED%^"+value+"%^WHITE%^.");
         return 1;
     }
     else
     {
-        write("Setting "+setting+" to "+value+".");
+        write("%^WHITE%^%^BOLD%^Setting %^GREEN%^"+setting+"%^WHITE%^ to %^CYAN%^"+value+"%^WHITE%^.");
         return 1;
     }
     return 1;
@@ -30,11 +30,40 @@ int cmd_set(string args)
 
 int set_simpleinv(string val)
 {
+    string *valid_values = ({"on","off"});
+    if(member_array(val,valid_values)==-1)
+    {
+        
+        write("%^BOLD%^%^RED%^Invalid value, valid values are:%^RESET%^ "+implode(valid_values,", "));
+        return 0;
+    }
+    if (val == "off")
+        TP->delete("simple inventory");
+    else
+        TP->set("simple inventory", 1);
+    return 1;
+}
+
+int set_hints(string val)
+{
+    string *valid_values = ({"on","off"});
+    if(member_array(val,valid_values)==-1)
+    {
+        
+        write("%^BOLD%^%^RED%^Invalid value, valid values are:%^RESET%^ "+implode(valid_values,", "));
+        return 0;
+    }
+    if (val == "off")
+        TP->delete("display hints");
+    else
+        TP->set("display hints", 0);
     return 1;
 }
 
 void help()
 {
+    //NOTE: PRESERVE FORMAT OF THIS HELPFILE FOR SORTING PURPOSES.
+    //Use M-x sort-lines if you're using emacs or | sort in vim.
     write("
 %^CYAN%^NAME%^RESET%^
 
@@ -47,6 +76,9 @@ set %^ORANGE%^%^ULINE%^SETTING%^RESET%^ %^ORANGE%^%^ULINE%^VALUE%^RESET%^
 %^CYAN%^DESCRIPTION%^RESET%^
 
 With this command you manipulate numerous mud settings:
+
+%^GREEN%^hints %^MAGENTA%^on|off%^RESET%^\n  This will turn on or off displaying equipped inventory items in <inventory> command. When it is toggled off, you can see equipped inventory in <eq>. %^MAGENTA%^Default value is on.%^RESET%^
+%^GREEN%^simpleinv %^MAGENTA%^on|off%^RESET%^\n  This will turn on or off displaying equipped inventory items in <inventory> command. When it is toggled off, you can see equipped inventory in <eq>. %^MAGENTA%^Default value is off.%^RESET%^
 
 %^CYAN%^SEE ALSO%^RESET%^
 
