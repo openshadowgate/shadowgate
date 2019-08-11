2226729283783b0f439458456f767ea829c1b16d
shadowgate <law@shadowgate.org>
2019-08-11 16:17:02 +0000

    autocommit 201908111716

--- cmds/druid/_wing.c
+++ cmds/druid/_wing.c
@@ -3,8 +3,9 @@
 #include <magic.h>
 inherit DAEMON;
 
-int cmd_wing(string str) {
-
+int cmd_wing(string str)
+{
+    object myshape;
     if (TP->query_bound() || TP->query_paralyzed())
     {
         TP->send_paralyzed_message("info",TP);
@@ -12,15 +13,16 @@ int cmd_wing(string str) {
     }
     if(!TP->query_property("shapeshifted"))
         return notify_fail("Only a bird can <wing>.\n");
-    if((string)TP->query("relationship_profile") != "druid_bird_999")
+    if((string)TP->query("relationship_profile") != "druid_bird_999" &&
+       (string)TP->query("relationship_profile") != "druid_dragon_999")
         return notify_fail("Only a bird can <wing>.\n");
     if(!objectp(myshape = TP->query_property("shapeshifted")))
-        return notify_fail("Only a bird can <wing>.\n");
+        return notify_fail("Something went wrong with your shape.\n");
     if(TP->query_casting())
         return notify_fail("You're already casting a spell!\n");
 }
 
-void help() {
+void help()
+{
     write("Bla bla bla cat placed stub");
 }
-
