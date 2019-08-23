5c9dc3d288c91cc5dabfe1a462b6390594718c62
shadowgate <law@shadowgate.org>
2019-08-23 09:17:01 +0000

    autocommit 201908231709

--- daemon/chat.c
+++ daemon/chat.c
@@ -12,8 +12,12 @@ int list_channel(string str);
 object *arches(object *objs);
 void do_amsg(object tp, object *list, string verb, string str, int emote);
 
+/**
+ * @file
+ * @brief Channels related functions
+ */
 
-void create() 
+void create()
 {
     string *chans;
     object *who;
@@ -22,10 +26,10 @@ void create()
     seteuid(getuid());
     channels = ([]);
     i = sizeof(who = users());
-    while(i--) 
+    while(i--)
     {
         j = sizeof(chans = (string *)who[i]->query_channels());
-        while(j--) 
+        while(j--)
         {
             if(!channels[chans[j]]) channels[chans[j]] = ({});
             channels[chans[j]] = distinct_array(channels[chans[j]]+({who[i]}));
@@ -35,7 +39,7 @@ void create()
 
 /**
  * This function formats messages
- * 
+ *
  * @param chan Channel for the message
  * @param name Name of the player
  * @param position Special position character to display whether player is an imm or not
@@ -68,15 +72,15 @@ string format_chat(string chan,string name,string position,string message,int em
  * @param who Whom to add
  *
  */
-varargs void add_user(string *chans, object who) 
+varargs void add_user(string *chans, object who)
 {
     object ob;
     int i;
     if(objectp(who) && userp(who)) ob = who;
     else if(base_name(ob = previous_object()) != OB_USER) return;
-    
+
     i = sizeof(chans);
-    while(i--) 
+    while(i--)
     {
         if(!channels[chans[i]]) channels[chans[i]] = ({});
         channels[chans[i]] = distinct_array(channels[chans[i]]+({ob}));
@@ -89,14 +93,14 @@ varargs void add_user(string *chans, object who)
  * @param *chans Pointer to channels array
  *
  */
-void remove_user(string *chans) 
+void remove_user(string *chans)
 {
     object ob;
     int i;
 
     if(base_name(ob = previous_object()) != OB_USER) return;
     i = sizeof(chans);
-    while(i--) 
+    while(i--)
     {
         if(!channels[chans[i]]) continue;
         else channels[chans[i]] -= ({ ob });
@@ -108,37 +112,38 @@ void remove_user(string *chans)
  * Sends message to everyone on related channel
  *
  * @param verb A verb user used to invoke this function. Could be a
- * channel name or channel name+emote to indicate emotes and actions
+ *        channel name or channel name+emote to indicate emotes and
+ *        actions
  * @param str Message user is sending
  *
  * @return 1 on success, 0 on failure.
  *
  */
-int do_chat(string verb, string str) 
+int do_chat(string verb, string str)
 {
     string msg;
     int emote;
     object *archlist, *pllist;
     string tpname,tppos;
 
-    if(!channels[verb]) 
+    if(!channels[verb])
     {
         if(sscanf(verb, "%semote", verb)) emote = 1;
         else return 0;
         if(!channels[verb]) return 0;
     }
-    
+
     if(member_array(TP, channels[verb]) == -1) return 0;
-    
-    if(!str) 
+
+    if(!str)
     {
         TP->set_blocked(verb);
         return 1;
     }
-    
-    if((int)TP->query_blocked(verb)) 
+
+    if((int)TP->query_blocked(verb))
     {
-        if((int)TP->query_blocked("all")) 
+        if((int)TP->query_blocked("all"))
         {
             message(verb, "%^BOLD%^%^RED%^You cannot chat while totally blocked.%^RESET%^", TP);
             return 1;
@@ -147,7 +152,7 @@ int do_chat(string verb, string str)
     }
 
     str += RESET ;
-    
+
     pllist = channels[verb];
     //pllist -= ({TP});
 
@@ -166,13 +171,13 @@ int do_chat(string verb, string str)
 }
 
 /**
- * This function prints list of everyone on a channel 
- * 
+ * This function prints list of everyone on a channel
+ *
  * @param str A channel to use
  *
  * @return 1 on succes, 0 on failure
  */
-int list_channel(string str) 
+int list_channel(string str)
 {
     string list;
     int i;
@@ -183,27 +188,27 @@ int list_channel(string str)
     list = "";
     list = "%^BOLD%^Users with the "+str+" channel on %^RESET%^(are true_invis):";
     i =sizeof(channels[str]);
-    while(i--) 
+    while(i--)
     {
         if(!channels[str][i]) continue;
         if (channels[str][i]->query_quietness() && wizardp(channels[str][i]) && !wizardp(TP)) continue;
         if((int)channels[str][i]->query_blocked(str)) continue;
         if(channels[str][i]->query_true_invis() && channels[str][i]->query_level() > TP->query_level() && TP != channels[str][i])  continue;
-        if(channels[str][i]->query_true_invis()) 
+        if(channels[str][i]->query_true_invis())
         {
             list += "    ("+capitalize(channels[str][i]->query_name()+")");
-        } 
-        else if(channels[str][i]->query_disguised()) 
+        }
+        else if(channels[str][i]->query_disguised())
         {
             list += "    "+capitalize(channels[str][i]->query_vis_name());
-        } 
-        else 
+        }
+        else
         {
             list += "    "+capitalize(channels[str][i]->query_name());
         }
         //list += "     "+channels[str][i]->query_cap_name();
     }
-    
+
     message("info", list, TP);
     return 1;
 }
@@ -252,13 +257,13 @@ void do_amsg(object tp, object *list, string chan, string msg, int emote)
         message(chan,format_chat(chan,tpname,tppos,msg,emote),list[i],ignored_list(TP));
 }
 
-int clear_channels() 
+int clear_channels()
 {
     channels = ([ ]);
     return 1;
 }
 
-void force_chat(object tp, string verb, string str, int emote) 
+void force_chat(object tp, string verb, string str, int emote)
 {
     object * list;
     if(!channels[verb]) return;
@@ -272,13 +277,13 @@ object *ignored_list(object obj)
     object *ppl,*ignoring=({});
     string name, *ignored;
     int i;
-    
+
     if(!objectp(obj)) { return ({}); }
     if(avatarp(obj)) { return ({}); }
     name = obj->query_true_name();
-    
+
     ppl = users();
-    
+
     for(i=0;sizeof(ppl),i<sizeof(ppl);i++)
     {
         if(!objectp(ppl[i])) { continue; }
@@ -287,6 +292,6 @@ object *ignored_list(object obj)
         if(member_array(name,ignored) == -1) { continue; }
         ignoring += ({ ppl[i] });
     }
-    
+
     return ignoring;
 }
