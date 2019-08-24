ebf1477fe58443723720bd36c9c7e354c8f5408f
shadowgate <law@shadowgate.org>
2019-08-24 19:17:01 +0000

    autocommit 201908241719

---
+++ cmds/mortal/_masterproto.c
@@ -0,0 +1,32 @@
+#include <std.h>
+#include <magic.h>
+#include <daemons.h>
+#include <master_limits.h>
+
+/**
+ * @file
+ */
+
+int cmd_masterproto(string args)
+{
+
+
+}
+
+
+void help()
+{
+    write("
+%^CYAN%^NAME%^RESET%^
+
+
+
+%^CYAN%^SYNTAX%^RESET%^
+
+%^CYAN%^DESCRIPTION%^RESET%^
+
+%^CYAN%^SEE ALSO%^RESET%^
+
+
+");
+}
