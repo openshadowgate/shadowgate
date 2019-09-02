8553857a00d8606c75dc5e5bf38657b6db0f8374
shadowgate <law@shadowgate.org>
2019-09-02 03:27:45 +0000

    autocommit 201909022703

--- std/class/stonelord.c
+++ std/class/stonelord.c
@@ -3,8 +3,8 @@
 #include <daemons.h>
 inherit DAEMON;
 
-void create() { 
-    ::create(); 
+void create() {
+    ::create();
 }
 
 object base_class_ob(object ob) {
@@ -21,7 +21,7 @@ string *query_base_classes(object obj) {
     if(!objectp(obj)) { return ({}); }
     base = obj->query("stonelord_base_class");
     if(!base) { return ({}); }
-    return ({ base }); 
+    return ({ base });
 }
 
 
@@ -48,7 +48,7 @@ int set_base_class(object obj, string choice) {
     }
     classes = obj->query_classes();
     if(!sizeof(classes)) { return 0; }
-    if(member_array(choice,classes) == -1) { return 0; }    
+    if(member_array(choice,classes) == -1) { return 0; }
     obj->set("stonelord_base_class",choice);
     return 1;
 }
@@ -75,8 +75,8 @@ string requirements() { // string version, maybe we'll need this, maybe not, can
         "    20 Base Class Levels\n"
         "    40 Character Levels\n"
         "    20 Points Spent in Endurance Skill\n"
-        "    Dwarf Race\n";
-    return str;    
+        "    Dwarf or Eath Genasi Race\n";
+    return str;
 }
 
 
@@ -87,20 +87,22 @@ int prerequisites(object player) {
     int adj;
     if(!objectp(player)) { return 0; }
 
-    if((string)player->query_race() != "dwarf") return 0;
+    if((string)player->query_race() != "dwarf" ||
+       (string)player->query("subrace") != "earth genasi")
+        return 0;
     race = player->query("subrace");
     if(!race) { race = player->query_race(); }
     race_ob = find_object_or_load(DIR_RACES+"/"+player->query_race()+".c");
     if(!objectp(race_ob)) { return 0; }
-    adj = race_ob->level_adjustment(race);    
-    skills = player->query_skills();    
-    if(skills["endurance"] < 20) { return 0; }    
+    adj = race_ob->level_adjustment(race);
+    skills = player->query_skills();
+    if(skills["endurance"] < 20) { return 0; }
     base = player->query("stonelord_base_class");
     if(!base) { return 0; }
     if(!player->is_class(base)) { return 0; }
-    if((player->query_class_level(base) + adj) < 20) { return 0; }    
+    if((player->query_class_level(base) + adj) < 20) { return 0; }
     if(player->query_level() < 40) { return 0; }
-    return 1;    
+    return 1;
 }
 
 mapping stat_requirements(object ob) { return base_class_ob(ob)->stat_requirements(); }
@@ -116,17 +118,17 @@ int caster_level_calcs(object player, string the_class) {
     string base;
     if(!objectp(player)) { return 0; }
     base = player->query("stonelord_base_class");
-    
+
     level = player->query_class_level(base);
-    level += player->query_class_level("stonelord");            
+    level += player->query_class_level("stonelord");
     return level;
 }
 
-mapping class_featmap(string myspec) {  
+mapping class_featmap(string myspec) {
     return ([ 1 : ({ "earthen blood" }), 4 : ({ "mighty resilience" }), 7 : ({ "command the stone" }), ]);
 }
 
-string *class_skills(object ob) {  
+string *class_skills(object ob) {
     return base_class_ob(ob)->class_skills();
 }
 
