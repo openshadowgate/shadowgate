5319a7455cb3490108d120e18c6a8c6b3c9d4a8e
shadowgate <law@shadowgate.org>
2019-09-05 10:17:01 +0000

    autocommit 201909051710

--- cmds/spells/p/_profane_nimbus.c
+++ cmds/spells/p/_profane_nimbus.c
@@ -9,20 +9,20 @@ int strikes;
 void create()
 {
     ::create();
-    set_spell_name("sacred nimbus");
+    set_spell_name("profane nimbus");
     set_spell_level(([ "cleric" : 5, "paladin" : 4 ]));
     set_spell_sphere("combat");
-    set_syntax("cast CLASS sacred nimbus");
-    set_damage_desc("divine to evil");
-    set_description("You are surrounded by a nimbus of golden light shaped like your god’s holy symbol or a symbol of your faith. Any evil creature attacking you will be burned by the light.");
+    set_syntax("cast CLASS profane nimbus");
+    set_damage_desc("divine to good");
+    set_description("You are surrounded by a nimbus of darkness shaped like your god’s holy symbol or a symbol of your faith. Any good creature attacking you will be scorched by the darkness.");
     set_property("magic",1);
     traveling_aoe_spell(1);
 }
 
 string query_cast_string()
 {
-    tell_object(caster,"%^YELLOW%^You growl a quiet plea to your divine patron.");
-    tell_room(place,"%^YELLOW%^"+caster->QCN+" growls a quiet plea under "+caster->QP+" breath.",caster);
+    tell_object(caster,"%^BOLD%^%^MAGENTA%^You growl a quiet plea to your divine patron.");
+    tell_room(place,"%^BOLD%^%^MAGENTA%^"+caster->QCN+" growls a quiet plea under "+caster->QP+" breath.",caster);
     return "display";
 }
 
@@ -43,12 +43,12 @@ void spell_effect(int prof)
     int duration;
     duration = (ROUND_LENGTH * 10) * clevel;
 
-    tell_object(caster,"%^YELLOW%^You feel your gods power warding you from the evil!");
-    tell_room(place,"%^YELLOW%^"+caster->QCN+" is suddenly surrounded by halo of light!",caster);
+    tell_object(caster,"%^BOLD%^%^MAGENTA%^You feel your gods power warding you from the evil!");
+    tell_room(place,"%^BOLD%^%^MAGENTA%^"+caster->QCN+" is suddenly surrounded by halo of darkness!",caster);
 
     caster->set_property("spelled", ({TO}));
     caster->set_property("nimbus",1);
-    caster->set_property("added short",({"%^YELLOW%^ (in halo of light)%^RESET%^"}));
+    caster->set_property("added short",({"%^BOLD%^%^MAGENTA%^ (in halo of darkness)%^RESET%^"}));
     addSpellToCaster();
     spell_successful();
     call_out("dest_effect",duration);
@@ -67,9 +67,9 @@ void execute_attack()
     if(strikes > clevel)                        { dest_effect(); return; }
 
     attackers = filter_array(caster->query_attackers(),(:
-                                                        $1 == 3 ||
-                                                        $1 == 6 ||
-                                                        $1 == 9
+                                                        $1 == 1 ||
+                                                        $1 == 4 ||
+                                                        $1 == 7
                                                         :));
 
     if(!sizeof(attackers))
@@ -91,9 +91,9 @@ void execute_attack()
 
     define_base_damage(-9);
 
-    tell_object(caster,"%^BOLD%^%^YELLOW%^The light burns "+targ->QCN+" painfully!");
-    tell_object(targ,"%^BOLD%^%^YELLOW%^The halo around "+caster->QCN+"'s body burns your very soul!");
-    tell_room(place,"%^BOLD%^%^YELLOW%^The halo around "+caster->QCN+" burns "+targ->QCN+"!",({targ,caster}));
+    tell_object(caster,"%^BOLD%^%^MAGENTA%^The darkness burns "+targ->QCN+" painfully!");
+    tell_object(targ,"%^BOLD%^%^MAGENTA%^The halo around "+caster->QCN+"'s body burns your very soul!");
+    tell_room(place,"%^BOLD%^%^MAGENTA%^The halo around "+caster->QCN+" burns "+targ->QCN+"!",({targ,caster}));
 
     strikes++;
     damage_targ(targ,targ->return_target_limb(),damage,"divine");
@@ -109,7 +109,7 @@ void dest_effect()
     {
         tell_object(caster,"%^RESET%^%^BOLD%^The halo around you fades.");
         caster->remove_property("nimbus");
-	    caster->remove_property_value("added short",({"%^YELLOW%^ (in halo of light)%^RESET%^"}));
+	    caster->remove_property_value("added short",({"%^BOLD%^%^MAGENTA%^ (in halo of darkness)%^RESET%^"}));
     }
     ::dest_effect();
     if(objectp(TO)) { TO->remove(); }
