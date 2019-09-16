aed307db5ab19bd1be1ff90581fee38977b84c89
shadowgate <law@shadowgate.org>
2019-09-16 19:58:21 +0000

    autocommit 201909165819

--- cmds/spells/b/_boneshatter.c
+++ cmds/spells/b/_boneshatter.c
@@ -12,7 +12,7 @@ void create()
     set_spell_sphere("necromancy");
     set_syntax("cast CLASS boneshatter on TARGET");
     set_damage_desc("on living only, untyped, -4 or -2 on save to str and dex");
-    set_description("With mere will you splinter you enemies skeleton or external shell. The enemy fights the change and if they fail, they become exhausted, taking damage to their strength and dexterity. If they Successfully overcome the pain, they instead become fatigued for a short while. This spell will work only on living beings, and will cause stat damage just once per target.");
+    set_description("With mere will you splinter you enemy's skeleton or external shell. The enemy fights the change and if they fail, they become exhausted, taking damage to their strength and dexterity. If they Successfully overcome the pain, they instead become fatigued for a short while. This spell will work only on living beings, and will cause stat damage just once per target.");
     set_save("fort");
     set_target_required(1);
 }
