04fc5effe6fc954b799f5011981af05a01b7d157
shadowgate <law@shadowgate.org>
2019-09-21 04:17:01 +0000

    autocommit 201909211704

--- cmds/mortal/_rumors.c
+++ cmds/mortal/_rumors.c
@@ -104,6 +104,27 @@ arealist = ({
             ({"Mysterious Plateau (Aramanth)",45,50,}),
                 });
 
+mapping areas = ([]);
+areas = ([
+             "/d/shadow/city" : "Shadow",
+             "/d/shadow/juran/city" : "Juran",
+             "/d/shadow/room/muuldaan/rooms/" : "Muul'daan",
+             "/d/darkwood/tabor/room/" : "Tabor",
+             "/d/azha/town/" : "Azha",
+             "/d/darkwood/camps/rooms/town/" : "Synoria",
+             "/d/tharis/newtharis/rooms" : "Tharis",
+             "/d/antioch/antioch2/rooms" : "Antioch",
+             "/d/dagger/tonovi/town" : "Tonovi",
+             "/d/dagger/Torm/city" : "Torm",
+             "/d/dagger/kinaro/" : "Kinaro",
+             "/d/attaya/newseneca/rooms" : "Seneca",
+             "/d/deku/town" : "Verbobone",
+             "/d/undead/rooms/town" : "Graez",
+             "/d/darkwood/room/rhapsody" : "the Rhapsody inn",
+             "/d/player_houses/innaeli/rooms/bsb1" : "the Bloody Scourge inn",
+             ]);
+
+
 int cmd_rumors(string args)
 {
     mixed * area;
@@ -112,8 +133,41 @@ int cmd_rumors(string args)
     int dispflag = 0;
     string color;
 
+    {
+        object *peeps, peep;
+        string *places = ({}), *searchlist = ({}), fn;
+        int i;
+
+        peeps = users();
+        if(!sizeof(peeps))
+            tell_object(TP,"%^BOLD%^%^CYAN%^There are no rumors of adventurers passing through cities.%^RESET%^\n");
+        else
+        {
+            searchlist = keys(areas);
+            foreach(peep in peeps)
+            {
+                if(!objectp(ENV(peep)))
+                    continue;
+                fn = base_name(environment(peep));
+                for(i = 0;i < sizeof(searchlist); i++)
+                {
+                    if((strsrch(fn,searchlist[i]) != -1) &&
+                       (member_array(areas[searchlist[i]],places) == -1))
+                        places += ({ areas[searchlist[i]] });
+                }
+            }
+
+            if(!sizeof(places))
+                tell_object(TP,"%^BOLD%^%^CYAN%^There are no rumors of adventurers passing through cities.%^RESET%^\n");
+            else
+                tell_object(TP, "%^BOLD%^%^CYAN%^There are rumors of adventurers recently passing through "+implode(places,", ")+".%^RESET%^\n");
+        }
+    }
+
+
     if(args =="all")
         allflag = 1;
+    write("%^BOLD%^The next areas might be appropriate to your level:");
     write("%^BOLD%^Levels Area");
     foreach(area in arealist)
     {
@@ -132,6 +186,8 @@ int cmd_rumors(string args)
         if(allflag||dispflag)
             write("%^CYAN%^"+sprintf("%2d",area[1])+"-"+sprintf("%2d",area[2])+color+"  "+area[0]+"%^RESET%^");
     }
+
+
     return 1;
 }
 
@@ -149,6 +205,8 @@ rumors [all]
 %^CYAN%^DESCRIPTION%^RESET%^
 
 This command prints recommended areas list that fits withing your level range.
+
+It will also display whether there any people in any cities you can roleplay with.
 
 %^CYAN%^SEE ALSO%^RESET%^
 
