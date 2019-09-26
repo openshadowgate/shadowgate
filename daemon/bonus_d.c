4d1aa4f7dfb3ac90ac3d4f915809196a324fc6fe
shadowgate <law@shadowgate.org>
2019-09-26 03:17:01 +0000

    autocommit 201909261703

--- daemon/bonus_d.c
+++ daemon/bonus_d.c
@@ -12,7 +12,7 @@ int query_stat_bonus(object who, string stat);
 
 int high_thaco(int level) { return (level - 20); }
 
-varargs int thaco(int level, string myclass,object ob) 
+varargs int thaco(int level, string myclass,object ob)
 {
     string file,*classes,myrace,mysubrace;
     int ret,i;
@@ -34,7 +34,7 @@ varargs int thaco(int level, string myclass,object ob)
             classes = (string *)ob->query_classes();
             if(!sizeof(classes)) { return 0; }
             for(i=0;i<sizeof(classes);i++)
-            {                
+            {
                 file = DIR_CLASSES+"/"+classes[i]+".c";
                 if(!file_exists(file)) { continue; }
                 ret += file->attack_bonus(ob);
@@ -51,7 +51,7 @@ varargs int thaco(int level, string myclass,object ob)
         }
     }
 
-    if (myclass == "fighter" || myclass == "ranger" || myclass == "paladin" || myclass == "antipaladin" || myclass == "cavalier" || myclass == "barbarian") 
+    if (myclass == "fighter" || myclass == "ranger" || myclass == "paladin" || myclass == "antipaladin" || myclass == "cavalier" || myclass == "barbarian")
     {
         ret = 20 - (level -1); // going back to the old 1 point per level formula -08/19/07 Ares
       //  if(level < 21) { ret = (20 - (level - 1)); }
@@ -60,14 +60,14 @@ varargs int thaco(int level, string myclass,object ob)
     if (myclass == "cleric" || myclass == "druid" || myclass == "warlord" || myclass == "psywarrior" || myclass == "monk") //temporary
     {
         if(level < 21) { ret = (20 - (2*((level-1) / 3))); }
-        else { ret = 8 - high_thaco(level); } 
+        else { ret = 8 - high_thaco(level); }
     }
-    if (myclass == "mage" || myclass == "illusionist" || myclass == "psion") 
+    if (myclass == "mage" || myclass == "illusionist" || myclass == "psion")
     {
         if(level < 21) { ret = (20 - ((level - 1) / 3)); }
         else { ret = 14 - high_thaco(level); }
     }
-    if (myclass == "thief" || myclass == "bard" || myclass == "assassin") 
+    if (myclass == "thief" || myclass == "bard" || myclass == "assassin")
     {
         if(level < 21) { ret = (20 - ((level + 1) / 2)); }
         else { ret = 10 - high_thaco(level); }
@@ -96,21 +96,21 @@ int query_dex_bonus(object target)
 {
     object *torso=({});
     int i,dex,ret,enc,bonus,max=10;
-    
+
     if(!objectp(target)) { return 0; }
     dex = (int)target->query_stats("dexterity");
 
     ret = (dex - 10) / 2 * -1;
 
-    if(!interactive(target)) 
-    { 
+    if(!interactive(target))
+    {
         //limiting this to a max of 3 from dex for mobs, simply because mobs end up with stupidly high dex stats
-        //unless they are manually set and hopefully this might 
+        //unless they are manually set and hopefully this might
         //help balance things out since I removed the double dex bonus to attack that everyone got - Saide
         if(target->query_property("full ac"))
         {
             if(ret > 3) ret = 3;
-            return ret; 
+            return ret;
         }
         else
         {
@@ -150,8 +150,8 @@ int query_dex_bonus(object target)
     // taking this out since we're letting everybody wear bracers now -Ares
     /*
     // gives a bonus of 1 point per 7 levels for fighter or cleric not wearing heavy armor
-    if ((!target->is_class("ranger") && !target->is_class("thief") && 
-        !target->is_class("mage") && !target->is_class("psion")) && 
+    if ((!target->is_class("ranger") && !target->is_class("thief") &&
+        !target->is_class("mage") && !target->is_class("psion")) &&
         !target->is_wearing_type("armour") && !target->is_wearing_type("armor"))
     {
         ret = ret + ( ((int)target->query_lowest_level()/7) * -1);
@@ -163,7 +163,7 @@ int query_dex_bonus(object target)
 
 int query_stance_bonus(object victim){
   int fnord= 0;
-  
+
   fnord -= (int)victim->query_defensive_bonus();
   fnord += (int)PO->query_offensive_bonus();
   return fnord;
@@ -175,7 +175,7 @@ int query_stance_bonus(object victim){
 int new_bab(int level, object ob)
 {
     string *classes, file, myrace, mysubrace;
-    int i, ret = 0;    
+    int i, ret = 0;
     if(objectp(ob))
     {
         if((int)ob->query_property("transformed") || (int)ob->query_property("dance-of-cuts")) { // fighter BAB under transformation/dance.
@@ -187,10 +187,10 @@ int new_bab(int level, object ob)
         classes = (string *)ob->query_classes();
         if(!sizeof(classes)) { return 0; }
         for(i=0;i<sizeof(classes);i++)
-        {                
+        {
             file = DIR_CLASSES+"/"+classes[i]+".c";
             if(!file_exists(file)) { continue; }
-            ret += file->attack_bonus(ob);            
+            ret += file->attack_bonus(ob);
             //since mobs are usually much higher level than players - Saide
             if(!interactive(ob)) return ret;
         }
@@ -220,7 +220,7 @@ int tohit_bonus(int dex, int str, object target) {
     ret = query_dex_bonus(target)*(-1);
     //this was decreasing their bonus for some reason. *(-1) to see if it fixes. Nienne, 07/10
     //ret += query_stance_bonus(target);
-    ret += abonus;    
+    ret += abonus;
     ret += (str - 10) / 2;
 
     return ret;
@@ -230,21 +230,21 @@ int tohit_bonus(int dex, int str, object target) {
 varargs effective_ac(object who)
 {
     int MyAc = 0, tmp = 0;
-    
+
     if(!objectp(who)) return 0;
-    
+
     //AC is starting at 10 now - Saide
-    //AC starts at 10 and goes up 
+    //AC starts at 10 and goes up
     //however, our AC goes down by default
     //so someone with a 2 AC should have an 18 now
-    //or be 8 points away from the base of 10, 
-    //someone with a -1 AC would have 21 AC - 
+    //or be 8 points away from the base of 10,
+    //someone with a -1 AC would have 21 AC -
     //because they are 11 points away from the base
-    //I had actually forgot this, then fixed it, then 
-    //realized I was right the first time - hopefully 
-    //this will explain why AC is 10 and then 
-    //the difference from 10 and AC is added 
-    //if the query_ac is above 0 and why 
+    //I had actually forgot this, then fixed it, then
+    //realized I was right the first time - hopefully
+    //this will explain why AC is 10 and then
+    //the difference from 10 and AC is added
+    //if the query_ac is above 0 and why
     //an additional 10 is added if it's below 0 - Saide
     MyAc = 10;
     tmp = (int)who->query_ac();
@@ -266,11 +266,11 @@ varargs ac_bonus(object who, object attacker)
     tmp *= -1;
     MyBonus += tmp;
     //2AC dodge bonus to target if they are actively dodging the attacker
-    if(FEATS_D->usable_feat(who,"dodge") && (string)who->query_property("dodging") == (string)attacker->query_name()) { MyBonus += 2; }    
+    if(FEATS_D->usable_feat(who,"dodge") && (string)who->query_property("dodging") == (string)attacker->query_name()) { MyBonus += 2; }
     return MyBonus;
 }
 
-varargs int hit_bonus(object who, object targ, int attack_num, object current) 
+varargs int hit_bonus(object who, object targ, int attack_num, object current)
 {
     int th, to_hit, tmp;
     int i,j,min,hold,mysize,fired,bab_scale, pen;
@@ -283,13 +283,13 @@ varargs int hit_bonus(object who, object targ, int attack_num, object current)
     if (who->query_unconscious() || who->query_bound()) { return 0; }
     if(!objectp(targ)) { return 0; }
     //in 3ed you get base attack bonus for your first attack and then
-    //-5 for every subsequent attack which is actually included 
-    //in the bab tables so attack 1 is full bonus, attack 2 
-    //is -5, attack 3 is -10 - I am not entirely certain how 
-    //this will translate here since there are many more 
+    //-5 for every subsequent attack which is actually included
+    //in the bab tables so attack 1 is full bonus, attack 2
+    //is -5, attack 3 is -10 - I am not entirely certain how
+    //this will translate here since there are many more
     //attacks for some classes - but will try it out and adjust
-    //accordingly - Saide 
-    //adjusting to -2 penalty per subsequent attack - 
+    //accordingly - Saide
+    //adjusting to -2 penalty per subsequent attack -
     //capping at -6 for everything now - Saide, November 2016
     if (sizeof(distinct_array((object *)who->query_wielded())) > 1)
     {
@@ -299,15 +299,15 @@ varargs int hit_bonus(object who, object targ, int attack_num, object current)
     if(pen > 6) pen = 6;
 
     th -= pen;
-    
+
     th += (int)who->query_attack_bonus();
-    
+
     tmp = new_bab((int)who->query_level(), who);
     //tmp *= -1;
     th += tmp;
-    
+
     to_hit = th;
-    
+
     if( (current && objectp(current)) && !who->query_property("shapeshifted") && current != who) { // weapon finesse calculations if wielding a weapon
       if(current->is_lrweapon())
       {
@@ -329,13 +329,13 @@ varargs int hit_bonus(object who, object targ, int attack_num, object current)
       }
     }
     // if no weapon (unarmed), run the same check
-    else 
+    else
     {
         if(FEATS_D->usable_feat(who,"weapon finesse"))
             to_hit += (query_dex_bonus(who) * -1);
         else to_hit += query_stat_bonus(who, "strength");
     }
- 
+
     // +1 BAB to rock gnome, racial, vs goblinoids
     if((string)who->query_race() == "gnome" && (string)who->query("subrace") == "rock gnome") {
       if((string)targ->query_race() == "goblin" || (string)targ->query_race() == "hobgoblin" || (string)targ->query_race() == "bugbear") to_hit += 1;
@@ -343,7 +343,7 @@ varargs int hit_bonus(object who, object targ, int attack_num, object current)
     // +1 BAB to morinnen human ethnicity if mounted
     if((string)who->query_race() == "human" && (string)who->query("subrace") == "morinnen" && who->query_in_vehicle()) to_hit += 1;
 
-    if (current && objectp(current) && current != who) 
+    if (current && objectp(current) && current != who)
     {
         if(current->query_property("enchantment")) {
             to_hit += (int)current->query_property("enchantment");
@@ -352,10 +352,10 @@ varargs int hit_bonus(object who, object targ, int attack_num, object current)
     if((!objectp(current) || current == who) && (who->is_class("monk") || FEATS_D->usable_feat(who, "enchanted fists")) && !who->query_property("shapeshifted"))
     {
         //if(find_player("saide")) tell_object(find_player("saide"), "Should be using unarmed enchantments... equal to "+COMBAT_D->unarmed_enchantment(who));
-        to_hit += COMBAT_D->unarmed_enchantment(who);        
+        to_hit += COMBAT_D->unarmed_enchantment(who);
     }
     if(who->query("protecting")) { to_hit -= random(7)+1; }
-    if(who->query_blind() && !FEATS_D->usable_feat(who,"blindfight")) { to_hit -= 4 + random(4); }
+    if(who->query_blind() && !FEATS_D->usable_feat(who,"blindfight")) { to_hit -= 8 + random(4); }
 
 
     // trying to scale bab with each attack, might take some tweaking -Ares
@@ -379,7 +379,7 @@ varargs int process_hit(object who, object targ, int attack_num, mixed current, 
     if(!intp(attack_num)) attack_num = 1;
     if(!flag) AC += effective_ac(targ);
     AC += ac_bonus(targ, who);
-    
+
     if (!interactive(who))
     {
         if(objectp(PlayerBoss = who->query_property("minion")))
@@ -401,11 +401,11 @@ varargs int process_hit(object who, object targ, int attack_num, mixed current, 
 
     if(intp(current)) bon += current;
     attack_roll = roll_dice(1, 20);
-    //removing the 1 attack roll = automatic miss for now - perhaps it is 
+    //removing the 1 attack roll = automatic miss for now - perhaps it is
     //overly punitive on our game? - Saide May 2016
     /*if(attack_roll == 1 && interactive(who))
     {
-        //essentially giving everyone the chance to reroll 1s, since they 
+        //essentially giving everyone the chance to reroll 1s, since they
         //are a miss now - at least after their first one - Saide
         if((int)who->query_property("rolled_a_1"))
         {
@@ -414,9 +414,9 @@ varargs int process_hit(object who, object targ, int attack_num, mixed current, 
         }
         else who->set_property("rolled_a_1", 1);
     }*/
-    
-    if(objectp(DebugOb)) 
-    {        
+
+    if(objectp(DebugOb))
+    {
         tell_object(DebugOb, "Attack roll = "+ attack_roll);
         tell_object(DebugOb, "Attack bonus = "+ bon);
         tell_object(DebugOb, "AC = "+AC);
@@ -424,13 +424,13 @@ varargs int process_hit(object who, object targ, int attack_num, mixed current, 
     if(attack_roll == 20) return 20;
     //if(attack_roll == 1) return -1;
     //does this change make AC less OP? - Saide, August 2017
-    if((bon + 15) < AC) 
+    if((bon + 15) < AC)
     {
         if(random(bon+AC+attack_roll) >= AC) return attack_roll;
         return 0;
     }
     if((attack_roll+bon) >= AC) return attack_roll;
-    return 0;    
+    return 0;
 }
 
 int query_stat_bonus(object who, string stat)
@@ -438,7 +438,7 @@ int query_stat_bonus(object who, string stat)
     int ret;
     if(!objectp(who)) return 0;
     if(!stringp(stat)) return 0;
-    return ret = (((int)who->query_stats(stat) - 10) / 2);    
+    return ret = (((int)who->query_stats(stat) - 10) / 2);
 }
 
 
@@ -455,11 +455,11 @@ int damage_bonus(int str) {
     ret += dbonus;
     return ret;
 }
-//added to allow this to still function since 
+//added to allow this to still function since
 //the function that called damage_bonus() is located
-//in combat_d.c now - which would mean combat_d.c is 
+//in combat_d.c now - which would mean combat_d.c is
 //now the previous object - Saide, September 2016
-int new_damage_bonus(object attacker, int str) 
+int new_damage_bonus(object attacker, int str)
 {
     int dbonus;
     if(objectp(attacker)) dbonus = (int)attacker->query_damage_bonus();
