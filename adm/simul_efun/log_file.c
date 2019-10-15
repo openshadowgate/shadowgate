06ccdaa325c636001da1be15c62c43a05b10fe99
shadowgate <law@shadowgate.org>
2019-10-15 17:17:01 +0000

    autocommit 201910151717

--- adm/simul_efun/log_file.c
+++ adm/simul_efun/log_file.c
@@ -13,18 +13,21 @@
  * @param fl Logfile name in logs directory you're going to write to
  * @param msg Message
  */
-void log_file(string fl, string msg)
+varargs void log_file(string fl, string msg, int continous)
 {
-//  Commented out by Thorn -- 950530
-//  if(base_name(previous_object()) != OB_USER &&
-//      geteuid(previous_object()) != UID_LOG &&
-//      geteuid(previous_object()) != UID_ROOT) return;
+    string ofname;
+
+
     if(!stringp(fl)) error("Bad argument 1 to log_file().\n");
     if(!stringp(msg)) msg = identify(msg);
     if(strsrch(fl, "..") != -1) error("Illegal file reference.\n");
     seteuid(UID_ROOT);
+    if(continous)
+        ofname = sprintf("%s.%d", fl,time());
+    else
+        ofname = sprintf("%s.old", fl);
     if(file_size(fl = sprintf("%s/%s", DIR_LOGS, fl)) > MAX_LOG_SIZE)
-      rename(fl, sprintf("%s.old", fl));
+      rename(fl, ofname);
     msg = ctime(time()) + " : " +msg;
     write_file(fl, msg);
     seteuid(UID_MUDLIB);
--- std/user/tsh.c
+++ std/user/tsh.c
@@ -215,9 +215,9 @@ nomask string process_input(string arg)
     USER_D->process_pkill_input(this_player(), arg);
     //TODO: log to syslog
     if(avatarp(TP))
-        log_file("avlog/"+TP->query_true_name(),identify(TP)+" "+do_alias(do_nicknames(handle_history(arg)))+"\n");
+        log_file("avlog/"+TP->query_true_name(),identify(TP)+" "+do_alias(do_nicknames(handle_history(arg)))+"\n",1);
     else
-        log_file("plog/"+TP->query_true_name(),do_alias(do_nicknames(handle_history(arg)))+"\n");
+        log_file("plog/"+TP->query_true_name(),do_alias(do_nicknames(handle_history(arg)))+"\n",1);
 
     if(adminBlock())
         if(member_array(arg,ABLOCK_WHITELIST)==-1)
