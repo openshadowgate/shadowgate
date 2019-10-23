92b3aa09e993f3a5359de3a7a734ddda243e14c5
shadowgate <law@shadowgate.org>
2019-10-23 13:17:02 +0000

    autocommit 201910231713

--- cmds/mortal/_wing.c
+++ cmds/mortal/_wing.c
@@ -3,26 +3,10 @@
 #include <magic.h>
 inherit DAEMON;
 
-/* 18:23:14 blazeti      | A cool breeze gives your wings some extra lift as you soar through the air. */
-/* 18:24:15 Historian    | The wind whips by your face as you zoom past the clouds. */
-/* 18:24:18 blazeti      | Spreading your wings wide, you allow the currents in the air to hold you steady. */
-/* 18:26:47 blazeti      | Peering far below, you can see a river winding it's way through a thick forest. */
-/* 18:27:09 blazeti      | The wind is cold as it blows past. */
-/* 18:28:39 blazeti      | You pull your wings in tight, giving your body a twist as you spiral into a dive towards the */
-/*                       | ground. */
-/* 18:29:20 blazeti      | With a forceful heave, you leap into the air and take flight. */
-/* 18:30:09 blazeti      | A flutter of wings takes you into the air. */
-/* 18:31:04 blazeti      | You slowly descend to the ground in a series of languid swoops. */
-/* 18:32:53 <--          | blazeti (blazeti@discord) has left #sha.ooc () */
-/* 21:46:18 -->          | blazeti (blazeti@discord) has joined #sha.ooc */
-/* 21:46:31 Diego_Slavem+| Powerful thrusts of your wings carry you higher. */
-/* 21:47:15 Aiden_Vaser  | LIke a Carnival Row brothel */
-/* 21:47:16 blazeti      | "thrust" just makes that sound like porn */
-/* 21:47:24 Diego_Slavem+| Soaring through the air calm you as you ride the wind currents */
-/* 21:47:42 Diego_Slavem+| thrust uh huh uh huh uh huh */
-/* 21:48:24 Diego_Slavem+| calms */
-/* 21:50:15 Diego_Slavem+| now porn would be...The cold are makes your nipples hard */
-/* 21:50:21 Diego_Slavem+| air */
+string * flyraces = ({"deva"});
+string * flysubraces = ({"fey'ri"});
+string * flyprofiles = ({"druid_bird_999","druid_dragon_999","vampire_bat_999"});
+
 
 string FLIGHT_ROOM = "/std/flying_room.c";
 
