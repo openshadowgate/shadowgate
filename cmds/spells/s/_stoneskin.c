559735edb77fe992327e5b0b4a85b5421a2f1d95
shadowgate <law@shadowgate.org>
2019-10-28 18:01:33 +0000

    autocommit 201910280118

--- cmds/spells/s/_stoneskin.c
+++ cmds/spells/s/_stoneskin.c
@@ -11,7 +11,7 @@ void create() {
     ::create();
     set_spell_name("stoneskin");
     set_spell_level(([ "mage" : 4,"druid" : 5, "monk" : 11]));
-    set_spell_sphere("alteration");
+    set_spell_sphere("abjuration");
     set_syntax("cast CLASS stoneskin on TARGET");
     set_description("This spell causes the target to take less damage from to a certain number of attacks, based on the level of "
 "the caster. It does not work together with iron body.");
@@ -20,24 +20,24 @@ void create() {
     set_target_required(1);
     set_components(([
       "mage" : ([ "granite dust":1, ]),
-    ]));	
+    ]));
 	set_helpful_spell(1);
 }
 
 int preSpell()
-{    
+{
     if(spell_type == "monk") target = caster;
     if(!objectp(caster))
     {
         return 0;
     }
-    if(!objectp(target)) 
+    if(!objectp(target))
     {
         tell_object(caster, "You must specify a target!");
         return 0;
     }
     if(target->query_stoneSkinned() || target->query_property("iron body"))
-    { 
+    {
         tell_object(caster,"That target already has protection of this nature!");
         return 0;
     }
@@ -51,12 +51,12 @@ string query_cast_string()
     return "%^BOLD%^"+caster->QCN+" sprinkles some granite dust over "+target->QCN+" and begins muttering incomprehensibly.";
 }
 
-void spell_effect(int prof) 
+void spell_effect(int prof)
 {
     ::spell_effect();
     if(spell_type == "monk") target = caster;
     if((string)TO->query_spell_type() == "potion") {
-      if(target->query_stoneSkinned() || target->query_property("iron body")){ 
+      if(target->query_stoneSkinned() || target->query_property("iron body")){
         tell_object(caster,"The potion doesn't seem to have any effect. You realise you already have protection of this nature!");
         TO->remove();
         return;
@@ -68,7 +68,7 @@ void spell_effect(int prof)
       tell_object(caster,"%^BOLD%^As the potion warms your stomach, a layer of granite ripples out over your skin.%^RESET%^");
       tell_room(place,"%^BOLD%^A layer of granite ripples out over "+caster->QCN+"'s skin.",caster);
     }
-    else 
+    else
     {
         tell_room(place, caster->QCN+"%^BOLD%^%^WHITE%^ is suddenly surrounded by an impenetrable granite armor!", caster);
         tell_object(caster, "%^BOLD%^%^WHITE%^You are suddenly surrounded by an impenetrable granite armor!%^RESET%^");
@@ -76,7 +76,7 @@ void spell_effect(int prof)
     }
     target->set_property("spelled",({TO}));
     target->set_property("damage resistance",10);
-    target->set_property("iron body",1);    
+    target->set_property("iron body",1);
     addSpellToCaster();
     return;
 }
@@ -95,7 +95,7 @@ void dest_effect() {
       tell_room(environment(target),"%^BOLD%^The armor surrounding "+target->QCN+" loses its form and falls apart.",target);
       tell_object(target,"%^BOLD%^The force protecting you breaks apart, leaving you vulnerable once again.");
       target->set_property("damage resistance",-10);
-      target->set_property("iron body",-1);      
+      target->set_property("iron body",-1);
     }
     ::dest_effect();
     if(objectp(TO)) TO->remove();
