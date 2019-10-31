787ea80d7ac82543202ce240490493c6c90295c0
shadowgate <law@shadowgate.org>
2019-10-31 12:02:53 +0000

    autocommit 201910310212

--- cmds/spells/s/_siphon_magic.c
+++ cmds/spells/s/_siphon_magic.c
@@ -2,6 +2,8 @@
 #include <spell.h>
 inherit SPELL;
 
+object fob,tob;
+
 void create() {
     ::create();
     set_spell_name("siphon magic");
@@ -9,55 +11,91 @@ void create() {
     set_spell_sphere("abjuration");
     set_syntax(
 "cast CLASS siphon magic on OBJECT2 into OBJECT2");
-    set_description("");
+    set_description("With this spell you can transfer enchantment value between objects of the same type.");
     set_verbal_comp();
     set_somatic_comp();
     set_arg_needed();
 	set_helpful_spell(1);
 }
 
-string query_casting_string() {
+string query_casting_string()
+{
     return "%^BOLD%^%^MAGENTA%^"+caster->QCN+" utters a short %^GREEN%^r%^CYAN%^h%^GREEN%^ym%^CYAN%^e %^MAGENTA%^under "+caster->QP+" breath.";
 }
 
-int preSpell() {
-    if(sscanf(arg,"%s into %s")!=2) {
-       tell_object(caster, "You must specify both object you take magic from and object you transfer it to.");
-       return 0;
-   }
-   return 1;
+int preSpell()
+{
+    string sfob, stob;
+    if(sscanf(arg,"%s into %s",sfob,stob)!=2)
+    {
+        tell_object(caster, "You must specify both object you take magic from and object you transfer it to.");
+        return 0;
+    }
+    fob = present(sfob,caster);
+    if (!objectp(fob))
+    {
+        tell_object(caster,"The first object is not present.");
+        return 0;
+    }
+    tob = present(stob,caster);
+    if (!objectp(tob))
+    {
+        tell_object(caster,"The Ssecond object is not present.");
+        return 0;
+    }
+    if(!(fob->is_weapon()||fob->is_armor()))
+    {
+        tell_object(caster,"The first object is neither a weapon nor an armor piece.");
+        return 0;
+    }
+    if(!(tob->is_weapon()||tob->is_armor()))
+    {
+        tell_object(caster,"The second object is neither a weapon nor an armor piece.");
+        return 0;
+    }
+    if(((fob->is_weapon()&&!tob->is_weapon()) ||
+        (!fob->is_weapon()&&tob->is_weapon())) &&
+       ((fob->is_armor()&&!tob->is_armor()) ||
+        (!fob->is_armo()&&tob->is_weapon())))
+    {
+        tell_object(caster,"Both objects must be of the same type.");
+        return 0;
+    }
+    if(fob->query_property("enchantment")<1)
+    {
+        tell_object(caster,"The first object must be enchanted.");
+        return 0;
+    }
+    if(tob->query_property("enchantment")>fob->query_property("enchantment"))
+    {
+        tell_object(caster,"The second object must not have higher enchantment than the first one.");
+        return 0;
+    }
+    if(tob->query_property("enchantment")<0)
+    {
+        tell_object(caster,"The second object must not be cursed.");
+        return 0;
+    }
+    return 1;
 }
 
 void spell_effect(int prof)
 {
-    string sob, sob2;
-    object ob, ob2;
     int enchantment;
 
-    ob = present(arg,caster);
-    if (!objectp(ob))
+    if (!objectp(fob)||!objectp(tob))
     {
-        tell_object(caster,"That object is not present.");
+        tell_object(caster,"That object is no longer present.");
         dest_effect();
         return;
     }
 
-    enchantment = ob->query_property("enchantment");
-    if(enchantment<1)
-    {
-        tell_object(caster,"%^BOLD%^%^MAGENTA%^You cannot unravel magic of this object further.%^RESET%^");
-        dest_effect();
-        return;
-    }
+    enchantment = fob->query_property("enchantment");
 
-    ob->remove_property("enchantment");
-    ob->set_property("enchantment",--enchantment);
-    tell_object(caster,"%^BOLD%^%^MAGENTA%^As you spell syllables enchantment on "+ob->query_short()+"%^BOLD%^%^MAGENTA%^ becomes weaker.%^RESET%^");
-
+    fob->remove_property("enchantment");
+    tob->remove_property("enchantment");
+    tob->set_property("enchantment",enchantment);
+    tell_object(caster,"%^BOLD%^%^MAGENTA%^As you spell syllables power on "+fob->query_short()+"%^BOLD%^%^MAGENTA%^ transfers to "+tob->query_short()+"%^BOLD%^%^MAGENTA%^.%^RESET%^");
+    tell_room(caster,"%^BOLD%^%^MAGENTA%^You see soft glow flowing from "+fob->query_short()+"%^BOLD%^%^MAGENTA%^ into "+tob->query_short()+"%^BOLD%^%^MAGENTA%^.%^RESET%^",({caster}));
     dest_effect();
-}
-
-void dest_effect() {
-    ::dest_effect();
-    if(objectp(TO)) TO->remove();
 }
