e4eb2ab3cc14d9a9d4893210f9c864c5046e1c7f
shadowgate <law@shadowgate.org>
2019-11-02 21:46:26 +0000

    autocommit 201911024621

--- cmds/spells/s/_shadowform.c
+++ cmds/spells/s/_shadowform.c
@@ -4,6 +4,7 @@
 inherit SPELL;
 
 int bonus;
+int SFDELAY = 600;
 
 void create()
 {
@@ -14,7 +15,7 @@ void create()
     set_syntax("cast CLASS shadowform");
     set_damage_desc("a chance to reduce damage to 1/5");
     set_save("will");
-    set_description("You replace the target’s body with mystic shadow material drawn from the Shadow Plane, rendering the target’s physical form only quasi-real. Whenever a foe tries to directly attack the target of the spell (for instance, with a weapon or a targeted spell), that foe must attempt a Will save to disbelieve. If successful, the opponent can attack the target normally and is unaffected by shadowform for 1 round. If the foe fails, the target takes only one-fifth the normal amount of damage from the foe’s successful attack.");
+    set_description("You replace own body with mystic shadow material drawn from the Shadow Plane, rendering the own physical form only quasi-real. Whenever a foe tries to directly attack you of the spell (for instance, with a weapon or a targeted spell), that foe must attempt a Will save to disbelieve. If successful, the opponent can attack you normally and is unaffected by shadowform for 1 round. If the foe fails, you take only one-fifth the normal amount of damage from the foe’s successful attack.");
     set_arg_needed();
 	set_helpful_spell(1);
 }
@@ -22,6 +23,11 @@ void create()
 int preSpell()
 {
     target = caster;
+    if(target->query_property("shadowform_spell_delay")>time())
+    {
+        tell_object(caster,"You can't infuse self so soon");
+        return 0;
+    }
     if(target->query_property("shadowform"))
     {
         tell_object(caster,"You are already under the influence of similar effect");
@@ -42,6 +48,8 @@ spell_effect()
     bonus=clevel/2+1;
     target->set_property("spelled", ({TO}) );
     target->set_property("shadowform",clevel);
+    target->remove_property("shadowform_spell_delay");
+    target->set_property("shadowform_spell_delay",time()+SFDELAY);
     target->set_property("added short",({"%^RESET%^%^BLUE%^ (shadow)%^RESET%^"}));
     addSpellToCaster();
     call_out("dest_effect",ROUND_LENGTH*(clevel/6+1));
