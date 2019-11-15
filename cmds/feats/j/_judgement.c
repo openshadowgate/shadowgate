6800ca5dd97c542577651c5431e98ce96077dba2
shadowgate <law@shadowgate.org>
2019-11-15 22:17:02 +0000

    autocommit 201911151722

--- cmds/feats/j/_judgement.c
+++ cmds/feats/j/_judgement.c
@@ -3,25 +3,11 @@
 #include <magic.h>
 inherit FEAT;
 
-string * JUDGEMENT_TYPES = ({
-        "destruction",
-            "healing",
-            "justice",
-            "piercing",
-            "protection",
-            "purity",
-            "resiliency",
-            "resistance",
-//            "smiting"
-});
-
-string * active_judgements = ({});
-
 void create()
 {
     ::create();
     feat_type("instant");
-    feat_category("DivineSpellcraft");
+    feat_category("Inquisition");
     feat_name("rally");
     feat_syntax("judgment FIRST [SECOND [THIRD]]");
     feat_prereq("Heh");
@@ -45,107 +31,24 @@ int prerequisites(object ob)
     return ::prerequisites(ob);
 }
 
-int cmd_judgment(string args)
+int cmd_judgement(string args)
 {
-    string first, second, third;
+    int i=3;
+    string j;
+    string *jtoactivate=({}),*argss;
+    string *active_judgements = ({});
+
     if(!objectp(TP))
         return 0;
-    if(!sizeof(TP->query_attackers()))
-    {
-        tell_object(TP,"You can't use judgement outside of combat.");
-        return 1;
-    }
-    if(TP->query_property("active_judgements"))
-    {
-        string * judgements = TP->query_property("active_judgements");
-        active_judgements(judgements,-1);
-    }
 
-    sscanf(args,"%s %s %s",first, second, third);
-    clevel = TP->query_guild_level("inquisitor");
-    if(first!="")
-        if(member_array(first,JUDGEMENT_TYPES)!=-1)
-        {
-            active_judgements+=({first});
-        }
-        else
-        {
-            tell_object(TP,"You can't use judgement outside of combat.");
-            return 1;
-        }
 
     return 1;
 }
 
-void activate_judgements(string * judgements,int direction)
-{
-    string j;
-    foreach(j in judgements)
-    {
-        if(member_array(j,JUDGEMENT_TYPES)!=-1)
-            call_other(TO,"judgement_"+j,direction);
-    }
-}
-
-void judgement_destruction(int direction)
-{
-    int bonus;
-    bonus = clevel / 3 + 1;
-    caster->add_damage_bonus(bonus*direction);
-}
-
-void judgement_healing(int direction)
-{
-    int bonus;
-    bonus = clevel/18+1;
-    caster->set_property("fast_healing",bonus*direction);
-}
-
-void judgement_justice(int direction)
-{
-    int bonus;
-    bonus = clevel / 5 + 1;
-    caster->add_attack_bonus(bonus*direction);
-}
-
-void judgement_piercing(int direction)
-{
-    caster->set_property("spell penetration",(clevel+10)*direction);
-    caster->set_property("empowered",(clevel/12+1)*direction);
-}
-
-void judgement_protection(int direction)
-{
-    int bonus;
-    bonus = clevel / 5 + 1;
-    caster->add_ac_bonus(bonus*direction);
-}
-
-void judgement_purity(int direction)
-{
-    int bonus;
-    bonus = clevel / 5 + 1;
-    caster->add_saving_bonus("all",bonus);
-}
-
-void judgement_resiliency(int direction)
-{
-    int bonus;
-    bonus = clevel / 5 + 1;
-    caster->set_property("spell damage resistance",bonus*direction);
-}
-
-void check()
-{
-    if(!objectp(caster))
-    {
-        dest_effect();
-        return;
-    }
-    call_out("check",ROUND_LENGTH);
-}
 
 void dest_effect()
 {
+    ::dest_effect();
+    remove_feat(TO);
     return;
 }
---
+++ cmds/feats/obj/judgement.c
@@ -0,0 +1,111 @@
+#include <std.h>
+
+inherit OBJECT;
+
+string * JUDGEMENT_TYPES = ({
+        "destruction",
+            "healing",
+            "justice",
+            "piercing",
+            "protection",
+            "purity",
+            "resiliency",
+            "resistance",
+//            "smiting"
+});
+
+string * active_judgements = ({});
+object caster;
+int clevel;
+
+void create()
+{
+    ::create();
+    set_name("judgement_obj");
+    set("id", ({ "judgement_obj" }) );
+    set("short", "");
+    set("long", "");
+    set_weight(0);
+}
+
+void setup_judgement(object mycaster, int myclevel)
+{
+    caster = mycaster;
+    clevel = myclevel;
+}
+
+void apply_judgements(string * judgements,int direction)
+{
+    string j;
+    foreach(j in judgements)
+    {
+        if(member_array(j,JUDGEMENT_TYPES)!=-1)
+        {
+            call_other(TO,"judgement_"+j,direction);
+        }
+    }
+}
+
+void judgement_destruction(int direction)
+{
+    int bonus;
+    bonus = clevel / 3 + 1;
+    caster->add_damage_bonus(bonus*direction);
+}
+
+void judgement_healing(int direction)
+{
+    int bonus;
+    bonus = clevel/18+1;
+    caster->set_property("fast_healing",bonus*direction);
+}
+
+void judgement_justice(int direction)
+{
+    int bonus;
+    bonus = clevel / 5 + 1;
+    caster->add_attack_bonus(bonus*direction);
+}
+
+void judgement_piercing(int direction)
+{
+    caster->set_property("spell penetration",(clevel+10)*direction);
+    caster->set_property("empowered",(clevel/12+1)*direction);
+}
+
+void judgement_protection(int direction)
+{
+    int bonus;
+    bonus = clevel / 5 + 1;
+    caster->add_ac_bonus(bonus*direction);
+}
+
+void judgement_purity(int direction)
+{
+    int bonus;
+    bonus = clevel / 5 + 1;
+    caster->add_saving_bonus("all",bonus);
+}
+
+void judgement_resiliency(int direction)
+{
+    int bonus;
+    bonus = clevel / 5 + 1;
+    caster->set_property("spell damage resistance",bonus*direction);
+}
+
+void check()
+{
+    if(!objectp(caster))
+    {
+        dest_effect();
+        return;
+    }
+    if(!sizeof(caster->query_attackers()))
+    {
+        tell_object(caster,"%^BOLD%^%^CYAN%^As the battle comes to an end your arcane zeal recedes.%^RESET%^");
+        dest_effect();
+        return;
+    }
+    call_out("check",ROUND_LENGTH);
+}
