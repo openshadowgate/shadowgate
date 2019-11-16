cba6d47851782de857034a4beca02d30c3954810
shadowgate <law@shadowgate.org>
2019-11-16 00:11:44 +0000

    autocommit 201911161100

--- cmds/feats/j/_judgement.c
+++ cmds/feats/j/_judgement.c
@@ -13,7 +13,24 @@ void create()
     feat_name("rally");
     feat_syntax("judgment FIRST [SECOND [THIRD]]");
     feat_prereq("Inquisitor L1");
-    feat_desc("Inquisitors have unusual");
+    feat_desc("Inquisitors have an unusual affinity to pronounce a judgement on their foes, to strengthen themselves with a channeled powers. At the beginning they can choose just one power to pronounce, but as inquisitor grows in power he learns to control up to three such powers. Changing the power is a free action, but any powers an inquisitor has will retreat after the combat and can not be summoned unless already in the combat.
+
+List of judgements to choose from:
+
+%^BOLD%^Destruction:%^RESET%^ The inquisitor is filled with divine wrath, giving them damage bonus point per every three levels.
+
+%^BOLD%^Healing:%^RESET%^ The inquisitor is surrounded by a healing light, gaining fast healing point per every eighteen levels.
+
+%^BOLD%^Justice:%^RESET%^ This judgment spurs the inquisitor to seek justice, gaining a sacred bonus point to attack per every three levels.
+
+%^BOLD%^Piercing:%^RESET%^ This judgment gives the inquisitor great focus and makes his spells more potent. This grants a level and ten points worth of spell penetration and a caster level point for every twelve levels.
+
+%^BOLD%^Protection:%^RESET%^ The inquisitor is surrounded by a protective aura, granting a sacred armor class point per every five levels.
+
+%^BOLD%^Purity:%^RESET%^ The inquisitor is protected from the vile taint of her foes, gaining a sacred bonus to all saves, worth of a point per every five levels.
+
+%^BOLD%^Resiliency:%^RESET%^ This judgment makes the inquisitor resistant to harm, granting them one point of spell damage resistance per five levels.
+");
 //    set_required_for(({"second judgement","third judgement"}));
     set_target_required(0);
     allow_blind(1);
@@ -52,9 +69,9 @@ int cmd_judgement(string args)
     JUDGEMENT_TYPES = "/cmds/feats/obj/judgement"->query_judgement_types();
 
 
-    if(FEAT_D->usable_feat(TP,"third judgement"))
+    if(FEATS_D->usable_feat(TP,"third judgement"))
         i = 3;
-    else if(FEAT_D->usable_feat(TP,"second judgement"))
+    else if(FEATS_D->usable_feat(TP,"second judgement"))
         i = 2;
     else
         i = 1;
