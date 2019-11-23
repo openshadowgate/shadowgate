980b2ac1a76d289b4a390b65a15bdb511856cb42
shadowgate <law@shadowgate.org>
2019-11-23 03:02:09 +0000

    autocommit 201911230203

---
+++ cmds/spells/d/_detect_the_faithful.c
@@ -0,0 +1,36 @@
+#include <std.h>
+#include <daemons.h>
+#include <spell.h>
+
+inherit SPELL;
+
+void create()
+{
+    ::create();
+    set_spell_name("detect the faithful");
+    set_spell_level(([ "inquisitor" : 1, "cleric" : 1, "druid" : 1, "inquisitor" : 1, "paladin" : 1, "ranger" : 1 ]));
+    set_spell_sphere("divination");
+    set_syntax("cast CLASS detect the faithful");
+    set_damage_desc("reveals presence of members of your faith");
+    set_description("You can detect presence of other worshipers of your deity.");
+    set_verbal_comp();
+    set_somatic_comp();
+}
+
+void spell_effect(int prof)
+{
+    object *attackers;
+    string mygod = caster->query_diety();
+
+    attackers = all_living(place);
+    attackers = filter_array(attackers, "is_non_immortal",FILTERS_D);
+    attackers = filter_array(attackers,(:$1->query_diety()==$2:),mygod);
+
+    if(sizeof(attackers))
+        tell_object(caster,"%^RED%^There are faithful present.%^RESET%^");
+    else
+        tell_object(caster,"%^RED%^No faithful are present present.%^RESET%^");
+
+    spell_successful();
+    dest_effect();
+}
