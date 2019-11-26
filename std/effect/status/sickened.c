c06dd359c28b0e4b69058797277e0cd2848ae0fc
shadowgate <law@shadowgate.org>
2019-11-26 14:17:01 +0000

    autocommit 201911261714

--- std/effect/status/sickened.c
+++ std/effect/status/sickened.c
@@ -4,6 +4,8 @@
 
 inherit STATUS;
 
+int power;
+
 void create()
 {
     ::create();
@@ -17,11 +19,13 @@ int status_effect()
     tell_object(target,"%^BLUE%^You feel sickened.%^RESET%^");
     tell_room(ENV(target),"%^BLUE%^"+target->QCN+" looks sickened.", target);
 
+    power=target->query_level()/12+1;
+
     for(i=0;i<sizeof(CORE_SKILLS);i++)
-        target->add_skill_bonus(CORE_SKILLS[i],-2);
-    target->add_attack_bonus(-2);
-    target->add_damage_bonus(-2);
-    target->add_saving_bonus("all",-2);
+        target->add_skill_bonus(CORE_SKILLS[i],-power);
+    target->add_attack_bonus(-power);
+    target->add_damage_bonus(-power);
+    target->add_saving_bonus("all",-power);
 
     call_out("dest_effect",ROUND_LENGTH*clevel);
     return 1;
@@ -35,10 +39,10 @@ int dest_effect()
         tell_object(target,"%^BLUE%^You no longer feel sickened.%^RESET%^");
         tell_room(ENV(target),"%^BLUE%^"+target->QCN+" no longer looks sickened.", target);
         for(i=0;i<sizeof(CORE_SKILLS);i++)
-            target->add_skill_bonus(CORE_SKILLS[i],2);
-        target->add_attack_bonus(2);
-        target->add_damage_bonus(2);
-        target->add_saving_bonus("all",2);
+            target->add_skill_bonus(CORE_SKILLS[i],power);
+        target->add_attack_bonus(power);
+        target->add_damage_bonus(power);
+        target->add_saving_bonus("all",power);
     }
     ::dest_effect();
     return 1;
