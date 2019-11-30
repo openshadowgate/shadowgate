503282576bfb9457b9f55b52c639b81a8496f5aa
shadowgate <law@shadowgate.org>
2019-11-30 12:20:57 +0000

    autocommit 201911302012

---
+++ cmds/spells/k/_keen_edge.c
@@ -0,0 +1,60 @@
+#include <std.h>
+inherit SPELL;
+
+int trackera, trackerb;
+
+void create() {
+    ::create();
+    set_spell_name("keen edge");
+    set_spell_level(([ "inquisitor" : 3, "mage" : 3 ]));
+    set_spell_sphere("alteration");
+    set_syntax("cast CLASS keen edge");
+    set_description("This spell makes a weapon magically keen, improving its ability to deal telling blows. This transmutation increases the threat range of the weapon and works the same way as lethal strikes feat does.
+
+%^BOLD%^%^RED%^See also:%^RESET%^ lethal strikes *feats");
+    set_verbal_comp();
+    set_somatic_comp();
+    set_helpful_spell(1);
+}
+
+string query_cast_string()
+{
+	tell_object(caster,"%^RED%^They lyrics to an ancient spell flood into your mind as you begin to hum.");
+	tell_room(place,"%^RED%^"+caster->QCN+" begins to hum.",caster);
+	return "display";
+}
+
+int preSpell()
+{
+    if (caster->query_property("keen_edge"))
+	{
+      	tell_object(caster,"You already have this spell active.");
+        return 0;
+    }
+    return 1;
+}
+
+void spell_effect(int prof) {
+    tell_object(caster, "%^RED%^You feel a greater understanding of combat filling your mind.");
+    caster->set_property("keen_edge",1);
+    if(member_array("martial weapon proficiency",(string*)caster->query_temporary_feats()) == -1) {
+      caster->add_temporary_feat("lethal strikes");
+      trackera = 1;
+    }
+    caster->set_property("spelled", ({TO}));
+    addSpellToCaster();
+}
+
+void dest_effect()
+{
+    if (objectp(caster))
+	{
+		caster->remove_property("keen_edge");
+      	tell_object(caster,"%^RED%^The knowledge of weapon skill leaves you.");
+      	if(trackera == 1)
+            caster->remove_temporary_feat("lethal strikes");
+    }
+    ::dest_effect();
+    if(objectp(TO))
+        TO->remove();
+}
