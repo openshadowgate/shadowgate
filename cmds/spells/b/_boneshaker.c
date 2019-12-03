da62fd923319c21c9828e3aa87ec21aca2c9d033
shadowgate <law@shadowgate.org>
2019-12-03 20:38:00 +0000

    autocommit 201912033820

--- cmds/spells/b/_boneshaker.c
+++ cmds/spells/b/_boneshaker.c
@@ -9,8 +9,8 @@ void create()
     set_spell_level(([ "mage" : 2, "cleric" : 2, "inquisitor":2 ]));
     set_spell_sphere("necromancy");
     set_syntax("cast CLASS boneshaker on TARGET");
-    set_damage_desc("untyped to living, half to undead");
-    set_description("Using a mental image of the target's external shell of skeleton, you shake it, causing target to undergo severe pain. This spell won't work on undead.");
+    set_damage_desc("untyped");
+    set_description("Using a mental image of the target's external shell of skeleton, you shake it, causing target to undergo severe pain.");
     set_target_required(1);
 }
 
@@ -22,19 +22,10 @@ string query_cast_string()
 void spell_effect(int prof)
 {
     spell_successful();
-    if(target->is_undead())
-        sdamage/=2;
     tell_object(caster,"%^BOLD%^%^WHITE%^You will crushing of "+target->QCN+"'s internals!");
     tell_room(place,"%^BOLD%^%^WHITE%^"+target->QCN+" staggers in pain!", ({caster, target}) );
     tell_object(target,"%^BOLD%^%^WHITE%^Your bones ache of pain as the spell hits you!");
     damage_targ(target, target->query_target_limb(), sdamage,"untyped");
     spell_successful();
     TO->dest_effect();
-}
-
-void dest_effect()
-{
-    ::dest_effect();
-    if(objectp(TO))
-        TO->remove();
 }
--- cmds/spells/b/_boneshatter.c
+++ cmds/spells/b/_boneshatter.c
@@ -11,7 +11,7 @@ void create()
     set_spell_level(([ "mage" : 4, "cleric" : 4 ]));
     set_spell_sphere("necromancy");
     set_syntax("cast CLASS boneshatter on TARGET");
-    set_damage_desc("untyped on living, half on undead, -4 or -2 on save to str and dex");
+    set_damage_desc("untyped, exhausted or fatigued on save");
     set_description("With mere will you splinter you enemy's skeleton or external shell. The enemy fights the change and if they fail, they become exhausted, taking damage to their strength and dexterity. If they Successfully overcome the pain, they instead become fatigued for a short while. This spell will work only on living beings, and will cause stat damage just once per target.");
     set_save("fort");
     set_target_required(1);
@@ -26,41 +26,16 @@ void spell_effect(int prof)
 {
     int duration;
     spell_successful();
-    if(target->is_undead())
-        sdamage/=2;
     tell_object(caster,"%^BOLD%^%^WHITE%^You will crushing of "+target->QCN+"'s internals!");
     tell_room(place,"%^BOLD%^%^WHITE%^"+target->QCN+" staggers as you hear a rushing sound!", ({caster, target}) );
     tell_object(target,"%^BOLD%^%^WHITE%^Your bones ache of pain as the spell hits you!");
     damage_targ(target, target->query_target_limb(), sdamage,"untyped");
-    if(!target->query_property("boneshattered"))
-    {
-        bonus = -4;
-        if(do_save(target,0))
-            bonus = -2;
+    duration = clevel/4+1;
 
-        tell_object(target,"%^BOLD%^%^WHITE%^You feel fatigued...");
-        target->add_stat_bonus("strength",bonus);
-        target->add_stat_bonus("dexterity",bonus);
-        target->set_property("boneshattered");
-        duration = (ROUND_LENGTH) * clevel;
-        call_out("dest_effect",duration);
-    }
+    if(do_save(target,0))
+        "/std/effect/status/fatigued"->apply_effect(target,duration);
     else
-        TO->dest_effect();
+        "/std/effect/status/exhausted"->apply_effect(target,duration);
+
     spell_successful();
-}
-
-void dest_effect()
-{
-    ::dest_effect();
-
-    if(objectp(target))
-    {
-        tell_object(target,"%^BOLD%^%^WHITE%^Your fatigue recedes, you feel power and finesse returning.");
-        target->add_stat_bonus("strength",-bonus);
-        target->add_stat_bonus("dexterity",-bonus);
-        target->remove_property("boneshattered");
-    }
-    if(objectp(TO))
-        TO->remove();
 }
