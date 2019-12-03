6b9474e8d5fdce8b09a9eb3ffda4860000ae57f4
shadowgate <law@shadowgate.org>
2019-12-03 20:04:09 +0000

    autocommit 201912030420

--- cmds/feats/b/_blindfight.c
+++ cmds/feats/b/_blindfight.c
@@ -8,7 +8,7 @@ void create() {
     feat_type("permanent");
     feat_category("GeneralCombat");
     feat_name("blindfight");
-    feat_desc("This feat reflects increased aptitude in handling oneself when robbed of sight, both in combat accuracy and in avoiding obstacles while walking. In addition, it will allow to retain dexterity bonus to armor class when blinded. It does not, however, allow the character to use spells or directly targetted abilities on an unseen foe.
+    feat_desc("This feat reflects increased aptitude in handling oneself when robbed of sight, both in combat accuracy and in avoiding obstacles while walking. It will allow to retain dexterity bonus to armor class when blinded and will allow to use targeted feats. It does not, however, allow the character to use spells or directly targetted abilities on an unseen foe.
 
 %^BOLD%^See also:%^RESET%^ status effects");
     permanent(1);
