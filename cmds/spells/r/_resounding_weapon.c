25e2d013b709104030ad3fb088c6d2a0385d61f9
shadowgate <law@shadowgate.org>
2019-12-04 22:33:01 +0000

    autocommit 201912043322

--- cmds/spells/r/_resounding_weapon.c
+++ cmds/spells/r/_resounding_weapon.c
@@ -63,7 +63,7 @@ void spell_effect()
         tell_object(caster, "%^CYAN%^Your "+targ->query_name()+" begins vibrating %^CYAN%^as you move your hand along it.");
         tell_room(place, "%^CYAN%^"+targ->query_name()+"%^CYAN%^ begins vibrating as %^GREEN%^"+caster->QCN+" %^CYAN%^moves %^GREEN%^"+caster->QP+" %^CYAN%^hand along it.",({caster}));
         targ->set_property("added short",({ "%^RESET%^%^CYAN%^ %^BOLD%^{%^RESET%^%^CYAN%^vibrating%^BOLD%^}%^RESET%^" }));
-        call_out("dest_effect",ROUND_LENGTH*6*clevel);
+//        call_out("dest_effect",ROUND_LENGTH*6*clevel);
         caster->set_property("spelled", ({TO}) );
         addSpellToCaster();
     }
