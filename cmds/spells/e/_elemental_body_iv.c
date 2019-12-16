bf2c1287e09e517bb8241484747df2c2886ccfdd
shadowgate <law@shadowgate.org>
2019-12-16 05:33:45 +0000

    autocommit 201912163305

--- cmds/spells/e/_elemental_body_iv.c
+++ cmds/spells/e/_elemental_body_iv.c
@@ -40,7 +40,7 @@ void effect(int direction)
 void create()
 {
     ::create();
-    set_spell_name("elemental body iii");
+    set_spell_name("elemental body iv");
     set_spell_level(([ "mage" : 7]));
     set_description("When you cast this spell, you infuse yourself with the power of the element. Infusions give the next effects as follows:
 
