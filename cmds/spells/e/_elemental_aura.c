05bb681064a6e9a31091d490a5b6a25928ad8166
shadowgate <law@shadowgate.org>
2019-12-17 00:33:01 +0000

    autocommit 201912173300

--- cmds/spells/e/_elemental_aura.c
+++ cmds/spells/e/_elemental_aura.c
@@ -6,10 +6,10 @@ int timer,flag,stage,toggle,counter;
 string element;
 mapping shortmap, colormap;
 shortmap = ([
-                "fire":"%^BOLD%^%^BLACK%^(%^RESET%^%^RED%^bathed in flames%^BOLD%^%^BLACK%^)%^RESET%^",
-                "cold":"%^BOLD%^%^BLACK%^(%^RESET%^%^CYAN%^in an icy haze%^BOLD%^%^BLACK%^)%^RESET%^",
-                "electricity":"%^BOLD%^%^BLACK%^(%^RESET%^%^ORANGE%^engulfed in stormbolts%^BOLD%^%^BLACK%^)%^RESET%^",
-                "acid":"%^BOLD%^%^BLACK%^(%^RESET%^%^GREEN%^in acid fog%^BOLD%^%^BLACK%^)%^RESET%^",
+                "fire":" %^BOLD%^%^BLACK%^(%^RESET%^%^RED%^bathed in flames%^BOLD%^%^BLACK%^)%^RESET%^",
+                "cold":" %^BOLD%^%^BLACK%^(%^RESET%^%^CYAN%^in an icy haze%^BOLD%^%^BLACK%^)%^RESET%^",
+                "electricity":" %^BOLD%^%^BLACK%^(%^RESET%^%^ORANGE%^engulfed in stormbolts%^BOLD%^%^BLACK%^)%^RESET%^",
+                "acid":" %^BOLD%^%^BLACK%^(%^RESET%^%^GREEN%^in acid fog%^BOLD%^%^BLACK%^)%^RESET%^",
                 ]);
 colormap = ([
                 "fire":"%^BOLD%^%^RED%^",
