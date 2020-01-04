e6aa7358b6d38c560ceaa27f9589b55c6305a1b3
shadowgate <law@shadowgate.org>
2020-01-04 20:12:46 +0000

    autocommit 202001041220

---
+++ cmds/feats/n/_negative_energy_conduit.c
@@ -0,0 +1,56 @@
+#include <std.h>
+#include <daemons.h>
+
+inherit FEAT;
+
+void create()
+{
+    ::create();
+    feat_type("permanent");
+    feat_category("Gravecaller");
+    feat_name("negative energy conduit");
+    feat_prereq("Gravecaller L4");
+    feat_desc("With this feat the gravecaller gains extended powers when channeling necromancy spells. Although inferior to power of archmage necromancer, this knowledge bears no penalties.");
+    permanent(1);
+}
+
+int allow_shifted() { return 0; }
+
+int prerequisites(object ob)
+{
+    if(!objectp(ob)) { return 0; }
+
+    if((int)ob->query_class_level("gravecaller") < 4)
+    {
+        dest_effect();
+        return 0;
+    }
+    return ::prerequisites(ob);
+}
+
+void execute_feat()
+{
+    ::execute_feat();
+    dest_effect();
+}
+
+void permanent_effects(object ob)
+{
+    ::permanent_effects(ob);
+    dest_effect();
+    return;
+}
+
+void reverse_permanent_effects(object ob)
+{
+    ::reverse_permanent_effects(ob);
+    dest_effect();
+    return;
+}
+
+void dest_effect()
+{
+    ::dest_effect();
+    remove_feat(TO);
+    return;
+}
--- std/magic/spell.c
+++ std/magic/spell.c
@@ -1005,7 +1005,7 @@ void wizard_interface(object user, string type, string targ)
     if(!(FEATS_D->usable_feat(caster,"supreme healer") && (member_array(spell_name,supreme_healer_spells) != -1)) &&
        !(FEATS_D->usable_feat(caster,"natures gift") && (member_array(spell_name,natures_gift_spells) != -1)) &&
        !(FEATS_D->usable_feat(caster,"raging healer") && (member_array(spell_name,raging_healer_spells) != -1) && caster->query_property("raged")) &&
-       !(FEATS_D->usable_feat(caster,"negative energy conduit" && casting_level < 6 && spell_sphere == "necromancy")))
+       !(FEATS_D->usable_feat(caster,"inspired necromancy" && casting_level < 6 && spell_sphere == "necromancy")))
     {
         if(!caster->check_memorized(spell_type,improv))
         {
@@ -1849,7 +1849,7 @@ void define_clevel()
         }
 
         if(caster->is_class("gravecaller"))
-            if(FEATS_D->usable_feat(caster, "inspired necromancy"))
+            if(FEATS_D->usable_feat(caster, "negative energy conduit"))
                 if(spell_sphere=="necromancy")
                     clevel += 4;
 
