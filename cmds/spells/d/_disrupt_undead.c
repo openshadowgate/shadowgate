262442a92f368f726b2c8e9ad0990be6d4244221
shadowgate <law@shadowgate.org>
2020-01-13 13:33:02 +0000

    autocommit 202001133313

--- cmds/spells/d/_disrupt_undead.c
+++ cmds/spells/d/_disrupt_undead.c
@@ -8,6 +8,7 @@ void create() {
     set_spell_name("disrupt undead");
     set_spell_level(([ "mage" : 1,"inquisitor":1 ]));
     set_spell_sphere("necromancy");
+    set_affixed_spell_level(5);
     set_damage_desc("positive energy on undead");
     set_syntax("cast CLASS disrupt undead on TARGET");
     set_description("With this spell caster directs a ray of positive energy that severely harms undead.");
@@ -19,20 +20,18 @@ void create() {
 spell_effect(int prof) {
 
     if((string)target->query_race() != "undead" && !target->query_property("undead"))
+    {
         tell_object(caster,"%^YELLOW%^As your target is not undead, the spell fizzles harmlessly.");
+        dest_effect();
+        return;
+    }
 
     spell_successful();
     if (interactive(caster)) {
         tell_object(caster,"%^YELLOW%^You chant sonorously and direct a ray of light at "+target->QCN+"!");
         tell_room(place,"%^YELLOW%^"+caster->QCN+" chants sonorously and directs a ray of light at "+target->QCN+"!", ({caster}) );
     }
-    define_base_damage(5);
     tell_object(place,"%^YELLOW%^A ray of light scorches "+target->QP+"!");
     damage_targ(target, target->query_target_limb(), sdamage,"positive energy");
     TO->dest_effect();
-}
-
-void dest_effect() {
-    ::dest_effect();
-    if(objectp(TO)) TO->remove();
 }
