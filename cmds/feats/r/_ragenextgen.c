53cd3105c2c305dbf7e8bc4648cbb83434aee2e4
shadowgate <law@shadowgate.org>
2020-01-21 18:33:01 +0000

    autocommit 202001213318

--- cmds/feats/r/_ragenextgen.c
+++ cmds/feats/r/_ragenextgen.c
@@ -3,6 +3,10 @@
 #include <magic.h>
 inherit FEAT;
 
+static string rage_class;
+
+object *exclude = ({});
+
 void create()
 {
     ::create();
@@ -21,7 +25,7 @@ int allow_shifted() { return 1; }
 int prerequisites(object ob)
 {
     if(!objectp(ob)) return 0;
-    if(ob->query_name()!="eekclone")
+    if(ob->query_name()!="ornulf")
     {
         dest_effect();
         return 0;
@@ -29,13 +33,106 @@ int prerequisites(object ob)
     return ::prerequisites(ob);
 }
 
-int cmd_ragenextgen(string args)
+int cmd_lifeless_shroud(string str)
 {
-    //Blah
+    object feat;
+    if(!objectp(TP)) { return 0; }
+    feat = new(base_name(TO));
+    feat->setup_feat(TP,str);
+    return 1;
+}
+
+string cm(string str)
+{
+    return CRAYON_D->color_string(str,"red and black");
+}
+
+void execute_feat()
+{
+    object obj;
+
+    if (!objectp(caster)) {
+        dest_effect();
+        return;
+    }
+
+    if (FEATS_D->is_active(caster, "rage")) {
+        obj = query_active_feat("rage");
+        tell_object(caster, cm("You struggle to catch your breath as you try to yank yourself away from your murderous reverie."));
+        obj->dest_effect();
+        dest_effect();
+        return;
+    }
+
+    if (caster->query_property("raged")) {
+        tell_object(caster, "%^RED%^You are already raging.%^RESET%^");
+        dest_effect();
+        break;
+    }
+    if (caster->query_property("using instant feat")) {
+        tell_object(caster, "%^BOLD%^You are already in the middle of using a feat.%^RESET%^");
+        dest_effect();
+        break;
+    }
+    if (caster->query_casting()) {
+        tell_object(caster, "%^BOLD%^You can't rage while you're casting a spell.%^RESET%^");
+        dest_effect();
+        break;
+    }
+
+    tell_object(caster, cm("The world takes on a tinge of red as you are overcome by an insatiable desire to see your enemies rendered into a bloody pulp."));
+    tell_room(place, "%^RED%^" + caster->QCN + " growls fiercely as " + caster->QS + " transforms into a homicidal maniac.%^RESET%^", caster);
+
+    caster->set_property("active_feats",({TO}));
+
+    caster->set_property("using instant feat",1);
+
+    call_out("enable_rage",ROUND_LENGTH);
+
+    ::execute_feat();
+    return;
+}
+
+void enable_rage()
+{
+    caster->remove_property_value("added short",({"%^RESET%^%^BOLD%^%^RED%^ (%^RESET%^%^RED%^enraged%^BOLD%^)%^RESET%^"}));
+    caster->set_property("added short",({"%^RESET%^%^BOLD%^%^RED%^ (%^RESET%^%^RED%^enraged%^BOLD%^)%^RESET%^"}));
+    caster->set_property("raged",1);
+}
+
+void execute_attack()
+{
+    object *attackers = ({ });
+    object *allies = ({ });
+    int i;
+
+    if (!objectp(caster)) {
+        dest_effect();
+        return;
+    }
+    if (caster->query_ghost() || caster->query_unconscious()) {
+        dest_effect();
+        return;
+    }
+
+    place = environment(caster);
+    attackers = caster->query_attackers();
+
+    if (objectp(place)) {
+        place->addObjectToCombatCycle(TO, 1);
+    } else {
+        dest_effect();
+    }
 }
 
 void dest_effect()
 {
+    if(objectp(caster))
+    {
+        tell_object(caster,cm("Your rage ends and you."));
+        caster->remove_property_value("active_feats",({TO}));
+        caster->remove_property_value("added short",({"%^RESET%^%^BOLD%^%^RED%^ (%^RESET%^%^RED%^enraged%^BOLD%^)%^RESET%^"}));
+    }
     ::dest_effect();
     remove_feat(TO);
     return;
---
+++ cmds/spells/h/_holy_aura.c
@@ -0,0 +1,116 @@
+#include <priest.h>
+#include <magic.h>
+#include <spell.h>
+
+inherit SPELL;
+
+int timer,flag,stage,toggle,counter;
+
+void create()
+{
+    ::create();
+    set_spell_name("holy aura");
+    set_spell_level(([ "mage" : 8, "cleric" : 8 ]));
+    set_spell_sphere("abjuration");
+    set_syntax("cast CLASS holy aura");
+    set_damage_desc("untyped, 4 AC, 4 to all saves");
+    set_description("You are surrounded by an aura of good that will harm all your opponents and will slightly protect you. This is nimbus-family spell that won't work with other nimbuses.");
+    set_property("magic",1);
+    traveling_aoe_spell(1);
+}
+
+int preSpell()
+{
+    if(caster->query_property("nimbus"))
+    {
+        tell_object(caster,"You are still affected by numbus.");
+        return 0;
+    }
+    return 1;
+}
+
+
+void spell_effect(int prof)
+{
+    int duration;
+    duration = (ROUND_LENGTH * 10) * clevel;
+
+    tell_object(caster,"%^BOLD%^%^MAGENTA%^You feel powers of light warding you from the evil!");
+    tell_room(place,"%^BOLD%^%^MAGENTA%^"+caster->QCN+" is suddenly surrounded by halo of light!",caster);
+
+    caster->set_property("spelled", ({TO}));
+    caster->set_property("nimbus",1);
+    caster->set_property("added short",({"%^BOLD%^%^WHITE%^ (in a light halo)%^RESET%^"}));
+    addSpellToCaster();
+    spell_successful();
+    caster->add_saving_bonus("all",4);
+    caster->add_ac_bonus(4);
+    execute_attack();
+    call_out("dest_effect",duration);
+    call_out("room_check",ROUND_LENGTH);
+}
+
+void room_check()
+{
+    if(!objectp(caster) || !objectp(ENV(caster)))
+    {
+        dest_effect();
+        return;
+    }
+
+    prepend_to_combat_cycle(ENV(caster));
+
+    call_out("room_check",ROUND_LENGTH*2);
+    return;
+}
+
+void execute_attack(){
+    object *attackers;
+    int i;
+
+    if(!flag)
+    {
+        flag = 1;
+        ::execute_attack();
+        return;
+    }
+
+    place      = environment(caster);
+    if(!objectp(caster) || !objectp(place))
+    {
+        dest_effect();
+        return;
+    }
+
+    attackers = filter_array(caster->query_attackers(),(:objectp($1):));
+    attackers = filter_array(attackers,(:$1->is_living():));
+
+    if(sizeof(attackers))
+    {
+        define_base_damage(0);
+        tell_room(place,"%^BOLD%^%^WHITE%^The light around "+caster->QCN+" falls upon "+caster->QP+" enemies!",({caster,target}));
+        tell_object(caster,"%^BOLD%^%^WHITE%^The light around you falls upon your enemies!");
+        for(i=0;i<sizeof(attackers);i++){
+            if(SAVING_D->saving_throw(attackers[i],"spell",0)) { continue; }
+            tell_object(attackers[i],"%^BOLD%^%^WHITE%^You are scorched by the light as you strike "
+                        ""+caster->QCN+"!");
+            damage_targ(attackers[i],attackers[i]->return_target_limb(),sdamage,"untyped");
+        }
+    }
+    prepend_to_combat_cycle(place);
+}
+
+void dest_effect()
+{
+    remove_call_out("room_check");
+    if(objectp(caster))
+    {
+        tell_object(caster,"%^RESET%^%^BOLD%^The halo around you fades.");
+        caster->remove_property("nimbus");
+        caster->add_ac_bonus(-4);
+        caster->add_saving_bonus("all",-4);
+	    caster->remove_property_value("added short",({"%^BOLD%^%^WHITE%^ (in a light halo)%^RESET%^"}));
+    }
+    ::dest_effect();
+    if(objectp(TO)) { TO->remove(); }
+}
