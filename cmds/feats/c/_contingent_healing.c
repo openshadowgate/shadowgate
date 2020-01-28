44c72204145fb19970d99d25a0a7f53e9dbea2bb
shadowgate <law@shadowgate.org>
2020-01-28 19:43:16 +0000

    autocommit 202001284319

--- cmds/feats/c/_contingent_healing.c
+++ cmds/feats/c/_contingent_healing.c
@@ -4,8 +4,9 @@
 
 inherit FEAT;
 
+object *exclude = ({});
 
-void create() 
+void create()
 {
     ::create();
     feat_type("instant");
@@ -35,7 +36,7 @@ int prerequisites(object ob)
 }
 
 
-int cmd_contingent_healing(string str) 
+int cmd_contingent_healing(string str)
 {
     object feat;
     if(!objectp(TP)) { return 0; }
@@ -47,13 +48,17 @@ int cmd_contingent_healing(string str)
 
 void execute_feat()
 {
+    object obj;
+
     if(FEATS_D->is_active(caster,"contingent healing"))
     {
-        tell_object(caster,"Contingent healing is already active.");
+        obj = query_active_feat("contingent healing");
+        obj->dest_effect();
+        caster = 0;
         dest_effect();
         return;
     }
-    ::execute_feat(); 
+    ::execute_feat();
 
     tell_object(caster,"%^RESET%^%^BOLD%^%^CYAN%^You take a deep breath and focus, preparing to heal any ally who is "
         "badly hurt.%^RESET%^");
@@ -68,34 +73,45 @@ void execute_attack()
 {
     object *party=({}), targ;
     int i, percent,casting;
-    
+
     if(!objectp(caster) || caster->query_ghost())
-    {        
+    {
         dest_effect();
         return;
     }
-    
+
     party = ob_party(caster);
     place = environment(caster);
-    
+
     if(!sizeof(party)) { party = ({ caster }); }
-    
-    for(i=0;i<sizeof(party);i++)
-    {
-        if(!objectp(caster) || caster->query_ghost())
-        {
+
+    for (i = 0; i < sizeof(party); i++) {
+        if (!objectp(caster) || caster->query_ghost()) {
             dest_effect();
             return;
         }
-        if(caster->query_unconscious() || caster->query_paralyzed() || (caster->query_blind() && !FEATS_D->usable_feat(caster,"blindfight"))) { continue; }
-        if(!objectp(party[i])) { continue; }
-        if(environment(party[i]) != environment(caster)) { continue; }
-        if(party[i]->query_hp_percent() > 60) { continue; }
-        if(percent && party[i]->query_hp_percent() > percent) { continue; }
+        if (caster->query_unconscious() || caster->query_paralyzed() || (caster->query_blind() && !FEATS_D->usable_feat(caster, "blindfight"))) {
+            continue;
+        }
+        if (!objectp(party[i])) {
+            continue;
+        }
+        if (party[i]->query_property("negative energy affinity")) {
+            continue;
+        }
+        if (environment(party[i]) != environment(caster)) {
+            continue;
+        }
+        if (party[i]->query_hp_percent() > 60) {
+            continue;
+        }
+        if (percent && party[i]->query_hp_percent() > percent) {
+            continue;
+        }
         percent = party[i]->query_hp_percent();
         targ = party[i];
     }
-    
+
     if(objectp(targ))
     {
         casting = caster->query_casting();
@@ -103,7 +119,7 @@ void execute_attack()
         new("/cmds/spells/h/_heal.c")->use_spell(caster, targ, caster->query_guild_level("cleric"), 100, "cleric");
         caster->set_casting(casting);
     }
-    
+
     if(objectp(place)) { place->addObjectToCombatCycle(TO,1); }
     else { dest_effect(); }
     return;
@@ -112,13 +128,12 @@ void execute_attack()
 
 void dest_effect()
 {
-    if(objectp(caster)) 
+    if(objectp(caster))
     {
-        tell_object(caster,"%^B_BLUE%^Your contingent healing deactivates.");
-        caster->remove_property_value("active_feats",({TO})); 
+        tell_object(caster,"%^B_RED%^You release your focus, your contingent healing deactivates.");
+        caster->remove_property_value("active_feats",({TO}));
     }
     ::dest_effect();
     remove_feat(TO);
     return;
 }
-
