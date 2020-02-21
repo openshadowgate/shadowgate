4a132b222bc2bc77a12e3a08c48eb11494e3db6f
shadowgate <law@shadowgate.org>
2020-02-21 14:33:01 +0000

    autocommit 202002213314

--- cmds/spells/w/_weird.c
+++ cmds/spells/w/_weird.c
@@ -6,10 +6,11 @@
 
 inherit SPELL;
 
-void create() {
+void create()
+{
     ::create();
     set_spell_name("weird");
-    set_spell_level(([ "mage" : 9,"oracle":9 ]));
+    set_spell_level(([ "mage" : 9, "oracle" : 9 ]));
     set_mystery("whimsy");
     set_spell_sphere("illusion");
     set_syntax("cast CLASS weird");
@@ -21,45 +22,51 @@ void create() {
     set_save("will");
 }
 
-string query_cast_string() {
-    return "%^CYAN%^"+caster->QCN+" speaks words of arcane horror.";
+string query_cast_string()
+{
+    return "%^CYAN%^" + caster->QCN + " speaks words of arcane horror.";
 }
 
-void spell_effect(int prof){
-    object *attackers;
-    int i,admg;
+void spell_effect(int prof)
+{
+    object* attackers;
+    int i, admg;
     attackers = caster->query_attackers();
-    attackers = filter_array(attackers, "is_non_immortal",FILTERS_D);
+    attackers = filter_array(attackers, "is_non_immortal", FILTERS_D);
     attackers = target_filter(attackers);
 
-    tell_room(place,"%^CYAN%^"+caster->QCN+" raises "+caster->QP+" hands and releases waves of horrific images!%^RESET%^");
-    if(!sizeof(attackers)){
-        tell_object(caster,"%^BOLD%^%^BLUE%^The place is filled with images of horrors, but nothing happens.%^RESET%^");
+    tell_room(place, "%^BOLD%^%^BLUE%^" + caster->QCN + " raises " + caster->QP + " hands and releases waves of horrific images!%^RESET%^");
+    if (!sizeof(attackers)) {
+        tell_object(caster, "%^BOLD%^%^BLUE%^The place is filled with images of horrors, but nothing happens.%^RESET%^");
         dest_effect();
         return;
     }
 
-    for(i=0;i<sizeof(attackers);i++){
-        if(!objectp(attackers[i])) continue;
+    for (i = 0; i < sizeof(attackers); i++) {
+        if (!objectp(attackers[i])) {
+            continue;
+        }
         define_base_damage(0);
-        admg=sdamage;
-        if(do_save(target) || random(3))
-        {
-            if(evade_splash(attackers[i])) continue;
-            admg=admg/2;
+        admg = sdamage;
+        if (do_save(target) || random(3)) {
+            if (evade_splash(attackers[i])) {
+                continue;
+            }
+            admg = admg / 2;
+        }else {
+            "/std/effect/status/cowering"->apply_effect(attackers[i], clevel / 18 + 1);
         }
-        else
-        {
-            "/std/effect/status/cowering"->apply_effect(attackers[i],clevel/18 + 1);
-        }
-        tell_object(attackers[i],"%^CYAN%^Your mind is filled with images of suffering!%^RESET%^");
-        damage_targ(attackers[i],attackers[i]->return_target_limb(),admg,"mental");
+        tell_object(attackers[i], "%^CYAN%^%^BOLD%^Your mind is filled with images of suffering!%^RESET%^");
+        damage_targ(attackers[i], attackers[i]->return_target_limb(), admg, "mental");
     }
     spell_successful();
     dest_effect();
 }
 
-void dest_effect(){
+void dest_effect()
+{
     ::dest_effect();
-    if(objectp(TO)) TO->remove();
+    if (objectp(TO)) {
+        TO->remove();
+    }
 }
