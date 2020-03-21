40e558b42077f3e82b7c5beb7aa1ad3ba55680b6
shadowgate <law@shadowgate.org>
2020-03-21 13:31:02 +0000

    autocommit 202003213113

--- cmds/spells/i/_improved_armor.c
+++ cmds/spells/i/_improved_armor.c
@@ -8,7 +8,7 @@
 #include <daemons.h>
 inherit SPELL;
 
-int benchmark,tally,bonus;
+int benchmark, tally, bonus;
 
 
 void create()
@@ -27,15 +27,15 @@ void create()
 
 int preSpell()
 {
-    if (!target) target = caster;
-    if(target->query_property("armoured"))
-    {
-        tell_object(caster,"%^BOLD%^%^BLACK%^You feel your spell repelled...");
+    if (!target) {
+        target = caster;
+    }
+    if (target->query_property("armoured")) {
+        tell_object(caster, "%^BOLD%^%^BLACK%^You feel your spell repelled...");
         return 0;
     }
-    if(target->query_property("raised resistance"))
-    {
-        tell_object(caster,"Your target already has raised resistance from a spell or temporary item.");
+    if (target->query_property("raised resistance")) {
+        tell_object(caster, "Your target already has raised resistance from a spell or temporary item.");
         return 0;
     }
     return 1;
@@ -45,11 +45,12 @@ void spell_effect(int prof)
 {
     bonus = 8;
 
-    if (!target) target = caster;
+    if (!target) {
+        target = caster;
+    }
 
-    if (!present(target,environment(caster)))
-    {
-        tell_object(caster,"%^BOLD%^Your target is not in this area.\n");
+    if (!present(target, environment(caster))) {
+        tell_object(caster, "%^BOLD%^Your target is not in this area.\n");
         TO->remove();
         return;
     }
@@ -57,69 +58,61 @@ void spell_effect(int prof)
     benchmark = target->query_hp();
     tally = 0;
 
-    if (target->query_property("armoured"))
-    {
-        tell_object(caster,"The spell is repelled by its own magic.");
+    if (target->query_property("armoured")) {
+        tell_object(caster, "The spell is repelled by its own magic.");
         TO->remove();
     }
 
-    if (!target->is_ok_armour("mage"))
-    {
-        tell_object(caster,"The spell can not offer protection to those wearing armor.");
-        if(objectp(TO)) { TO->remove(); }
+    if (!target->is_ok_armour("mage")) {
+        tell_object(caster, "The spell can not offer protection to those wearing armor.");
+        if (objectp(TO)) {
+            TO->remove();
+        }
         return;
     }
 
     spell_successful();
 
-    if (target == caster)
-    {
-        tell_object(caster,"%^BOLD%^%^CYAN%^A magical shield shimmers around you as "
-            "the dragon scale vanishes from your hand!");
-        tell_room(place,"%^BOLD%^%^CYAN%^A magical shield shimmers around "
-            ""+caster->QCN+" as the dragon scale vanishes from "+caster->QP+" "
-            "hand!.", caster );
-    }
-
-    else
-    {
-        tell_object(caster,"%^BOLD%^%^CYAN%^A magical shield shimmers around "
-            ""+target->QCN+" as the dragon scale vanishes from your hand!");
-        tell_object(target,"%^BOLD%^%^CYAN%^A magical shield shimmers around you as "
-            "the dragon scale vanishes from "+caster->QP+" hand!.");
-        tell_room(place,"%^BOLD%^%^CYAN%^A magical shield shimmers around "+target->QCN+" "
-            "as the dragon scale vanishes from "+caster->QCN+"'s hand!.",({caster, target}) );
+    if (target == caster) {
+        tell_object(caster, "%^BOLD%^%^CYAN%^A magical shield shimmers around you as "
+                    "the dragon scale vanishes from your hand!");
+        tell_room(place, "%^BOLD%^%^CYAN%^A magical shield shimmers around "
+                  "" + caster->QCN + " as the dragon scale vanishes from " + caster->QP + " "
+                  "hand!.", caster);
+    }else {
+        tell_object(caster, "%^BOLD%^%^CYAN%^A magical shield shimmers around "
+                    "" + target->QCN + " as the dragon scale vanishes from your hand!");
+        tell_object(target, "%^BOLD%^%^CYAN%^A magical shield shimmers around you as "
+                    "the dragon scale vanishes from " + caster->QP + " hand!.");
+        tell_room(place, "%^BOLD%^%^CYAN%^A magical shield shimmers around " + target->QCN + " "
+                  "as the dragon scale vanishes from " + caster->QCN + "'s hand!.", ({ caster, target }));
     }
 
     target->add_ac_bonus(bonus);
-    target->set_property("magic resistance",clevel/2); //to match caster's lament
-    target->set_property("raised resistance",1);
-    target->set_property("spelled", ({TO}) );
-    target->set_property("armoured",1);
+    target->set_property("magic resistance", clevel / 2); //not quite as good as caster's lament since it also gives 8 AC
+    target->set_property("raised resistance", 1);
+    target->set_property("spelled", ({ TO }));
+    target->set_property("armoured", 1);
     addSpellToCaster();
     call_out("test", 7);
 }
-
 
 void test()
 {
     int max_damage, add_tally, thp;
 
-    if (!objectp(target))
-    {
+    if (!objectp(target)) {
         remove();
         return;
     }
 
-    if (!target->is_ok_armour("mage"))
-    {
-        tell_object(caster,"The spell can not offer protection to those wearing armor.");
+    if (!target->is_ok_armour("mage")) {
+        tell_object(caster, "The spell can not offer protection to those wearing armor.");
         TO->dest_effect();
         return;
     }
 
-    if (!objectp(target))
-    {
+    if (!objectp(target)) {
         dest_effect();
         return;
     }
@@ -128,25 +121,29 @@ void test()
     return 0;
 }
 
-
 void dest_effect()
 {
-    if (find_call_out("test") != -1) remove_call_out("test");
+    if (find_call_out("test") != -1) {
+        remove_call_out("test");
+    }
 
-    if(objectp(target))
-    {
+    if (objectp(target)) {
         target->add_ac_bonus(-1 * bonus);
-        target->remove_property_value("spelled", ({TO}) );
-        target->set_property("magic resistance",-clevel/2);
-        target->remove_property("raised resistance",-1);
+        target->remove_property_value("spelled", ({ TO }));
+        target->set_property("magic resistance", -clevel / 2);
+        target->remove_property("raised resistance", -1);
         tell_object(target, "%^CYAN%^The magic shielding around you glows briefly, then fades away.");
-        tell_room(environment(target),"%^CYAN%^"+target->QCN+" glows briefly.", target );
+        tell_room(environment(target), "%^CYAN%^" + target->QCN + " glows briefly.", target);
         target->remove_property("armoured");
     }
 
     ::dest_effect();
-    if(objectp(TO)) TO->remove();
+    if (objectp(TO)) {
+        TO->remove();
+    }
 }
 
-
-string query_cast_string() { return "%^CYAN%^"+caster->QCN+" holds up a piece of leather in the air."; }
+string query_cast_string()
+{
+    return "%^CYAN%^" + caster->QCN + " holds up a piece of leather in the air.";
+}
