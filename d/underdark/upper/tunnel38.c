44a0426d87d150e6d5100af46de80464cfc74f64
shadowgate <law@shadowgate.org>
2020-03-21 17:31:02 +0000

    autocommit 202003213117

--- d/underdark/upper/tunnel38.c
+++ d/underdark/upper/tunnel38.c
@@ -1,12 +1,21 @@
-#include <std.h> 
-#include "../defs.h" 
-inherit INH+"spiderarches"; 
+#include <std.h>
+#include "../defs.h"
+inherit INH + "spiderarches";
 
-void create(){ 
-   ::create(); 
-   set_long(""+::query_long()+"\n");
-   set_exits(([ 
-"southwest":UPPER"tunnel37",
-"southeast":UPPER"tunnel35",
-   ])); 
+void create()
+{
+    ::create();
+    set_long("" + ::query_long() + "\n");
+    set_exits(([
+                   "southwest" : UPPER "tunnel37",
+                   "southeast" : UPPER "tunnel35",
+               ]));
+}
+
+void reset()
+{
+    ::reset();
+    if (!present("altar")) {
+        new("realms/odin/ashra_altar")->move(this_object());
+    }
 }
