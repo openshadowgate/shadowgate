eed8a4116512424be04d4974102f6017f3ed7b11
shadowgate <law@shadowgate.org>
2020-03-24 13:31:01 +0000

    autocommit 202003243113

--- d/guilds/alliance/hall/common.c
+++ d/guilds/alliance/hall/common.c
@@ -22,7 +22,7 @@ void create()
     set_short("Quarters Common Area");
     set_long("\n\
 %^CYAN%^Quarters Common Area%^RESET%^\n"+
-"This is the common area for the various living quarters in the Alliance Reborn Keep. The area is very spacious and comfortable, and serves as a nice lounging area, with couches and chairs arranged around a beautiful stone fireplace. Above the fireplace is a beautiful painting depicting a group of heroes standing tall and shining in a background of darkness. On the fireplace mantle is a cigar box, for special occasions.");
+"This is the common area for the various living quarters in the Alliance Reborn Keep. The area is very spacious and comfortable, and serves as a nice lounging area, with leather couches and cushioned chairs arranged around a beautiful stone fireplace. Above the fireplace is a beautiful painting depicting a group of heroes standing tall and shining in a background of darkness. On the fireplace mantle is a cigar box, for special occasions. On a small table in one of the corners is a magical decanter with a gentle amber liquid withing it and several cups arranged neatly.");
     set_exits( ([
                  "east":"/d/guilds/alliance/hall/hall",
                  "north":"/d/guilds/alliance/hall/quartersnorth",
