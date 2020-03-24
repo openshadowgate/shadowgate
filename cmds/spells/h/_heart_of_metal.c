5c8f3ac1662f05d8c606e2dd74529e9eb9b92863
shadowgate <law@shadowgate.org>
2020-03-24 19:11:02 +0000

    autocommit 202003241119

--- cmds/spells/h/_heart_of_metal.c
+++ cmds/spells/h/_heart_of_metal.c
@@ -8,7 +8,8 @@ inherit SPELL;
 #define MYTYPES ({ "silver", "cold iron" })
 
 object mywpn;
-string mypname, pname, origtype, weaponarg, typearg;
+string origtype, weaponarg, typearg;
+int args;
 
 void create()
 {
@@ -17,7 +18,7 @@ void create()
     set_spell_level(([ "mage" : 3, "cleric" : 3, "inquisitor" : 3 ]));
     set_spell_sphere("alteration");
     set_syntax("cast CLASS heart of metal <weapon> with <type>");
-    set_description("Using this spell, you temporarily lend the powers of either silver or cold iron to the target weapon.  This will change its base damage type for the duration of the spell.");
+    set_description("Using this spell, you temporarily lend the powers of either silver or cold iron to the target weapon.  This will change its base damage type for the duration of the spell.  NOTE: This spell is currently being debugged.  I wouldn't mess with it right now.");
     set_verbal_comp();
     set_somatic_comp();
     set_arg_needed();
@@ -27,31 +28,30 @@ void create()
 
 int preSpell()
 {
-    origtype = mywpn->query_base_damage_type();
-    if (!arg) {
+    args = sscanf(arg, "%s with %s", weaponarg, typearg);
+    if (!weaponarg) {
         tell_object(caster, "You must specify a target weapon for this power!");
         dest_effect();
         return 0;
     }
-    if (!mywpn = present(arg, caster)) {
-        tell_object(caster, "You don't have any " + arg + " in your inventory!");
+    if (!mywpn = present(weaponarg, caster)) {
+        tell_object(caster, "You don't have any " + weaponarg + " in your inventory!");
         dest_effect();
         return 0;
     }
     if (!mywpn->is_weapon()) {
-        tell_object(caster, "That " + arg + " is not a weapon!");
+        tell_object(caster, "That " + weaponarg + " is not a weapon!");
         return 0;
     }
-    if (sscanf(arg, "%s with %s", weaponarg, typearg) != 2) {
+    if (args != 2) {
         tell_object(CASTER, "You need to specify both your weapon and the type of metal.");
-        dest_effect();
         return 0;
     }
     if (member_array(typearg, MYTYPES) == -1) {
         tell_object(caster, "That is not a valid type of metal!");
-        dest_effect();
         return 0;
     }
+    origtype = mywpn->query_damage_type();
     return 1;
 }
 
@@ -62,13 +62,13 @@ void spell_effect(int prof)
     case "silver":
         mywpn->set_damage_type("silver");
         mywpn->remove_property("added short string");
-        mywpn->set_property("added short string", ({ " %^BOLD%^%^WHITE%^{{s%^RESET%^%^WHITE%^i%^BOLD%^%^WHITE%^lv%^RESET%^%^WHITE%^e%^BOLD%^%^WHITE^&ry%^BOLD%^%^WHITE%^}}%^RESET%^" }));
+        mywpn->set_property("added short string", ({ " %^BOLD%^%^WHITE%^{{s%^RESET%^%^WHITE%^i%^BOLD%^%^WHITE%^lv%^RESET%^%^WHITE%^e%^BOLD%^%^WHITE%^ry%^BOLD%^%^WHITE%^}}%^RESET%^" }));
         mywpn->set_property("added short", mywpn->query_property("added short string"));
         if (interactive(caster)) {
-            tell_object(caster, "%^BOLD%^%^WHITE%^You gather the very essence of silver about you infuse your " + arg + " with its mystical powers.%^RESET%^");
+            tell_object(caster, "%^BOLD%^%^WHITE%^You gather the very essence of silver about you infuse your " + weaponarg + " with its mystical powers.%^RESET%^");
         }
         tell_room(place, "%^BOLD%^%^WHITE%^As " + caster->QCN + " concentrates on " + caster->QP + " "
-                  "" + arg + ", the weapon itself turns to pure silver!.%^RESET%^", caster);
+                  "" + weaponarg + ", the weapon itself turns to pure silver!.%^RESET%^", caster);
         caster->remove_paralyzed();
         spell_successful();
         mylevel = clevel;
@@ -76,16 +76,15 @@ void spell_effect(int prof)
         break;
 
     case "cold iron":
-        origtype = mywpn->query_base_damage_type();
         mywpn->set_damage_type("cold iron");
         mywpn->remove_property("added short string");
         mywpn->set_property("added short string", ({ " %^RESET%^%^RED%^{{cold iron}}%^RESET%^" }));
         mywpn->set_property("added short", mywpn->query_property("added short string"));
         if (interactive(caster)) {
-            tell_object(caster, "%^RESET%^%^RED%^You gather the very essence of cold iron about you infuse your " + arg + " with its mystical powers.%^RESET%^");
+            tell_object(caster, "%^RESET%^%^RED%^You gather the very essence of cold iron about you infuse your " + weaponarg + " with its mystical powers.%^RESET%^");
         }
         tell_room(place, "%^RESET%^%^RED%^As " + caster->QCN + " concentrates on " + caster->QP + " "
-                  "" + arg + ", the weapon itself turns to pure cold iron!.%^RESET%^", caster);
+                  "" + weaponarg + ", the weapon itself turns to pure cold iron!.%^RESET%^", caster);
         caster->remove_paralyzed();
         spell_successful();
         mylevel = clevel;
