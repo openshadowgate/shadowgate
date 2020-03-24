c75e0d66fc0328f1d1e67d5246f17bfdad59de52
shadowgate <law@shadowgate.org>
2020-03-24 13:11:01 +0000

    autocommit 202003241113

---
+++ d/guilds/alliance/hall/common.c
@@ -0,0 +1,36 @@
+/*
+  common.c
+  
+  Written for the new guild Alliance Reborn.
+  
+  -- Tlaloc -- 3.24.20
+*/
+
+#include <std.h>
+
+inherit VAULT;
+
+void create()
+{
+    ::create();
+    
+    set_terrain(VILLAGE);
+    set_travel(PAVED_ROAD);
+    set_indoors(1);
+    set_light(3);
+    set_property("teleport proof", 67);
+    set_short("Quarters Common Area");
+    set_long("\n\
+%^CYAN%^Quarters Common Area%^RESET%^\n"+
+"This is the common area for the various living quarters in the Alliance Reborn Keep. The area is very spacious and comfortable, and serves as a nice lounging area, with couches and chairs arranged around a beautiful stone fireplace. Above the fireplace is a beautiful painting depicting a group of heroes standing tall and shining in a background of darkness. On the fireplace mantle is a cigar box, for special occasions.");
+    set_exits( ([
+                 "east":"/d/guilds/alliance/hall/hall",
+                 "north":"/d/guilds/alliance/hall/quartersnorth",
+                 "west" :"/d/guilds/alliance/hall/quarterswest",
+                 "south" :"/d/guilds/alliance/hall/quarterssouth",
+               ]) );
+               
+    set_door("west door", "/d/guilds/alliance/hall/quarterswest", "west");
+    set_door("north door", "/d/guilds/alliance/hall/quartersnorth", "north");
+    set_door("south door", "/d/guilds/alliance/hall/quarterssouth", "south");
+}
