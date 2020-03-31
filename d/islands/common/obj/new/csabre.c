e6a8d590ca43bd5a4b5ffeb53b2edbcce895e25f
shadowgate <law@shadowgate.org>
2020-03-31 01:51:01 +0000

    autocommit 202003315101

--- d/islands/common/obj/new/csabre.c
+++ d/islands/common/obj/new/csabre.c
@@ -1,3 +1,4 @@
+//updated to include set_special_material_type - Odin 3/30/20
 #include <std.h>
 
 inherit "/d/common/obj/weapon/longsword.c";
@@ -25,92 +26,91 @@ void create()
         " Light and balanced, the sabre's handle is also made from solid mithril with the pommel of the blade extending from the bottom. The "
         "handle is wrapped in a rough %^BOLD%^%^GREEN%^skin%^BOLD%^%^CYAN%^ of some kind which allows for a stronger grip.");
 
-    set("read","%^BOLD%^%^WHITE%^Fast as ye maybe - Quicksilver will always bring the wrath of justice to the miscreants%^RESET%^");
-    set("langage","common");
+    set("read", "%^BOLD%^%^WHITE%^Fast as ye maybe - Quicksilver will always bring the wrath of justice to the miscreants%^RESET%^");
+    set("langage", "common");
 
-    set_hit((:TO,"hit":));
-    set_wield((:TO,"wield_func":));
-    set_unwield((:TO,"unwield_func":));
+    set_hit((: TO, "hit" :));
+    set_wield((: TO, "wield_func" :));
+    set_unwield((: TO, "unwield_func" :));
 
     set_value(100000);
 
-    set_item_bonus("magic resistance",50);
-    set_item_bonus("fortitude",6);
+    set_item_bonus("magic resistance", 50);
+    set_item_bonus("fortitude", 6);
+    set_special_material_type("silver");
 
     set_lore("The crystal sabre's origin is well known. The weapon was made for the leader of the Paladin Order of "
-        "Tyr in southern Azha. Winning many battles, Sir Kristan of Poeral (later known as Lord Kristan) claimed "
-        "his lord Tyr and the sword combined with his courage helped win the day. When the Paladin died he passed "
-        "it on to his bretheren.");
+             "Tyr in southern Azha. Winning many battles, Sir Kristan of Poeral (later known as Lord Kristan) claimed "
+             "his lord Tyr and the sword combined with his courage helped win the day. When the Paladin died he passed "
+             "it on to his bretheren.");
 
-    set_property("no curse",1);
-    set_property("enchantment",7);
+    set_property("no curse", 1);
+    set_property("enchantment", 7);
 
     counter = 0;
 
-    set_property("quest required","%^BOLD%^%^RED%^Defeated the great red wyrm Klauth!%^RESET%^");
-    set_property("level required",35);
+    set_property("quest required", "%^BOLD%^%^RED%^Defeated the great red wyrm Klauth!%^RESET%^");
+    set_property("level required", 35);
 }
-
 
 int unwield_func()
 {
-    tell_object(ETO,"The balance of the blade is lost as you unwield the weapon.");
+    tell_object(ETO, "The balance of the blade is lost as you unwield the weapon.");
     return 1;
 }
-
 
 int wield_func(string str)
 {
     return 1;
 }
 
-
 int hit(object targ)
 {
-    if(!objectp(TO)) return 1;
-    if(!objectp(ETO)) return 1;
-    if(!objectp(targ)) return 1;
+    if (!objectp(TO)) {
+        return 1;
+    }
+    if (!objectp(ETO)) {
+        return 1;
+    }
+    if (!objectp(targ)) {
+        return 1;
+    }
 
-    if(random(1000) < 333)
-    {
-        switch(random(9))
-        {
+    if (random(1000) < 333) {
+        switch (random(9)) {
         case 0:
 
-            tell_room(EETO,"%^BOLD%^%^WHITE%^"+ETOQCN+" cuts effortlessly through the target!%^RESET%^",({ETO,targ}));
-            tell_object(ETO,"%^BOLD%^%^WHITE%^You cut through your target with ease!%^RESET%^");
-            tell_object(targ,"%^BOLD%^%^WHITE%^"+ETOQCN+" easily finds the soft spot in your armor for a critical blow!%^RESET%^");
-            targ->do_damage("torso",roll_dice(4,8)+8);
+            tell_room(EETO, "%^BOLD%^%^WHITE%^" + ETOQCN + " cuts effortlessly through the target!%^RESET%^", ({ ETO, targ }));
+            tell_object(ETO, "%^BOLD%^%^WHITE%^You cut through your target with ease!%^RESET%^");
+            tell_object(targ, "%^BOLD%^%^WHITE%^" + ETOQCN + " easily finds the soft spot in your armor for a critical blow!%^RESET%^");
+            targ->do_damage("torso", roll_dice(4, 8) + 8);
             counter++;
             break;
 
         case 1:
 
-            tell_room(EETO,"%^BOLD%^%^RED%^"+ETOQCN+" jabs the sword into "+targ->QCN+"'s torso and twists the blade before pulling out!%^RESET%^",({ETO,targ}));
-            tell_object(ETO,"%^BOLD%^%^RED%^You jab the sabre into your opponent and twist the handle before pulling out!%^RESET%^");
-            tell_object(targ,"%^BOLD%^%^RED%^"+ETOQCN+" jabs the sword into your torso and twists it before pulling out!%^RESET%^");
-            targ->do_damage("torso",roll_dice(4,8)+8);
+            tell_room(EETO, "%^BOLD%^%^RED%^" + ETOQCN + " jabs the sword into " + targ->QCN + "'s torso and twists the blade before pulling out!%^RESET%^", ({ ETO, targ }));
+            tell_object(ETO, "%^BOLD%^%^RED%^You jab the sabre into your opponent and twist the handle before pulling out!%^RESET%^");
+            tell_object(targ, "%^BOLD%^%^RED%^" + ETOQCN + " jabs the sword into your torso and twists it before pulling out!%^RESET%^");
+            targ->do_damage("torso", roll_dice(4, 8) + 8);
             counter++;
             break;
 
         case 2..4:
 
-            if(counter > 4)
-            {
-                tell_room(EETO,"%^BOLD%^%^CYAN%^"+ETOQCN+" moves quicker than the eye!%^RESET%^",({ETO,targ}));
-                tell_object(ETO,"%^BOLD%^%^CYAN%^Suddenly you feel a quickness come over your body!%^RESET%^");
-                tell_object(targ,"%^BOLD%^%^CYAN%^"+ETOQCN+" move quicker than the eye!%^RESET%^");
+            if (counter > 4) {
+                tell_room(EETO, "%^BOLD%^%^CYAN%^" + ETOQCN + " moves quicker than the eye!%^RESET%^", ({ ETO, targ }));
+                tell_object(ETO, "%^BOLD%^%^CYAN%^Suddenly you feel a quickness come over your body!%^RESET%^");
+                tell_object(targ, "%^BOLD%^%^CYAN%^" + ETOQCN + " move quicker than the eye!%^RESET%^");
                 counter = 0;
                 ETO->execute_attack();
                 ETO->execute_attack();
                 ETO->execute_attack();
-            }
-            else
-            {
-                tell_room(EETO,"%^RESET%^%^MAGENTA%^"+ETOQCN+" deftly performs a quick strike!%^RESET%^",({ETO,targ}));
-                tell_object(ETO,"%^RESET%^%^MAGENTA%^You deftly perform a quick strike!%^RESET%^");
-                tell_object(targ,"%^RESET%^%^MAGENTA%^"+ETOQCN+" deftly perform a quick strike!%^RESET%^");
-                targ->do_damage("torso",roll_dice(4,8)+8);
+            }else {
+                tell_room(EETO, "%^RESET%^%^MAGENTA%^" + ETOQCN + " deftly performs a quick strike!%^RESET%^", ({ ETO, targ }));
+                tell_object(ETO, "%^RESET%^%^MAGENTA%^You deftly perform a quick strike!%^RESET%^");
+                tell_object(targ, "%^RESET%^%^MAGENTA%^" + ETOQCN + " deftly perform a quick strike!%^RESET%^");
+                targ->do_damage("torso", roll_dice(4, 8) + 8);
                 ETO->execute_attack();
                 counter++;
             }
@@ -118,40 +118,40 @@ int hit(object targ)
 
         case 5:
 
-            tell_room(EETO,"%^BOLD%^%^BLACK%^"+ETOQCN+" elbows "+targ->QCN+" in the nose then brings the blade of the sabre back across "+targ->QCN+"'s eyes!%^RESET%^",({ETO,targ}));
-            tell_object(ETO,"%^BOLD%^%^BLACK%^You elbow your opponent in the nose then bring the sword back across the eyes!%^RESET%^");
-            tell_object(targ,"%^BOLD%^%^BLACK%^"+ETOQCN+" elbows you in the nose then brings the sword back across your eyes!%^RESET%^");
-            targ->set_paralyzed(7,"%^BOLD%^%^RED%^You are trying to wipe the blood from your eyes!%^RESET%^");
-            targ->do_damage("torso",roll_dice(3,10));
+            tell_room(EETO, "%^BOLD%^%^BLACK%^" + ETOQCN + " elbows " + targ->QCN + " in the nose then brings the blade of the sabre back across " + targ->QCN + "'s eyes!%^RESET%^", ({ ETO, targ }));
+            tell_object(ETO, "%^BOLD%^%^BLACK%^You elbow your opponent in the nose then bring the sword back across the eyes!%^RESET%^");
+            tell_object(targ, "%^BOLD%^%^BLACK%^" + ETOQCN + " elbows you in the nose then brings the sword back across your eyes!%^RESET%^");
+            targ->set_paralyzed(7, "%^BOLD%^%^RED%^You are trying to wipe the blood from your eyes!%^RESET%^");
+            targ->do_damage("torso", roll_dice(3, 10));
             counter++;
             break;
 
         case 6:
 
-            tell_room(EETO,"%^BOLD%^%^BLUE%^"+ETOQCN+"'s sabre glows blue as it makes contact with its target!%^RESET%^",({ETO,targ}));
-            tell_object(ETO,"%^BOLD%^%^BLUE%^The sabre glows a bright blue upon striking the foe!%^RESET%^");
-            tell_object(targ,"%^BOLD%^%^BLUE%^"+ETOQCN+"'s sabre glows blue as it makes contact with you!%^RESET%^");
-            targ->do_damage("torso",roll_dice(3,10));
+            tell_room(EETO, "%^BOLD%^%^BLUE%^" + ETOQCN + "'s sabre glows blue as it makes contact with its target!%^RESET%^", ({ ETO, targ }));
+            tell_object(ETO, "%^BOLD%^%^BLUE%^The sabre glows a bright blue upon striking the foe!%^RESET%^");
+            tell_object(targ, "%^BOLD%^%^BLUE%^" + ETOQCN + "'s sabre glows blue as it makes contact with you!%^RESET%^");
+            targ->do_damage("torso", roll_dice(3, 10));
             counter++;
             break;
 
         case 7:
 
-            tell_room(EETO,"%^BOLD%^%^YELLOW%^"+ETOQCN+"'s sword burns into "+targ->QCN+"!%^RESET%^",({ETO,targ}));
-            tell_object(ETO,"%^BOLD%^%^YELLOW%^An inner force from the sword warms your whole body!");
-            tell_object(targ,"%^BOLD%^%^YELLOW%^"+ETOQCN+"'s sword burns into you, seemingly drawing some of your power away!%^RESET%^");
-            set_property("magic",1);
-            ETO->add_hp(random(40)+30);
+            tell_room(EETO, "%^BOLD%^%^YELLOW%^" + ETOQCN + "'s sword burns into " + targ->QCN + "!%^RESET%^", ({ ETO, targ }));
+            tell_object(ETO, "%^BOLD%^%^YELLOW%^An inner force from the sword warms your whole body!");
+            tell_object(targ, "%^BOLD%^%^YELLOW%^" + ETOQCN + "'s sword burns into you, seemingly drawing some of your power away!%^RESET%^");
+            set_property("magic", 1);
+            ETO->add_hp(random(40) + 30);
             remove_property("magic");
             counter++;
             break;
 
         case 8:
 
-            tell_room(EETO,"%^BOLD%^%^RED%^"+ETOQCN+" stabs "+targ->QCN+" in the foot and extracts the blade to slash "+targ->QO+" across the stomach!%^RESET%^",({ETO,targ}));
-            tell_object(ETO,"%^BOLD%^%^RED%^ You stab "+targ->QCN+" in the foot, then quickly extract the blade to slash "+targ->QO+" across the stomach!%^RESET%^");
-            tell_object(targ,"%^BOLD%^%^RED%^"+ETOQCN+" stabs you in the foot and then quickly extracts the blade to slash you across the stomach!%^RESET%^");
-            targ->do_damage("torso",roll_dice(8,12)+10);
+            tell_room(EETO, "%^BOLD%^%^RED%^" + ETOQCN + " stabs " + targ->QCN + " in the foot and extracts the blade to slash " + targ->QO + " across the stomach!%^RESET%^", ({ ETO, targ }));
+            tell_object(ETO, "%^BOLD%^%^RED%^ You stab " + targ->QCN + " in the foot, then quickly extract the blade to slash " + targ->QO + " across the stomach!%^RESET%^");
+            tell_object(targ, "%^BOLD%^%^RED%^" + ETOQCN + " stabs you in the foot and then quickly extracts the blade to slash you across the stomach!%^RESET%^");
+            targ->do_damage("torso", roll_dice(8, 12) + 10);
             counter++;
             break;
         }
--- d/islands/common/obj/new/dragonaxe.c
+++ d/islands/common/obj/new/dragonaxe.c
@@ -1,3 +1,4 @@
+//updated to include set_special_material_type - Odin 3/30/2020
 #include <std.h>
 inherit "/d/common/obj/weapon/battle_axe.c";
 
@@ -8,7 +9,7 @@ void create()
 {
     ::create();
 
-    set_id(({"axe","dragon slayer","battle axe","medium axe","dragonbane"}));
+    set_id(({ "axe", "dragon slayer", "battle axe", "medium axe", "dragonbane" }));
 
     set_name("dragonbane");
 
@@ -17,54 +18,52 @@ void create()
     set_short("%^BOLD%^%^RED%^Drag%^RESET%^o%^BOLD%^%^RED%^nbane%^RESET%^");
 
     set_long(
-        "%^BOLD%^%^RED%^This axe was forged with the highest quality of mithril"+
-        " you have ever laid eyes on. The handle is made of finely carved redwood"+
-        " and the handle is laced in red leather rings to match the color"+
-        " of the handle. The mithril axe head is set into the top of the"+
-        " handle and is fashioned with a crescent blade on each side."+
-        " The axe holds no evidence of previous battle use. Not even the"+
-        " wooden handle contains a single scratch and the blade gleams in the light"+
+        "%^BOLD%^%^RED%^This axe was forged with the highest quality of mithril" +
+        " you have ever laid eyes on. The handle is made of finely carved redwood" +
+        " and the handle is laced in red leather rings to match the color" +
+        " of the handle. The mithril axe head is set into the top of the" +
+        " handle and is fashioned with a crescent blade on each side." +
+        " The axe holds no evidence of previous battle use. Not even the" +
+        " wooden handle contains a single scratch and the blade gleams in the light" +
         " as if it had just been polished.");
 
-    set_property("no curse",1);
-    set_property("enchantment",7);
+    set_property("no curse", 1);
+    set_property("enchantment", 7);
 
-    set_property("lore difficulty",15);
+    set_property("lore difficulty", 15);
     set_value(175500);
+    set_special_material_type("silver");
 
-    set_wield((:TO,"wieldit":));
-    set_hit((:TO,"hitit":));
-    set_unwield((:TO,"unwieldit":));
+    set_wield((: TO, "wieldit" :));
+    set_hit((: TO, "hitit" :));
+    set_unwield((: TO, "unwieldit" :));
 
-    set_item_bonus("fire resistance",50);
-    set_item_bonus("electricity resistance",50);
+    set_item_bonus("fire resistance", 50);
+    set_item_bonus("electricity resistance", 50);
 
     set_lore("This axe has an intresting history. Hundreds of years ago, Kraken Battleshield, a dwarf from the old dwarven "
-        "empire Barindor, made this axe with the help of a half-elven mage in hopes stopping Klauth from terrorizing an "
-        "elven settlement on the surface. Unknown to the creators of the axe, Klauth was scrying them the whole time and "
-        "obliterated the party the day they came to the surface. Not much remained from the encounter, except the axe in perfect condition.");
+             "empire Barindor, made this axe with the help of a half-elven mage in hopes stopping Klauth from terrorizing an "
+             "elven settlement on the surface. Unknown to the creators of the axe, Klauth was scrying them the whole time and "
+             "obliterated the party the day they came to the surface. Not much remained from the encounter, except the axe in perfect condition.");
 
-    set_property("quest required","%^BOLD%^%^RED%^Defeated the great red wyrm Klauth!%^RESET%^");
-    set_property("level required",35);
+    set_property("quest required", "%^BOLD%^%^RED%^Defeated the great red wyrm Klauth!%^RESET%^");
+    set_property("level required", 35);
 }
-
 
 int wieldit()
 {
-    tell_object(ETO,"%^BOLD%^%^RED%^As you grip the handle of the axe, you get the urge to hunt down a dragon!%^RESET%^");
-    tell_room(EETO,"%^BOLD%^%^RED%^"+ETO->QCN+" grips the handle of the axe and looks prepared for battle!%^RESET%^",ETO);
+    tell_object(ETO, "%^BOLD%^%^RED%^As you grip the handle of the axe, you get the urge to hunt down a dragon!%^RESET%^");
+    tell_room(EETO, "%^BOLD%^%^RED%^" + ETO->QCN + " grips the handle of the axe and looks prepared for battle!%^RESET%^", ETO);
     counter = 0;
     return 1;
 }
 
-
 int unwieldit()
 {
-    tell_object(ETO,"%^BOLD%^%^RED%^You feel as if a certain rage for battle has left your body.%^RESET%^");
-    tell_room(EETO,"%^BOLD%^%^RED%^"+ETO->QCN+" unwields the axe and exhales heavily.%^RESET%^",ETO);
+    tell_object(ETO, "%^BOLD%^%^RED%^You feel as if a certain rage for battle has left your body.%^RESET%^");
+    tell_room(EETO, "%^BOLD%^%^RED%^" + ETO->QCN + " unwields the axe and exhales heavily.%^RESET%^", ETO);
     return 1;
 }
-
 
 int hitit(object ob)
 {
@@ -72,116 +71,106 @@ int hitit(object ob)
     int i, rand;
 
     string race = (string)ob->query_race();
-    if(objectp(shape = ob->query_property("shapeshifted")))
-    {
+    if (objectp(shape = ob->query_property("shapeshifted"))) {
         race = shape->query_shape_race();
     }
 
-    if(!objectp(TO)) return 1;
-    if(!objectp(ETO)) return 1;
-    if(!objectp(ob)) return 1;
+    if (!objectp(TO)) {
+        return 1;
+    }
+    if (!objectp(ETO)) {
+        return 1;
+    }
+    if (!objectp(ob)) {
+        return 1;
+    }
 
-    if(race == "dragon") { rand = 650; }
-    else { rand = 333; }
+    if (race == "dragon") {
+        rand = 650;
+    }else {
+        rand = 333;
+    }
 
-    if(random(1000) < rand)
-    {
-        switch(random(5))
-        {
+    if (random(1000) < rand) {
+        switch (random(5)) {
         case 0:
 
-            if(race == "dragon")
-            {
-                tell_object(ETO,"%^RESET%^%^GREEN%^You strike the soft spot in the belly of the beast with suprising accuracy%^RESET%^");
-                tell_object(ob,"%^RESET%^%^GREEN%^"+ETO->QCN+" strikes you with suprising accuracy in the soft spot of your belly!%^RESET%^");
-                tell_room(EETO,"%^RESET%^%^GREEN%^"+ETO->QCN+" strikes the dragon in the soft spot of the belly with astounding accuracy!%^RESET%^",({ETO,ob}));
-                ob->do_damage("torso",random(100)+75);
-                counter ++;
+            if (race == "dragon") {
+                tell_object(ETO, "%^RESET%^%^GREEN%^You strike the soft spot in the belly of the beast with suprising accuracy%^RESET%^");
+                tell_object(ob, "%^RESET%^%^GREEN%^" + ETO->QCN + " strikes you with suprising accuracy in the soft spot of your belly!%^RESET%^");
+                tell_room(EETO, "%^RESET%^%^GREEN%^" + ETO->QCN + " strikes the dragon in the soft spot of the belly with astounding accuracy!%^RESET%^", ({ ETO, ob }));
+                ob->do_damage("torso", random(100) + 75);
+                counter++;
                 break;
-            }
-            else
-            {
-                tell_object(ETO,"%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_object(ob,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_room(EETO,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts deeply with the razor sharp edge of the axe!%^RESET%^",({ETO,ob}));
-                ob->do_damage("torso",random(40)+20);
+            }else {
+                tell_object(ETO, "%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_object(ob, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_room(EETO, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts deeply with the razor sharp edge of the axe!%^RESET%^", ({ ETO, ob }));
+                ob->do_damage("torso", random(40) + 20);
                 break;
             }
 
         case 1:
 
-            if(race == "dragon")
-            {
-                tell_object(ETO,"%^BOLD%^%^WHITE%^With a tremendous roar you strike powerfully with the axe!%^RESET%^");
-                tell_object(ob,"%^BOLD%^%^WHITE%^"+ETO->QCN+" roars loudly and strikes you powerfully with the axe!%^RESET%^");
-                tell_room(EETO,"%^BOLD%^%^WHITE%^"+ETO->QCN+" roars with rage and strikes the dragon %^BOLD%^%^RED%^powerfully%^BOLD%^%^WHITE%^ with the axe!%^RESET%^",({ETO,ob}));
-                ob->do_damage("torso",random(100)+75);
-                counter ++;
+            if (race == "dragon") {
+                tell_object(ETO, "%^BOLD%^%^WHITE%^With a tremendous roar you strike powerfully with the axe!%^RESET%^");
+                tell_object(ob, "%^BOLD%^%^WHITE%^" + ETO->QCN + " roars loudly and strikes you powerfully with the axe!%^RESET%^");
+                tell_room(EETO, "%^BOLD%^%^WHITE%^" + ETO->QCN + " roars with rage and strikes the dragon %^BOLD%^%^RED%^powerfully%^BOLD%^%^WHITE%^ with the axe!%^RESET%^", ({ ETO, ob }));
+                ob->do_damage("torso", random(100) + 75);
+                counter++;
                 break;
-            }
-            else
-            {
-                tell_object(ETO,"%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_object(ob,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_room(EETO,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts deeply with the razor sharp edge of the axe!%^RESET%^",({ETO,ob}));
-                ob->do_damage("torso",random(40)+20);
+            }else {
+                tell_object(ETO, "%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_object(ob, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_room(EETO, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts deeply with the razor sharp edge of the axe!%^RESET%^", ({ ETO, ob }));
+                ob->do_damage("torso", random(40) + 20);
                 break;
             }
 
         case 2:
 
-            if(race == "dragon")
-            {
-                if(counter > 5)
-                {
-                    tell_object(ETO,"%^BOLD%^%^RED%^You enter a frenzy and swing your axe wildly!%^RESET%^");
-                    tell_object(ob,"%^BOLD%^%^RED%^"+ETO->QCN+" enters a frenzy, swinging the axe wildly!%^RESET%^");
-                    tell_room(EETO,"%^BOLD%^%^RED%^"+ETO->QCN+" enters a frenzy, swinging the axe wildly!%^RESET%^",({ETO,ob}));
+            if (race == "dragon") {
+                if (counter > 5) {
+                    tell_object(ETO, "%^BOLD%^%^RED%^You enter a frenzy and swing your axe wildly!%^RESET%^");
+                    tell_object(ob, "%^BOLD%^%^RED%^" + ETO->QCN + " enters a frenzy, swinging the axe wildly!%^RESET%^");
+                    tell_room(EETO, "%^BOLD%^%^RED%^" + ETO->QCN + " enters a frenzy, swinging the axe wildly!%^RESET%^", ({ ETO, ob }));
                     counter = 0;
-                    for(i=0;i<8;i++)
-                    {
+                    for (i = 0; i < 8; i++) {
                         ETO->execute_attack();
                     }
-                }
-                else
-                {
-                    tell_object(ETO,"%^BOLD%^%^BLUE%^The edge of the axe flashes briefly with a blue light as you strike the dragon!%^RESET%^");
-                    tell_object(ob,"%^BOLD%^%^BLUE%^"+ETO->QCN+"'s axe flashes briefly with a blue light as it strikes you!%^RESET%^");
-                    tell_room(EETO,"%^BOLD%^%^BLUE%^"+ETO->QCN+"'s axe flashes briefly with a blue light as it makes contact with the dragon!%^RESET%^",({ETO,ob}));
-                    ob->do_damage("torso",random(30)+50);
-                    counter ++;
+                }else {
+                    tell_object(ETO, "%^BOLD%^%^BLUE%^The edge of the axe flashes briefly with a blue light as you strike the dragon!%^RESET%^");
+                    tell_object(ob, "%^BOLD%^%^BLUE%^" + ETO->QCN + "'s axe flashes briefly with a blue light as it strikes you!%^RESET%^");
+                    tell_room(EETO, "%^BOLD%^%^BLUE%^" + ETO->QCN + "'s axe flashes briefly with a blue light as it makes contact with the dragon!%^RESET%^", ({ ETO, ob }));
+                    ob->do_damage("torso", random(30) + 50);
+                    counter++;
                 }
                 break;
-            }
-            else
-            {
-                tell_object(ETO,"%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_object(ob,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_room(EETO,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts deeply with the razor sharp edge of the axe!%^RESET%^",({ETO,ob}));
-                ob->do_damage("torso",random(30)+10);
+            }else {
+                tell_object(ETO, "%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_object(ob, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_room(EETO, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts deeply with the razor sharp edge of the axe!%^RESET%^", ({ ETO, ob }));
+                ob->do_damage("torso", random(30) + 10);
                 break;
             }
 
         case 3..4:
 
-            if(race == "dragon")
-            {
-                tell_object(ETO,"%^BOLD%^%^BLUE%^The edge of the axe flashes briefly with a blue light as you strike the dragon!%^RESET%^");
-                tell_object(ob,"%^BOLD%^%^BLUE%^"+ETO->QCN+"'s axe flashes briefly with a blue light as it strikes you!%^RESET%^");
-                tell_room(EETO,"%^BOLD%^%^BLUE%^"+ETO->QCN+"'s axe flashes briefly with a blue light as it makes contact with the dragon!%^RESET%^",({ETO,ob}));
-                ob->do_damage("torso",random(100)+75);
-                counter ++;
+            if (race == "dragon") {
+                tell_object(ETO, "%^BOLD%^%^BLUE%^The edge of the axe flashes briefly with a blue light as you strike the dragon!%^RESET%^");
+                tell_object(ob, "%^BOLD%^%^BLUE%^" + ETO->QCN + "'s axe flashes briefly with a blue light as it strikes you!%^RESET%^");
+                tell_room(EETO, "%^BOLD%^%^BLUE%^" + ETO->QCN + "'s axe flashes briefly with a blue light as it makes contact with the dragon!%^RESET%^", ({ ETO, ob }));
+                ob->do_damage("torso", random(100) + 75);
+                counter++;
                 break;
-            }
-            else
-            {
-                tell_object(ETO,"%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_object(ob,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
-                tell_room(EETO,"%^BOLD%^%^WHITE%^"+ETO->QCN+" cuts deeply with the razor sharp edge of the axe!%^RESET%^",({ETO,ob}));
-                ob->do_damage("torso",random(40)+20);
+            }else {
+                tell_object(ETO, "%^BOLD%^%^WHITE%^You cut deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_object(ob, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts you deeply with the razor sharp edge of the axe!%^RESET%^");
+                tell_room(EETO, "%^BOLD%^%^WHITE%^" + ETO->QCN + " cuts deeply with the razor sharp edge of the axe!%^RESET%^", ({ ETO, ob }));
+                ob->do_damage("torso", random(40) + 20);
                 break;
             }
         }
     }
-    return roll_dice(3,6);
+    return roll_dice(3, 6);
 }
