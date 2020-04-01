799ce98b97f0c0d4e1d3e81b4c04cd0d19086645
shadowgate <law@shadowgate.org>
2020-04-01 15:51:01 +0000

    autocommit 202004015115

--- cmds/spells/w/_weighed_in_the_balance.c
+++ cmds/spells/w/_weighed_in_the_balance.c
@@ -1,4 +1,4 @@
-//Weighed in the Balance - new Vigilance domain spell to 
+//Weighed in the Balance - new Vigilance domain spell to
 //replace Dictum with the rebalancing of domains
 //Coded by ~Circe~ 5/5/08
 //Based in part on the mass cure/cause wounds spells by Ares
@@ -13,7 +13,8 @@ inherit SPELL;
 int calculate_healing();
 int calculate_damage();
 
-void create(){
+void create()
+{
     ::create();
     set_author("circe");
     set_spell_name("weighed in the balance");
@@ -22,96 +23,115 @@ void create(){
     set_spell_domain("vigilance");
     set_syntax("cast CLASS weighed in the balance");
     set_description("On casting this spell, the priest calls upon his deity to judge those in the surrounding area.  "
-"Those aiding the caster will be healed (they must be in a party) while those attacking the caster will suffer damage.");
+                    "Those aiding the caster will be healed (they must be in a party) while those attacking the caster will suffer damage.");
     set_verbal_comp();
     set_somatic_comp();
 }
 
-int preSpell(){
-   object *attackers = ({});
-   attackers = caster->query_attackers();
-   if(!sizeof(attackers)){
-      tell_object(caster,"You may only call upon "+
-         ""+(string)caster->query_diety()+"'s judgment when you "+
-         "are in battle!");
-      return 0;
+int preSpell()
+{
+    object* attackers = ({});
+    attackers = caster->query_attackers();
+    if (!sizeof(attackers)) {
+        tell_object(caster, "You may only call upon " +
+                    "" + (string)caster->query_diety() + "'s judgment when you " +
+                    "are in battle!");
+        return 0;
 //can only be cast when there are attackers
-   }
-   return 1;
+    }
+    return 1;
 }
 
-string query_cast_string(){
-   tell_object(caster,"%^BOLD%^%^BLUE%^You plant your feet and "+
-      "point your finger at your enemy, calling for %^YELLOW%^"+
-      ""+(string)caster->query_diety()+"'s %^BLUE%^aid.%^RESET%^");
-   tell_room(place,"%^BOLD%^%^BLUE%^"+caster->QCN+" pauses mid-"+
-      "battle to point a point a finger and fix "+caster->QP+" "+
-      "attackers with a stern gaze, calling upon "+caster->QP+" "+
-      "deity's aid.%^RESET%^",caster);
+string query_cast_string()
+{
+    tell_object(caster, "%^BOLD%^%^BLUE%^You plant your feet and " +
+                "point your finger at your enemy, calling for %^YELLOW%^" +
+                "" + (string)caster->query_diety() + "'s %^BLUE%^aid.%^RESET%^");
+    tell_room(place, "%^BOLD%^%^BLUE%^" + caster->QCN + " pauses mid-" +
+              "battle to point a point a finger and fix " + caster->QP + " " +
+              "attackers with a stern gaze, calling upon " + caster->QP + " " +
+              "deity's aid.%^RESET%^", caster);
     return "display";
 }
 
-void spell_effect(int prof){
-   int i;
-   string party;
-   object *targets = ({}),*attackers = ({});
+void spell_effect(int prof)
+{
+    int i;
+    string party;
+    object* targets = ({}), * attackers = ({});
 
-   party = (string)caster->query_party();
+    party = (string)caster->query_party();
 
-   if(party) { targets = (object *)PARTY_OB->query_party_members(party); }
-   attackers = caster->query_attackers();
-   targets += ({caster});
-   targets = distinct_array(targets);
+    if (party) {
+        targets = (object*)PARTY_OB->query_party_members(party);
+    }
+    attackers = caster->query_attackers();
+    targets += ({ caster });
+    targets = distinct_array(targets);
 
-   tell_room(place,"%^YELLOW%^A golden balance appears in the air before "+
-      ""+caster->QCN+", the pans slowly rising and falling.%^RESET%^",caster);
-   tell_object(caster,"%^YELLOW%^A golden balance appears in the air before "+
-      "you, the pans slowly rising and falling.%^RESET%^",caster);
-   tell_room(place,"%^BOLD%^As the balance reaches %^BLUE%^equilibrium"+
-      "%^WHITE%^, a %^YELLOW%^blastwave%^WHITE%^ of %^BLUE%^force "+
-      "%^WHITE%^radiates outward, shocking the area!%^RESET%^");
-   if(sizeof(targets)){
-      for(i=0;i<sizeof(targets);i++){
-         if(!objectp(targets[i])) { continue; }
-         if(!present(targets[i],place)) { continue; }
-         if(targets[i]->query_property("berserked")){
-            tell_object(targets[i],"%^RED%^The rage fights off the healing!%^RESET%^");
-         }else{
-            tell_object(targets[i],"%^CYAN%^You are found worthy "+
-               "by the scales and feel a healing wave reach you.%^RESET%^");
-            tell_room(place,"%^CYAN%^A soft wave of energy bathes "+
-               ""+targets[i]->QCN+".%^RESET%^",targets[i]);
-            damage_targ(targets[i],targets[i]->return_target_limb(),(-1 * calculate_healing()),"divine");
-         }
-      }
-   }
-   if(sizeof(attackers)){
-      for(i=0;i<sizeof(attackers);i++){
-         if(!objectp(attackers[i])) { continue; }
-         if(!present(attackers[i],place)) { continue; }
-         tell_object(attackers[i],"%^YELLOW%^The blast from the "+
-            "scales rocks you, searing your skin!%^RESET%^");
-         tell_room(place,"%^YELLOW%^The blast from the scales "+
-            "sears %^"+attackers[i]->QCN+"!%^RESET%^",({attackers[i]}));
-         damage_targ(attackers[i],attackers[i]->return_target_limb(),calculate_damage(),"divine");
-         spell_kill(attackers[i],caster);
-      }
-   }
-   spell_successful();
-   dest_effect();
-   return;
+    tell_room(place, "%^YELLOW%^A golden balance appears in the air before " +
+              "" + caster->QCN + ", the pans slowly rising and falling.%^RESET%^", caster);
+    tell_object(caster, "%^YELLOW%^A golden balance appears in the air before " +
+                "you, the pans slowly rising and falling.%^RESET%^", caster);
+    tell_room(place, "%^BOLD%^As the balance reaches %^BLUE%^equilibrium" +
+              "%^WHITE%^, a %^YELLOW%^blastwave%^WHITE%^ of %^BLUE%^force " +
+              "%^WHITE%^radiates outward, shocking the area!%^RESET%^");
+    if (sizeof(targets)) {
+        set_helpful_spell(1);
+        for (i = 0; i < sizeof(targets); i++) {
+            if (!objectp(targets[i])) {
+                continue;
+            }
+            if (!present(targets[i], place)) {
+                continue;
+            }
+            if (targets[i]->query_property("berserked")) {
+                tell_object(targets[i], "%^RED%^The rage fights off the healing!%^RESET%^");
+            }else {
+                tell_object(targets[i], "%^CYAN%^You are found worthy " +
+                            "by the scales and feel a healing wave reach you.%^RESET%^");
+                tell_room(place, "%^CYAN%^A soft wave of energy bathes " +
+                          "" + targets[i]->QCN + ".%^RESET%^", targets[i]);
+                damage_targ(targets[i], targets[i]->return_target_limb(), (-1 * calculate_healing()), "divine");
+            }
+        }
+    }
+    if (sizeof(attackers)) {
+        set_helpful_spell(0);
+        for (i = 0; i < sizeof(attackers); i++) {
+            if (!objectp(attackers[i])) {
+                continue;
+            }
+            if (!present(attackers[i], place)) {
+                continue;
+            }
+            tell_object(attackers[i], "%^YELLOW%^The blast from the " +
+                        "scales rocks you, searing your skin!%^RESET%^");
+            tell_room(place, "%^YELLOW%^The blast from the scales " +
+                      "sears %^" + attackers[i]->QCN + "!%^RESET%^", ({ attackers[i] }));
+            damage_targ(attackers[i], attackers[i]->return_target_limb(), calculate_damage(), "divine");
+            spell_kill(attackers[i], caster);
+        }
+    }
+    spell_successful();
+    dest_effect();
+    return;
 }
 
-int calculate_healing(){
+int calculate_healing()
+{
     return sdamage;
 }
 
-int calculate_damage(){
+int calculate_damage()
+{
     return sdamage;
 }
 
-void dest_effect(){
+void dest_effect()
+{
     ::dest_effect();
-    if(objectp(TO)) TO->remove();
-
+    if (objectp(TO)) {
+        TO->remove();
+    }
 }
