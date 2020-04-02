a8093fc381e26e1dc66ce72c28de1b282b62fa98
shadowgate <law@shadowgate.org>
2020-04-02 20:51:02 +0000

    autocommit 202004025120

---
+++ cmds/spells/e/_empathic_relief.c
@@ -0,0 +1,48 @@
+/*
+  _empathic_relief.c
+  
+  Cures all negative statuses on the target.
+  
+  -- Tlaloc -- 4.2.20
+*/
+
+#include <spell.h>
+#include <magic.h>
+#include <daemons.h>
+
+inherit SPELL;
+
+void create()
+{
+    ::create();
+    
+    set_author("tlaloc");
+    set_spell_name("empathic relief");
+    set_spell_level( ([ "psion" : 2 ]) );
+    set_spell_sphere("healing");
+    set_syntax("cast CLASS empathic relief on TARGET");
+    set_description("You cleanse the subject's body of unhealthy influences, relieving all debilitating conditions, including dazed, confused, stunned, blinded, paralyzed, fatigued, exhausted, sickened, and poisoned.");
+    set_target_required(1);
+    set_helpful_spell(1);
+}
+
+string query_cast_string()
+{
+    return "%^CYAN%^" + sprintf("%s concentrates on %s psionic powers", caster->QCN, caster->query_possessive());
+}
+
+void spell_effect(int prof)
+{
+    string myname, yourname;
+    
+    myname = caster->QCN;
+    yourname = target->QCN;
+    
+    tell_object(caster, "%^BOLD%^" + sprintf("You pour psychic energy into %s's body, cleansing %s of all ailments.", yourname, target->query_possessive()));
+    tell_object(target, "%^BOLD%^" + sprintf("%s pours psychic energy into your body, cleansing you of all ailments.", myname));
+    tell_room(place, "%^BOLD%^" + sprintf("%s pours psychic energy into %s, cleansing all of %s ailments.", myname, yourname, target->query_possessive()), ({ target, caster }));
+    
+    "/std/magic/cleanse"->cleanse(target);\
+    spell_successful();
+    dest_effect();
+}
