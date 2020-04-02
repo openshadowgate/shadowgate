e071e671ff538670f1c8dcebb9e4b693435d3d37
shadowgate <law@shadowgate.org>
2020-04-02 21:31:02 +0000

    autocommit 202004023121

--- d/common/obj/armour/fullplate.c
+++ d/common/obj/armour/fullplate.c
@@ -1,24 +1,28 @@
 inherit "/std/plate";
 
-void create() {
+void create()
+{
     ::create();
     set_max_dex_bonus(3);
     set_armor_prof("heavy");
     set_name("fullplate");
-     set_id(({ "armor","full plate armor","plate","medium fullplate"}));
+    set_id(({ "armor", "full plate armor", "plate", "medium fullplate" }));
     set("short", "A suit of full plate armor");
-    set("long", 
-	"This is the impressive, high gothic style armor of the late "
-"middle ages and renaissance. It is perfectly forged and fitted. All the  "
-"plates are interlocking and carefully angled to deflect blows. The metal "
-"plates are backed by padding and chainmail. The weight is well " 
-"distributed. The armor is hot and extremely expensive."
-       );
+    set("long",
+        "This is the impressive, high gothic style armor of the late "
+        "middle ages and renaissance. It is perfectly forged and fitted. All the  "
+        "plates are interlocking and carefully angled to deflect blows. The metal "
+        "plates are backed by padding and chainmail. The weight is well "
+        "distributed. The armor is hot and extremely expensive."
+        );
     set_weight(36);
-    set("value", 10000);
+    set("value", 1500);
     set_type("armour");
-    set_limbs( ({ "torso" }) );
+    set_limbs(({ "torso" }));
     set_ac(9);
 }
 
-int is_metal() { return 1; }
+int is_metal()
+{
+    return 1;
+}
