8c2aa7a6ee7e83a6c90589b6c69114355e79eca6
shadowgate <law@shadowgate.org>
2020-04-02 23:31:01 +0000

    autocommit 202004023123

--- d/shadow/mon/testdummy.c
+++ d/shadow/mon/testdummy.c
@@ -9,18 +9,19 @@ void create()
 {
     ::create();
     set_name("dummy");
-    set_id(({ "dummy", "silly dummy",}));
+    set_id(({ "dummy", "test dummy", }));
     set_race("test dummy");
     set_body_type("human");
-    set_short("silly dummy");
-    set_long("Show me what you got.");
+    set_short("ooc test dummy");
+    set_long("Come at me, bro!");
     set_hd(1, 1);
     set_class("fighter");
     set_mlevel("fighter", 1);
     set_hp(10000000);
 }
 
-int do_typed_damage_effects(victim,limb,damage,damage_type){
-  tell_room(ETO, "%^MAGENTA%^Dummy says%^RESET%^: I got hit to " + limb + " for " + damage + " " +damage_type + " damage!");
-  return damage;
+int do_typed_damage_effects(victim, limb, damage, damage_type)
+{
+    tell_room(ETO, "%^MAGENTA%^Dummy says%^RESET%^: I got hit to " + limb + " for " + damage + " " + damage_type + " damage!");
+    return damage;
 }
