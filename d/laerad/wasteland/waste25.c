2d790a754b16eab911ad4e34ac5ff99272ec1381
shadowgate <law@shadowgate.org>
2020-04-03 16:11:01 +0000

    autocommit 202004031116

--- d/laerad/wasteland/waste25.c
+++ d/laerad/wasteland/waste25.c
@@ -1,21 +1,26 @@
 #include <std.h>
 inherit "/d/laerad/wsland";
 
-void create(){
+void create()
+{
     ::create();
-    set_exits( ([
-	"west":"/d/laerad/wasteland/waste14",
-	"east":"/d/laerad/wasteland/waste34",
-	"north":"/d/laerad/wasteland/waste24",
-	"south":"/d/laerad/wasteland/waste26"
-    ]) );
+    set_exits(([
+                   "west" : "/d/laerad/wasteland/waste14",
+                   "east" : "/d/laerad/wasteland/waste34",
+                   "north" : "/d/laerad/wasteland/waste24",
+                   "south" : "/d/laerad/wasteland/waste26"
+               ]));
 }
-int clean_up(){return 1;}
+
+int clean_up()
+{
+    return 1;
+}
 
 reset()
 {
     ::reset();
-    if (!objectp(present("silvim", TO))) {
+    if (!objectp(present("arganon", TO))) {
         new("/d/laerad/mon/anti")->move(this_object());
     }
 }
