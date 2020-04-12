e850ca9794f7ce9371c12fb65c749447b2e72774
shadowgate <law@shadowgate.org>
2020-04-12 22:31:01 +0000

    autocommit 202004123122

--- d/antioch/antioch2/rooms/trail6.c
+++ d/antioch/antioch2/rooms/trail6.c
@@ -1,7 +1,8 @@
 //Updated to mention Jarmila's temple, which is moved to north from northwest to improve mapping. Kismet.
+//Ready to install: proper path is /d/antioch/antioch2/rooms/trail6  Kismet.   
 
 #include <std.h>
-#include "../antioch.h"
+#include "../antioch.h" 
 inherit ROOM;
 
 void create()
@@ -13,12 +14,12 @@ void create()
 	set_property("light",2);
 	set_short("Road Through the Woods");
 	set_long(
-	"%^GREEN%^Road Through the Woods
-%^GREEN%^This is a little %^CYAN%^road%^GREEN%^ through the %^BOLD%^%^BLACK%^C%^RESET%^%^CYAN%^haru %^BOLD%^%^BLACK%^M%^BLACK%^o%^RESET%^%^CYAN%^u%^BOLD%^%^BLACK%^n%^RESET%^%^CYAN%^ta%^BOLD%^%^BLACK%^i%^RESET%^%^CYAN%^ns%^GREEN%^ that leads to the great city of Antioch.  The the west, you can see the gates of the city.  To the northeast is the path leading back to the ruins of old Antioch.  The road is cobbled here and large %^CYAN%^stones%^GREEN%^ are set on either side of the road with runes carved into them telling of the great city of Antioch and blessing it with peace, luck, and wealth.  A beautiful shrine stands to the north. To the southwest a road leads around the city into the mountains.\n"
+	"%^GREEN%^Road Through the Woods\n"+
+"%^ORANGE%^This is a little %^CYAN%^road%^ORANGE%^ through the %^BOLD%^%^BLACK%^C%^RESET%^%^CYAN%^haru %^BOLD%^%^BLACK%^M%^BLACK%^o%^RESET%^%^CYAN%^u%^BOLD%^%^BLACK%^n%^RESET%^%^CYAN%^ta%^BOLD%^%^BLACK%^i%^RESET%^%^CYAN%^ns%^ORANGE%^ that leads to the great city of Antioch. The the west, you can see the gates of the city. To the northeast is the path leading back to the ruins of old Antioch. The road is cobbled here and large st%^CYAN%^on%^ORANGE%^es are set on either side of the road with runescarved into them telling of the great city of Antioch and blessing it with peace, luck, and wealth. A beautiful %^YELLOW%^shrine%^RESET%^%^ORANGE%^ stands to the north. To the southwest, a road leads around the city into the mountains.\n"
 	);
 	set("night long",
-	"%^GREEN%^Road Through the Woods 
-%^BLUE%^The peaks of the %^BOLD%^%^BLACK%^C%^RESET%^%^CYAN%^haru%^BLUE%^ Mountains are outlined by the moon.  The forest seems less dangerous here for the gates of Antioch are just to your west and you can see by the light from their guard tower.  Back to the northeast is the %^CYAN%^road%^BLUE%^ leading to the ruins of Antioch.  That path seems more sinister, with the odd shadows from the %^CYAN%^t%^BOLD%^%^BLACK%^r%^RESET%^%^CYAN%^ees%^BLUE%^ slithering across the road.  Here the road is cobbled and the large %^CYAN%^s%^BOLD%^%^BLACK%^t%^BLACK%^o%^RESET%^%^CYAN%^n%^BOLD%^%^BLACK%^e%^RESET%^%^CYAN%^s%^BLUE%^ on either side of it are engraved with runes telling of the great city of Antioch and blessing it with peace, luck and wealth.  A beautiful shrine stands to the north.  To the southwest a road leads around the city.\n"
+	"%^GREEN%^Road Through the Woods\n"+ 
+"%^BOLD%^BLUE%^The peaks of the %^BOLD%^%^BLACK%^C%^RESET%^%^CYAN%^haru Mountains%^RESET%^%^BOLD%^%^BLUE%^ are outlined by the moon. The forest seems less dangerous here for the gates of Antioch are just to your west and you can see by the light from their guard tower. Back to the northeast is the %^RESET%^%^CYAN%^road%^BOLD%^%^BLUE%^ leading to the ruins of Antioch. That path seems more sinister, with the odd shadows from the %^RESET%^%^CYAN%^t%^BOLD%^%^BLACK%^r%^RESET%^%^CYAN%^ees%^BOLD%^%^BLUE%^ slithering across the road. Here the road is cobbled and the large %^CYAN%^s%^BOLD%^%^BLACK%^t%^BLACK%^o%^RESET%^%^CYAN%^n%^BOLD%^%^BLACK%^e%^RESET%^%^CYAN%^s%^BOLD%^%^BLUE%^ on either side of it are engraved with runes telling of the great city of Antioch and blessing it with peace, luck and wealth. A beautiful %^BOLD%^%^WHITE%^shrine%^BOLD%^%^BLUE%^ stands to the north.  To the southwest a road leads around the city.\n" 
 	);
 	set_smell("default","You can smell the scents of a large city.");
 	set_listen("default","You can hear the voices of the guards up ahead and music drifting down from the shrine to the north.");
@@ -27,8 +28,8 @@ void create()
 		({"road","little road","path","trail"}) : "The road is paved with cobbled stones here and leads to the gates of Antioch.",
 		({"stones","large stones"}) : "Large flat stones have been placed on either side of the road and are engraved with runes telling of Antioch's greatness and blessing it with peace, luck and wealth",
 		({"trees","tree","pines"}) : "The trees are thick and tall but thin out up ahead where the city is.",
+		({"shrine","beautiful shrine"}) : "A beautiful shrine dedicated to Jarmila stands to the north, within a lush garden.",
 	]));
-// Everything above this line is error-free. Remove this line. Kismet.
 	set_exits(([
 		"northeast" : ROOMS+"trail5",
 		"north" : "/d/magic/temples/jarmila/jarmilaentry",
--- d/antioch/wild/forest/trail6.c
+++ d/antioch/wild/forest/trail6.c
@@ -9,56 +9,60 @@
 #include "/d/antioch/areadefs.h"
 inherit ROOM;
 
-void create() {
-  ::create();
-   set_terrain(HEAVY_FOREST);
-   set_travel(DIRT_ROAD);
+void create()
+{
+    ::create();
+    set_terrain(HEAVY_FOREST);
+    set_travel(DIRT_ROAD);
     set_climate("mountain");
-  set_property("light", 2);
-  set_property("indoors", 0);
-  set_property("no castle", 1);
-  set("short","A trail");
-  set("long", "This is a trail leading through a dense forest.  Although "
-    "the area in general is quite unsettling, there is a slightly different "
-    "feel here.  Trees and heavy brush line the trail but open to a temple to "
-    "the northwest.  A tree that used to block the trail to the northeast has "
-    "been chopped down and moved to the side.  The trail northeast looks very "
-    "dark and foreboding.\n"
-  );
-  set_exits( ([
-    "south": FOREST+"trail5",
-    "grove":"/d/magic/temples/eldath",
-    "northeast" : "/d/antioch/valley/rooms/dt1",
-    "brush" : "/d/player_houses/abrahil/yard",    
-    ]) );
-  add_invis_exit("brush");
-  add_pre_exit_function("go_thru", "brush");
-  set_search("brush", "The brush to the west looks like you could get through "
-     "it." );
-  set_search("default", "You notice the brush to the west looks thinner than "
-     "the rest.");
-  set_smell("default","A soft breeze from the west smells fresh and clean."  );
-  set_listen("default","You hear the sounds of a bubbling brook nearby.");
-  set_items( (["brush" : "The brush looks thinner to the west.", 
-       "trees" : "Trees, which have been here for years are thinner to the west.",
-       "tree" : "Just a tree blown down by the wind that"+
-         " has been chopped up and moved to the west side of the trail.",
-        "birds" : "All types and varieties, singing and flying around.",
-        "flowers" : "It seems as if they had grown here forever.",
-        "flower" : "A nice looking one.",
-     ({ "forest", "east" }) : "A mystical forest of pure evil.",
-    ]) );
+    set_property("light", 2);
+    set_property("indoors", 0);
+    set_property("no castle", 1);
+    set("short", "A trail");
+    set("long", "This is a trail leading through a dense forest.  Although "
+        "the area in general is quite unsettling, there is a slightly different "
+        "feel here.  Trees and heavy brush line the trail but open to a temple to "
+        "the northwest.  A tree that used to block the trail to the northeast has "
+        "been chopped down and moved to the side.  The trail northeast looks very "
+        "dark and foreboding.\n"
+        );
+    set_exits(([
+                   "south" : FOREST + "trail5",
+                   "grove" : "/d/magic/temples/eldath",
+                   "northeast" : "/d/antioch/valley/rooms/dt1",
+                   "brush" : "/d/player_houses/abrahil/yard",
+               ]));
+    add_invis_exit("brush");
+    add_pre_exit_function("go_thru", "brush");
+    set_search("brush", "The brush to the west looks like you could get through "
+               "it.");
+    set_search("default", "You notice the brush to the west looks thinner than "
+               "the rest.");
+    set_smell("default", "A soft breeze from the west smells fresh and clean.");
+    set_listen("default", "You hear the sounds of a bubbling brook nearby.");
+    set_items((["brush" : "The brush looks thinner to the west.",
+                "trees" : "Trees, which have been here for years are thinner to the west.",
+                "tree" : "Just a tree blown down by the wind that" +
+                " has been chopped up and moved to the west side of the trail.",
+                "birds" : "All types and varieties, singing and flying around.",
+                "flowers" : "It seems as if they had grown here forever.",
+                "flower" : "A nice looking one.",
+                ({ "forest", "east" }) : "A mystical forest of pure evil.",
+               ]));
 }
 
-int go_thru() {
-   tell_object(TP,"The brush parts easily as you move westward through it.");
-   tell_room(ETP,"The brush to the west seems to part easily as "+TPQN+" moves "
-      "through it.");
-   return 1;
+int go_thru()
+{
+    tell_object(TP, "The brush parts easily as you move westward through it.");
+    tell_room(ETP, "The brush to the west seems to part easily as " + TPQN + " moves "
+              "through it.");
+    return 1;
 }
 
-void reset(){
-   ::reset();
-      if(!present("sign"))
-      new("/d/common/obj/areas/darktrail_sign.c")->move(TO);
+void reset()
+{
+    ::reset();
+    if (!present("sign")) {
+        new("/d/common/obj/areas/darktrail_sign.c")->move(TO);
+    }
 }
