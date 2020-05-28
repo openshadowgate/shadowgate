4f6e70b409577d0a12f0cd04fd99fcf754891579
shadowgate <law@shadowgate.org>
2020-05-28 21:00:02 +0000

    autocommit 202005280021

--- cmds/spells/s/_siphon_magic.c
+++ cmds/spells/s/_siphon_magic.c
@@ -123,7 +123,9 @@ void spell_effect(int prof)
 
     fob->remove_property("enchantment");
     tob->remove_property("enchantment");
+    if(tob->query_property("player enchantment")){
     tob->remove_property("player enchantment");
+    }
     tob->set_property("enchantment", abs(enchantment));
     tell_object(caster, "%^BOLD%^%^MAGENTA%^You place " + fob->query_short() + "%^BOLD%^%^MAGENTA%^ over " + tob->query_short() + "%^BOLD%^%^MAGENTA%^ and melt a golden pile beneath them, the vapor transfers the magic.%^RESET%^");
     tell_room(caster, "%^BOLD%^%^MAGENTA%^" + caster->QCN + " places " + fob->query_short() + "%^BOLD%^%^MAGENTA%^ over " + tob->query_short() + "%^BOLD%^%^MAGENTA%^ and melts a pile of gold beneath them into a vapor that carries magic.%^RESET%^", ({ caster }));
