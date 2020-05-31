0a9d8c9f81be5addf64e50c886deb689ac3a037f
shadowgate <law@shadowgate.org>
2020-05-31 15:54:12 +0000

    Find_object_or_load fix

--- adm/simul_efun/find_object_or_load.c
+++ adm/simul_efun/find_object_or_load.c
@@ -16,7 +16,6 @@
    If anyone changes anything with this efun, keep this in mind. ** Saide **
 =====================================================================*/
 #include <security.h>
-#include <daemons.h>
 
 /**
  * @file
@@ -28,48 +27,49 @@
  * @param str File descriptor of an object
  * @return Object's descriptor.
  */
-object find_object_or_load (mixed str)
+object find_object_or_load(string str)
 {
-    object ob, ns;
-    string err;
+    object ob;
 
-    if(objectp(str))
-    {
-        if(!str->is_room()) return str;
-        ns = ALT_WORLD_D->find_alt(str);
-        if(objectp(ns)) str = ns;
-        if(!str->query_reset_number()) { str->reset(); }
-        if(!str->is_control_room()) return str;
-        if((int)str->query_reset_number() != (int)str->query_property("lastresetmobs")) { str->place_mobs(); }
-        return str;
-    }
     ob = find_object(str);
-    if (ob != 0)
-    {
-        if(!objectp(ob)) return ob;
-        if(!ob->is_room()) return ob;
-        if(!ob->query_reset_number()) { ob->reset(); }
-        ns = ALT_WORLD_D->find_alt(str, ob->query("alternative world"));
-        if(objectp(ns)) ob = ns;
-        if(!ob->is_control_room()) return ob;
-        if((int)ob->query_reset_number() != (int)ob->query_property("lastresetmobs")) { ob->place_mobs(); }
-        if(clonep(ob)) return ob;
+    if (ob != 0) {
+        if (!objectp(ob)) {
+            return ob;
+        }
+        if (clonep(ob)) {
+            return ob;
+        }
+        if (!ob->is_room()) {
+            return ob;
+        }
+        if (!ob->query_reset_number()) {
+            ob->reset();
+        }
+        if (!ob->is_control_room()) {
+            return ob;
+        }
+        if ((int)ob->query_reset_number() != (int)ob->query_property("lastresetmobs")) {
+            ob->place_mobs();
+        }
         return ob;
     }
     seteuid(UID_ROOT);
-    if(!file_exists(str)) { err = catch(call_other(str, "???")); }
-    else if(!file_exists(str + ".c")) { err = catch(call_other(str, "???")); }
-    else call_other(str,"???");
-    if(err) { log_file("debug.log", "**ERROR: "+err+" -- when trying find_object_or_load(\""+str+"\""); }
-    //seteuid(0);
+    catch(call_other(str, "???"));
+    seteuid(0);
     ob = find_object(str);
-    if(!objectp(ob)) return ob;
-    if(!ob->is_room()) return ob;
-    ns = ALT_WORLD_D->find_alt(ob, ob->query("alternative world"));
-    if(objectp(ns)) ob = ns;
+    if (!objectp(ob)) {
+        return ob;
+    }
+    if (clonep(ob)) {
+        return ob;
+    }
+    if (!ob->is_room()) {
+        return ob;
+    }
     ob->reset();
-    if(clonep(ob)) return ob;
-    if(!ob->is_control_room()) return ob;
+    if (!ob->is_control_room()) {
+        return ob;
+    }
     ob->place_mobs();
     return ob;
 }
