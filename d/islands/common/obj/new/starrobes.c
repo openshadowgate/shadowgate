0c1576a5dd42cbf69ce1246daaf2cfd2c758d1f2
brantlymedders <brantlymedders@gmail.com>
2020-06-01 13:29:52 -0400

    Fix to allow oracle to wear star robes.

--- d/islands/common/obj/new/starrobes.c
+++ d/islands/common/obj/new/starrobes.c
@@ -2,7 +2,7 @@
 // added auto-close of an existing rift if the robe is removed from wearer. Nienne, 11/12.
 #include <std.h>
 
-#define CLASSES_ALLOWED ({ "mage","cleric","druid","sorcerer","psion","bard" })
+#define CLASSES_ALLOWED ({ "mage","cleric","druid","sorcerer","psion","bard","oracle" })
 
 
 inherit "/d/common/obj/clothing/magerobe.c";
