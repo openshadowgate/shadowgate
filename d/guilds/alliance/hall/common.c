3b86b89e225a760b5402e7f6cd476344bd4a4214
shadowgate <law@shadowgate.org>
2020-06-04 21:33:01 +0000

    autocommit 202006043321

--- d/guilds/alliance/hall/common.c
+++ d/guilds/alliance/hall/common.c
@@ -19,7 +19,7 @@ void create()
     set_indoors(1);
     set_light(3);
     set_property("teleport proof", 67);
-    set_short("Quarters Common Area");
+    set_short("%^BOLD%^%^CYAN%^Quarters Common Area%^RESET%^");
     set_long("\n\
 %^CYAN%^Quarters Common Area%^RESET%^\n" +
              "This is the common area for the various living quarters in the Alliance Reborn Keep. The area is very spacious and comfortable, and serves as a nice lounging area, with leather couches and cushioned chairs arranged around a beautiful stone fireplace. Above the fireplace is a beautiful painting depicting a group of heroes standing tall and shining in a background of darkness. On the fireplace mantle is a cigar box, for special occasions. On a small table in one of the corners is a magical decanter with a gentle amber liquid withing it and several cups arranged neatly.");
--- d/guilds/alliance/hall/quarterswest.c
+++ d/guilds/alliance/hall/quarterswest.c
@@ -1,8 +1,8 @@
 /*
   quarterswest.c
-  
+
   Written for the new guild Alliance Reborn.
-  
+
   -- Tlaloc -- 3.24.20
 */
 
@@ -13,20 +13,20 @@ inherit VAULT;
 void create()
 {
     ::create();
-    
+
     set_terrain(VILLAGE);
     set_travel(PAVED_ROAD);
     set_indoors(1);
     set_light(3);
     set_property("teleport proof", 67);
-    set_short("Quarters Common Area");
+    set_short("%^YELLOW%^Yellow Quarters%^RESET%^");
     set_long("\n\
 %^YELLOW%^Yellow Quarters - Squad Bay%^RESET%^\n"+
 "This is a large room meant to house up to twelve people in relative comfort. It is a squad bay setup with two rows of three bunks, complete with footlockers. There is a small stone fireplace on the far wall with a mantle holding the crest of the Alliance Reborn. Around the fire is a set of simple wooden chairs. ");
     set_exits( ([
                  "east" :"/d/guilds/alliance/hall/common",
                ]) );
-               
+
     set_door("yellow door", "/d/guilds/alliance/hall/common", "east");
-    
+
 }
