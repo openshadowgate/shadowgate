36788abb7299746d706a81ccf79310542507dfb7
shadowgate <law@shadowgate.org>
2020-06-15 21:00:03 +0000

    autocommit 202006150021

--- d/common/obj/misc/street_light.c
+++ d/common/obj/misc/street_light.c
@@ -2,9 +2,7 @@
 #include <daemons.h>
 inherit OBJECT;
 
-// int original_light;  not used so zapping it  *Styx*
 // adding sets so ones inheriting this can have diff. day/night shorts *Styx* 12/24/03
-
 void set_nightShort(string str) { set("nightShort", str);  }
 void set_dayShort(string str) {  set("dayShort", str);  }
 
@@ -16,27 +14,22 @@ void create() {
     set_dayShort("A dark street light");
     set_nightShort("%^BOLD%^%^WHITE%^A lit street light%^RESET%^");
     set_weight(1000000);
-    set_long("The street light is made of wrought iron and stands about"
-	" ten feet high.  It appears to be an oil fueled lamp.  It lights "
-        "up the streets at night."
-    );
+    set_long("The street light is made of wrought iron and stands about ten feet high.  It appears to be an oil fueled lamp.  It lights up the streets at night.");
     set_value(0);
     set_property("no animate", 1);   // making this standard for street lights *Styx* 12/03
-//    original_light =  -9999;
 }
 
 void init() {
     string tod;
-   ::init();   // don't know how we missed having this but adding it now, Styx 12/24/03
+    ::init(); // don't know how we missed having this but adding it now, Styx 12/24/03
     if(!interactive(TP)) return;
     tod = EVENTS_D->query_time_of_day();
-    //burning that lamp fuel all the time until eternal night is over -- Yves
-    // if((tod == "night") && !query_property("light")) {
+    if((tod == "night") && !query_property("light")) {
         set_property("light",3);
         set_short(query("nightShort"));
-    // }
-    // if((tod != "night") && query_property("light")){
-    //     remove_property("light");
-    //     set_short(query("dayShort"));
-    // }
+    }
+    if((tod != "night") && query_property("light")){
+        remove_property("light");
+        set_short(query("dayShort"));
+    }
 }
