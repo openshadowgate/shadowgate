e98f4415013efc254a2e51f70a77adc4ef516970
brantlymedders <brantlymedders@gmail.com>
2020-06-21 10:12:53 -0400

    giant versions for crafting

---
+++ d/common/obj/weapon/giant_double_axe.c
@@ -0,0 +1,27 @@
+#include <std.h>
+inherit "/std/weapon";
+
+void create() {
+   ::create();
+   set_name("giant double axe");
+   set_id(({"axe", "double axe"}));
+   set_short("giant double axe");
+   set_long(
+      "This axe was built from strong metals to form a sturdy shaft between"+
+     " the two full battle axes connected to each end of the weapon. This allows"+
+      " a well trained person to use both axes in combat."
+   );
+   set_value(50);
+   set_size(3);
+   set_type("slashing");
+   set_prof_type("double axe");
+   set_weapon_prof("exotic");
+   set_wc(3,8);  //putting this a die higher than TT to reflect that we lack tech to make it usable as two weapons without needing ambidexterity - Odin
+   set_large_wc(3,8); //putting this a die higher than TT to reflect that we lack tech to make it usable as two weapons without needing ambidexterity - Odin
+   set_weight(20);
+   set_weapon_speed(11);
+   set_critical_threat_range(1);
+   set_critical_hit_multiplier(3);
+   set_damage_type("slashing");
+   set_property("repairtype",({"weaponsmith"}));
+}
---
+++ d/common/obj/weapon/giant_double_bladed_sword.c
@@ -0,0 +1,27 @@
+#include <std.h>
+inherit "/std/weapon";
+void create() {
+   ::create();
+   set_id(({"sword", "double bladed sword"}));
+   set_name("giant double bladed sword");
+   set_short("giant double bladed sword");
+   set_long(
+      "This sword consist of steel and wood. The shaft of the weapon is crafted"+
+      " in a fashion similar to a thick staff. Set on both sides of the staff are"+
+      " long blades so that the wielder can use either side of the weapon"+
+      " to attack, giving weapon much versatility in combat."
+   );
+   set_weight(20);
+   set_size(3);
+   set_value(50);
+   set_wc(3,8); //putting this a die higher than TT to reflect that we lack tech to make it usable as two weapons without needing ambidexterity
+    set_type("slashing");
+   set_large_wc(3,8); //putting this a die higher than TT to reflect that we lack tech to make it usable as two weapons without needing ambidexterity
+   set_weapon_prof("exotic");
+   set_weapon_speed(10);
+
+   set_critical_threat_range(2);
+   set_critical_hit_multiplier(2);
+   set_damage_type("slashing");
+   set_property("repairtype",({"weaponsmith"}));
+}
