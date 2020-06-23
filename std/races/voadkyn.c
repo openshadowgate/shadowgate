ec8a7d9a3a08c8bf7ea2c1eee776178cd791bdb8
brantlymedders <brantlymedders@gmail.com>
2020-06-22 20:14:11 -0400

    Voadkyn patch.

--- std/races/voadkyn.c
+++ std/races/voadkyn.c
@@ -20,7 +20,7 @@ string *restricted_deities(string subrace) {
 // stats in order: str, dex, con, int, wis, cha
 int *stat_mods(string subrace) { return ({ 2, 4, -2, 0, -2, 0 }); }
 
-mapping skill_mods(string subrace) { return ([ "stealth" : 2 ]); }
+mapping skill_mods(string subrace) { return ([ "stealth" : 2, "survival" : 2  ]); }
 
 int level_adjustment(string subrace) { return 0; }
 
