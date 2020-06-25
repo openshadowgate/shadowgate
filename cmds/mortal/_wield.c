6acd8ba6243d219760ad55962637891d0371cc88
nm0i <nm0i@me0w.net>
2020-06-25 05:31:52 +0300

    https://shadowgate.org/git/shadowgate/shadowgate/issues/339

--- cmds/mortal/_wield.c
+++ cmds/mortal/_wield.c
@@ -4,7 +4,7 @@ inherit DAEMON;
 
 //void do_wear_stat(object myplayer, object myitem, string mystat, int mymod);
 
-int cmd_wield(string str) 
+int cmd_wield(string str)
 {
     object ob, *obs;
     string what, hand1, hand2, ok, *wielder, wieldf,ob_quest,*player_quests;
@@ -14,7 +14,7 @@ int cmd_wield(string str)
 
     if(!str) return notify_fail("Wield what?\n");
 
-    if (TP->query_bound()) 
+    if (TP->query_bound())
     {
         TP->send_paralyzed_message("info",TP);
         return 1;
@@ -33,19 +33,19 @@ int cmd_wield(string str)
         return 1;
     }
 
-    if(sscanf(str, "%s in %s", what, hand1) != 2) 
+    if(sscanf(str, "%s in %s", what, hand1) != 2)
     {
         what = str;
         inc = 0;
         wielder = this_player()->query_wielding_limbs();
-        while(inc < sizeof(wielder)) 
+        while(inc < sizeof(wielder))
         {
             if(this_player()->query_weapon(wielder[inc]) || member_array("shield", ((mapping)TP->query_body())[wielder[inc]]["armour"]) != -1 ||  member_array("thiefshield", ((mapping)TP->query_body())[wielder[inc]]["armour"]) != -1)
             {
                 inc ++;
                 continue;
             }
-            else 
+            else
             {
                 if(!hand1) hand1 = wielder[inc];
                 free_hands++;
@@ -55,8 +55,8 @@ int cmd_wield(string str)
         }
         if(!hand1) return notify_fail("You have no free hands.\n");
     }
-    
-    if(!(ob = present(what,TP)) ) 
+
+    if(!(ob = present(what,TP)) )
     {
         return notify_fail("That is not here.\n");
     }
@@ -69,7 +69,7 @@ int cmd_wield(string str)
             return 1;
         }
     }
-    
+
     if(ob->query_property("quest required") && interactive(TP))
     {
         ob_quest = ob->query_property("quest required");
@@ -82,23 +82,23 @@ int cmd_wield(string str)
             tell_object(TP,"You have not earned the right to make use of this item");
             return 1;
         }
-        
+
         for(i=0;i<sizeof(player_quests);i++)
         {
             player_quests[i] = FILTERS_D->filter_colors(player_quests[i]);
-            player_quests[i] = lower_case(player_quests[i]);            
+            player_quests[i] = lower_case(player_quests[i]);
         }
-        
+
         if(member_array(ob_quest, player_quests) == -1)
         {
             tell_object(TP,"You have not earned the right to make use of this item");
             return 1;
         }
     }
-    
+
     if(ob->query_property("master weapon"))
     {
-        obs = TP->query_wielded();        
+        obs = TP->query_wielded();
         for(i=0;sizeof(obs),i<sizeof(obs);i++)
         {
             if(!objectp(obs[i])) { continue; }
@@ -107,26 +107,26 @@ int cmd_wield(string str)
             return 1;
         }
     }
-        
-    if(ob->query_property("monsterweapon") && userp(TP)) 
+
+    if(ob->query_property("monsterweapon") && userp(TP))
     {
         ob->remove();
         return 1;
     }
-    
+
     if(ob->is_bow()) return 0;
-    if(ob->query_worn()) 
+    if(ob->query_worn())
     {
         return notify_fail("You can't wield something you're wearing.\n");
     }
 
-    if(ob->query_wielded()) 
+    if(ob->query_wielded())
     {
         message("my_action", "You are already wielding that!\n",this_player());
         return 1;
     }
 
-    if(!ob->wieldable() && interactive(TP)) 
+    if(!ob->wieldable() && interactive(TP))
     {
         message("my_action", "Your class will not allow you to wield that!\n",this_player());
         return 1;
@@ -134,7 +134,7 @@ int cmd_wield(string str)
     if(TP->query_casting()){ //new fix to stop stacking spells with wear/wield in a round - Nienne, 12/09
       message("my_action", "You are too busy casting a spell to wield that!",this_player());
       return 1;
-    } 
+    }
     its_size = ob->query_size();
     my_size = (int)this_player()->query_size();
     if(my_size >= its_size) size_diff = 0;
@@ -143,12 +143,12 @@ int cmd_wield(string str)
          FEATS_D->usable_feat(TP,"titan grip") &&
          !sizeof(TP->query_wielded()))
         size_diff-=1;
-    if(size_diff > 1) 
+    if(size_diff > 1)
     {
         write("This weapon is too big for you, even with both hands!");
         return 1;
     }
-    if(size_diff > 0 && free_hands < 2) 
+    if(size_diff > 0 && free_hands < 2)
     {
         write("You do not have enough free hands!");
         return 1;
@@ -158,8 +158,8 @@ int cmd_wield(string str)
     if(ob->query_property("enchantment"))
     //      if(level/5 < ob->query_property("enchantment")) {
     //changing to +1/7 levels
-    
-    if (userp(TP) && (level/5 < ob->query_property("enchantment")) ) 
+
+    if (userp(TP) && (level/5 < ob->query_property("enchantment")) )
     {
         message("my_action", "You are not experienced enough to utilize this equipment.",TP);
         return 1;
@@ -176,12 +176,12 @@ int cmd_wield(string str)
         }
     }
     if(sizeof(obs) && ob->query_property("lance")) { tell_object(TP, "You cannot dual wield with a lance."); return 1; }
-    wieldf = ob->query_wield();    
+    wieldf = ob->query_wield();
 
-    if(size_diff > 0) 
+    if(size_diff > 0)
     {
         str = hand1;
-        if(sscanf(str, "%s and %s", hand1, hand2) != 2)        
+        if(sscanf(str, "%s and %s", hand1, hand2) != 2)
         {
         //            message("my_action", "You must wield such a weapon with two hands.\n",
         //              this_player());
@@ -190,7 +190,7 @@ int cmd_wield(string str)
             hand2 = "left hand";
         }
 
-        if(hand1 == hand2) 
+        if(hand1 == hand2)
         {
             message("my_action","Ummm care to try again?",this_player());
             return 1;
@@ -203,7 +203,7 @@ int cmd_wield(string str)
         f = (:call_other,ob,wieldf:);
         if(ob->query_property("fwield"))
         {
-            if(!((*f)())) 
+            if(!((*f)()))
             {
                 return 1;
             }
@@ -212,19 +212,19 @@ int cmd_wield(string str)
     }
 
 
-    else 
+    else
     {
-        if(sscanf(hand1, "%s and %s", str, hand2) == 2) 
+        if(sscanf(hand1, "%s and %s", str, hand2) == 2)
         {
             message("my_action", "You must only use one hand to wield that "+
                 "weapon.", this_player());
             return 1;
         }
-        
+
         f = (:call_other, ob, wieldf:);
         if(ob->query_property("fwield"))
         {
-            if(!((*f)())) 
+            if(!((*f)()))
             {
                 return 1;
             }
@@ -232,17 +232,17 @@ int cmd_wield(string str)
      	ok  = (string)this_player()->equip_weapon_to_limb(ob,hand1,0);
     }
 
-    if(ok) 
+    if(ok)
     {
         message("my_action", ok, this_player());
         return 1;
     }
-    
+
     if(stringp(wieldf) && !ob->query_property("fwield"))
         message("my_action", wieldf, this_player());
-    else 
+    else
         message("my_action", "You wield "+ob->query_short()+".", this_player());
-    
+
     if(objectp(ETP))
     {
         message("other_action", (string)this_player()->query_cap_name() +
@@ -252,6 +252,12 @@ int cmd_wield(string str)
 	POISON_D->is_object_poisoned(ob, TP, "wield", 1);
     ob->set_wielded(this_player());
     ob->set_limbString(" (wielded in " + hand1 + (hand2 ? " and "+hand2+")" : ")"));
+
+    // https://shadowgate.org/git/shadowgate/shadowgate/issues/339
+
+    if (item_size > 1) {
+       ob->reset_hidden_seen();
+    }
 
     //   new function for running stat adjustments on item wear.
 /*    if((int)ob->query("strbonus")) do_wear_stat(TP,ob,"strbonus",(int)ob->query("strbonus"));
@@ -264,17 +270,17 @@ int cmd_wield(string str)
     // new code for item bonus properties to replace all existing props/funcs. Nienne, 7/12.
 	if(itembonuses = ob->query_item_bonuses()) ob->run_item_bonuses("equip",TP,itembonuses);
 
-    if((string)ob->query_prof_type() == "magic weapons") 
+    if((string)ob->query_prof_type() == "magic weapons")
     {
         TP->set_skill("magic weapons",100);
     }
 
-    if((string)ob->query_prof_type() == "unknown") 
+    if((string)ob->query_prof_type() == "unknown")
     {
         tell_object(TP,"Error: Please contact a wiz.. weapon type is unknown.");
     }
-    
-    else if(ob->query_prof_type() && !TP->query_skill(ob->query_prof_type())) 
+
+    else if(ob->query_prof_type() && !TP->query_skill(ob->query_prof_type()))
     {
         TP->add_skill(ob->query_prof_type(),1);
     }
@@ -305,4 +311,3 @@ N.B. Shields are worn, not weld, as they are armour pieces. Attempt to wield shi
 unwield, wear, remove, inv, eq, limbs, look
 ");
 }
-
