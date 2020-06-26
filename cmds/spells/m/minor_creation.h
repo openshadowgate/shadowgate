b6be7462c5b095b8e8af9f3798fb3bd91dbc537b
brantlymedders <brantlymedders@gmail.com>
2020-06-25 20:34:28 -0400

    Minor creation updates to include more swag.

--- cmds/spells/m/minor_creation.h
+++ cmds/spells/m/minor_creation.h
@@ -1,13 +1,16 @@
 #include "/d/common/common.h"
 
 #define STUFF (([ \
+                    "armorsmith toolset" : CMISC "tools/armorsmithtoolset", \
                     "bandage" : CMISC "bandage", \
                     "climbing tools" : CMISC "climb_tool", \
                     "dagger" : CWEAP "dagger", \
                     "dark lantern" : CMISC "darkbeacon", \
                     "dark oil" : CMISC "darkoil", \
                     "fishing pole" : CMISC "fishing_pole", \
+                    "foraging kit" : CMISC "tools/foragekit", \
                     "flint" : CMISC "flint", \
+                    "jewelling kit" : CMISC "tools/jewellingkit", \
                     "lantern" : CMISC "lantern", \
                     "large sack" : CMISC "sack", \
                     "mace" : CWEAP "mace", \
@@ -17,9 +20,12 @@
                     "robe" : CARMOR "robe", \
                     "rope" : CMISC "rope", \
                     "scalemail" : CARMOR "scale", \
+                    "sewing kit" : CMISC "tools/sewingkit", \
                     "shield" : CARMOR "shield", \
                     "small sack" : CMISC "ssack", \
                     "soap" : CMISC "soap", \
+                    "locksmith tools" : CMISC "thief_tools", \
                     "torch" : CMISC "torch", \
+                    "weaponsmith toolset" : CMISC "tools/weaponsmithtoolset", \
                     "writing quill" : CMISC "tools/quill4", \
                 ]))
--- d/common/obj/misc/thief_tools.c
+++ d/common/obj/misc/thief_tools.c
@@ -3,17 +3,17 @@
 inherit OBJECT;
 void create(){
         ::create();
-        set_name("Thief's tools");
-        set_id(({"tools","thief toolx", "thief tools", "lock picks","picks"}));
-//set_short("A leather case of tools"); //based on a suggestions by the players - cythera 6/05\\
-set_short("Thief's tools"); //restoring Thief's tools to their former glory - Uriel 1/2020
+        set_name("locksmith tools");
+        set_id(({"tools","thief toolx", "thief tools", "lock picks", "locksmith tools" "picks"}));
+set_short("A leather case of tools"); //based on a suggestions by the players - cythera 6/05\\
+//set_short("Thief's tools"); //restoring Thief's tools to their former glory - Uriel 1/2020
         set_long(
 @CIRCE
-This is a small, soft leather case that can be rolled and tied 
-closed with leather thongs.  The case, when fully opened, reveals 
-rows of pouches and slots filled with various skeleton keys, long 
-metal picks and pries, a small handsaw for especially stubborn 
-locks.  It even has a small hammer with a metal wedge.  These 
+This is a small, soft leather case that can be rolled and tied
+closed with leather thongs.  The case, when fully opened, reveals
+rows of pouches and slots filled with various skeleton keys, long
+metal picks and pries, a small handsaw for especially stubborn
+locks.  It even has a small hammer with a metal wedge.  These
 tools would probably aid any locksmith.
 CIRCE
 );
@@ -22,17 +22,17 @@ CIRCE
 }
 void break_picks(){
 	TO->set_id(({"tools","broken tool","lock picks","picks"}));
-//TO->set_short("A leather case of broken tools");
-TO->set_short("broken Thief's tools");
+TO->set_short("A leather case of broken tools");
+//TO->set_short("broken Thief's tools");
 	TO->set_long(
 @CIRCE
-This soft leather case has many pouches that were once filled 
-with tools.  Now, however, many of the tools are missing or 
-broken, making this set completely worthless for opening a 
+This soft leather case has many pouches that were once filled
+with tools.  Now, however, many of the tools are missing or
+broken, making this set completely worthless for opening a
 stubborn lock.
 CIRCE
 );
 	TO->set_value(0);
       TO->set_property("broken",1);
-	
+
 }
