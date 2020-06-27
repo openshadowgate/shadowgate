2dbe1efd047964280dd5a0fe4f754a2cdbae813a
brantlymedders <brantlymedders@gmail.com>
2020-06-27 17:00:33 -0400

    Adding to mage list as per PFSRD

--- cmds/spells/d/_daylight.c
+++ cmds/spells/d/_daylight.c
@@ -8,7 +8,7 @@ inherit SPELL;
 void create() {
     ::create();
     set_spell_name("daylight");
-    set_spell_level(([ "cleric" : 3, "bard" : 3, "paladin" : 3,"druid" : 3, "inquisitor" : 3,"oracle":3]));
+    set_spell_level(([ "cleric" : 3, "bard" : 3, "paladin" : 3,"druid" : 3, "inquisitor" : 3,"oracle":3, "mage":3]));
     set_spell_sphere("invocation_evocation");
     set_mystery(({"heavens","solar"}));
     set_syntax("cast CLASS daylight");
