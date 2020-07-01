36e213ff13636bd870518abfb765387d5631de40
shadowgate <law@shadowgate.org>
2020-07-01 23:00:02 +0000

    autocommit 202007010023

--- cmds/mortal/_teach.c
+++ cmds/mortal/_teach.c
@@ -169,7 +169,7 @@ void step(int when, object who, object targ, string what)
             // base 5% increase + bonuses, this doesn't appear to work even when I up the 5% base to 50% base, something with add_grammar is off - Odin 7/1/2020
             targ->add_grammar(what,
                               BONUS_D->query_stat_bonus(who,"intelligence") * 75 +
-                              BONUS_D->query_stat_bonus(targ, "intelligence") * 75 + 50000);
+                              BONUS_D->query_stat_bonus(targ, "intelligence") * 75 + 500);
 
             who->remove_property("teaching");
             targ->remove_property("taught");
@@ -181,7 +181,7 @@ void step(int when, object who, object targ, string what)
         // base 5% increase + bonuses, this doesn't appear to work even when I up the 5% base to 50% base, something with add_grammar is off - Odin 7/1/2020
         targ->add_grammar(what,
                           BONUS_D->query_stat_bonus(who, "intelligence") * 75 +
-                          BONUS_D->query_stat_bonus(targ, "intelligence") * 75 + 50000);
+                          BONUS_D->query_stat_bonus(targ, "intelligence") * 75 + 500);
 
         who->remove_property("teaching");
         targ->remove_property("taught");
