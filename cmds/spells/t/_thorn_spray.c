b4621e7f6d8c0ea32f3f8aa95e815b7a23dc2644
nm0i <nm0i@me0w.net>
2020-07-03 22:17:58 +0300

    Fix to #402

--- cmds/spells/t/_thorn_spray.c
+++ cmds/spells/t/_thorn_spray.c
@@ -1,17 +1,15 @@
-// Thorn Spray
-
 #include <std.h>
 
 inherit SPELL;
 
 void dest_effect();
 
-void create() {
+void create()
+{
     ::create();
     set_spell_name("thorn spray");
-    set_spell_level(([ "classless" : 7 ]));
-    set_spell_sphere("plant");
-    set_spell_domain("plant");
+    set_spell_level(([ "cleric" : 7, "druid" : 6]));
+    set_spell_sphere("conjuration_summoning");
     set_syntax("cast CLASS thorn spray on TARGET");
     set_description("This spell will cause a number of sharp thorns to fly from the caster's hand toward the target.");
     set_verbal_comp();
@@ -25,17 +23,19 @@ void spell_effect(int prof)
 
     numdarts = sdamage;
 
-    tell_object(caster,"%^ORANGE%^Sharp thorns shoot from your hand at "+target->QCN+"!");
-    tell_object(target,"%^ORANGE%^Sharp thorns shoot from "+caster->QCN+"'s hand at you!");
-    tell_room(place,"%^ORANGE%^Sharp thorns shoot from "+caster->QCN+"'s hand at "+target->QCN+"!",({caster, target}) );
+    tell_object(caster, "%^ORANGE%^Sharp thorns shoot from your hand at " + target->QCN + "!");
+    tell_object(target, "%^ORANGE%^Sharp thorns shoot from " + caster->QCN + "'s hand at you!");
+    tell_room(place, "%^ORANGE%^Sharp thorns shoot from " + caster->QCN + "'s hand at " + target->QCN + "!", ({ caster, target }));
 
-    damage_targ(target, target->query_target_limb(), numdarts,"piercing");
-    spell_kill(target,caster);
+    damage_targ(target, target->query_target_limb(), numdarts, "piercing");
+    spell_kill(target, caster);
     dest_effect();
 }
 
 void dest_effect()
 {
     ::dest_effect();
-    if(objectp(TO)) TO->remove();
+    if (objectp(TO)) {
+        TO->remove();
+    }
 }
