d20c256bc5a9e14870e559659e08662df4ca3014
nm0i <nm0i@me0w.net>
2020-07-04 23:11:52 +0300

    Fix to #441.

--- d/islands/common/obj/new/bag.c
+++ d/islands/common/obj/new/bag.c
@@ -3,7 +3,7 @@
 inherit "/std/bag_logic";
 
 
-void create() 
+void create()
 {
     ::create();
     set_id(({"bag","sack","bag of holding"}));
@@ -11,13 +11,12 @@ void create()
     set_short("%^BLUE%^Bag of permanent holding%^RESET%^");
     set_obvious_short("%^BLUE%^%^BOLD%^A blue bag decorated with %^YELLOW%^r%^WHITE%^u%^YELLOW%^nes%^RESET%^");
     set_weight(5);
-    
+
     set_long("%^WHITE%^%^BOLD%^This is a small bag that you can put things in, made of some unknown, but quite "
         "resiliant, %^BLUE%^cobalt-blue cloth%^WHITE%^.  Around the opening of the bag appear %^YELLOW%^orn%^WHITE%^a"
         "%^YELLOW%^te ru%^WHITE%^n%^YELLOW%^es%^WHITE%^ within the cloth, perhaps dyed there.  As you reach you notice "
-        "there is a lot more space in here than a normal bag of this size.%^RESET%^\nNOTE: This bag is to allow "
-        "players to carry the same amount of equipment with less sacks. Not for players to abuse.");
-        
+        "there is a lot more space in here than a normal bag of this size.%^RESET%^");
+
     set_lore("%^WHITE%^%^BOLD%^The great archmage Arkhiesan Korr is claimed to have been the first to have "
         "found the means by which to make a bag of holding permanent.  Part of the original Brotherhood of the Sixteen "
         "Sigils, he continued his research in secret after their group was disbanded.  His completed research, "
