0d825a7471f8c96e4d1dfb0ede5cbd90a45eb3d6
Yves <mana.starved@yahoo.com>
2020-07-05 18:17:38 -0700

    https://shadowgate.org/git/shadowgate/shadowgate/issues/405

--- d/attaya/newseneca/rooms/skyline44.c
+++ d/attaya/newseneca/rooms/skyline44.c
@@ -8,7 +8,7 @@ void create(){
    ::create();
    set_long(::query_long()+"To both the east and west lie %^GREEN%^"+
       "gardens %^RESET%^with monuments set among stone benches.  "+
-      "South of here is the wide %^BOLD%^town square %^RESET%^"+
+      "North of here is the wide %^BOLD%^town square %^RESET%^"+
       "at the juncture of two major roadways.\n");
    set_exits(([
       "west" : ROOMS"monument4",
