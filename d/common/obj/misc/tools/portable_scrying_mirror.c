740a16de8006590f651b538c3cd3172422eda4f3
shadowgate <law@shadowgate.org>
2020-07-07 00:00:03 +0000

    autocommit 202007070000

--- d/common/obj/misc/tools/portable_scrying_mirror.c
+++ d/common/obj/misc/tools/portable_scrying_mirror.c
@@ -19,7 +19,7 @@ create() {
 	set_property("no animate", 1);
 	set_long("This is a rather plain, but finely made mirror.  The reflective surface appears to be silver polished to an incredible lustre.  The surface is large and with the stout frame, this mirror looks to weigh quite a bit, and is even bulkier than it is heavy.");
 	set_value(10000);
-	set_weight(100000);
+	set_weight(20);
 	set_no_clean(1);
 }
 
