2b288644bedd3d1731eba72883ca26f3eba8ef4c
Yves <mana.starved@yahoo.com>
2020-07-06 20:34:53 -0700

    Third time is the charm?

--- d/darkwood/tabor/room/math10.c
+++ d/darkwood/tabor/room/math10.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <std.h>
 #include "../include/tabor.h"
 
@@ -57,63 +56,3 @@ int GoThroughDoor(){
    }
    return 1;
 }
-=======
-#include <std.h>
-#include "../include/tabor.h"
-
-inherit ROOM;
-
-void create() {
-  ::create();
-  set_travel(DIRT_ROAD);
-  set_terrain(CITY);
-  set_light(2);
-  set_name("Mathowvy Road, Tabor");
-  set_short("Mathowvy Road, Tabor");
-  set_long(
-@DAI
-%^BOLD%^Mathowvy Road, Tabor%^RESET%^
-You are on a fairly steep upward slope on Mount Tabor, approaching the
-fortress of %^YELLOW%^Caer Tabor%^RESET%^.  The road is starting to narrow here and
-you see it curve around a bend to the northwest.  Directly east
-is a large, rounded building made of what looks to be crystalline
-walls.  To the west is a small empty lot that appears to be a work in progress.  To the south, you see the slope become gentler and the
-road get wider as it leads straight into town.
-
-%^RESET%^%^MAGENTA%^Mathowvy Road %^RESET%^continues to the northwest and south.
-DAI
-    );
-  set_exits(([
-   "south" : ROOMDIR+"math9",
-    "northwest" : ROOMDIR+"math11",
-    "enter" : ROOMDIR+"psientry"
-  ]));
-  set_listen("default","You hear the muted sounds of a large town to the south.");
-  set_smell("default","The air here is clean with only faint "+
-     "scents of the town below.");
-  set_items(([
-    "town" : "You can see the buildings of the town from here.",
-    "castle" : "You see the thick, ancient walls of the fortress loom above you.",
-    "building" : "The building seems to be made of crystal!  Its walls "+
-       "and roof are rounded and seem to reflect light at all times."
-  ]));
-  set_pre_exit_functions(({"enter"}),({"GoThroughDoor"}));
- }
-
-void reset(){
-  ::reset();
-  if(!present("street_light",TO))
-    new("/d/common/obj/misc/street_light")->move(TO);
-}
-
-int GoThroughDoor(){
-   if(TP->is_class("psion") || avatarp(TP)) {
-      tell_object(TP, "As you walk through the archway, you feel a "+
-         "slight prickling sensation pass over your skin.");
-      tell_room(ETP, "The air within the archway shimmers briefly "+
-         "as "+TPQCN+" passes through it.", TP);
-        return 1;
-   }
-   return 1;
-}
->>>>>>> 5cd256ac8fbf7a68b708fa733e3677ed4d2ea560
