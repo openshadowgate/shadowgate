2d6918383348db12bb2f6a45e792da37c40f5fa4
shadowgate <law@shadowgate.org>
2020-07-11 12:33:01 +0000

    autocommit 202007113312

---
+++ cmds/spells/e/_endure_elements.c
@@ -0,0 +1,86 @@
+/*
+  _endure_elementa.c
+  
+  Ranger gains +10 Fire and Cold resistance
+  
+  -- Tlaloc -- 7.11.20
+*/
+
+#include <std.h>
+#include <spell.h>
+#include <magic.h>
+#include <daemons.h>
+
+inherit SPELL;
+
+void effect(int direction)
+{
+    if(direction > 0)
+    {
+        caster->set_property("castspellresist",1);
+    }
+    else
+    {
+        caster->remove_property("castspellresist");
+    }
+    
+    caster->set_resistance("fire", 10 * direction);
+    caster->set_resistance("cold", 10 * direction);
+
+    return;
+}
+
+void create()
+{
+    ::create();
+    
+    set_author("tlaloc");
+    set_spell_name("endure elements");
+    set_spell_level( ([ "ranger" : 1 ]) );
+    set_spell_sphere("alteration");
+    set_syntax("cast CLASS endure elements");
+    set_damage_desc("+10 to fire and cold resistance.");
+    set_description("With this spell, the caster gains enhanced protection from extreme heat and cold.");
+    set_helpful_spell(1);
+}
+
+int preSpell()
+{
+   if(caster->query_property("castspellresist") || caster->query_property("fiery body"))
+   {
+      tell_object(caster,"You already have protection of this nature!");
+      return 0;
+   }
+   
+   return 1;
+}
+
+
+string query_cast_string()
+{
+    return "%^GREEN%^" + sprintf("%s concentrates on %s woodland powers", caster->QCN, caster->query_possessive());
+}
+
+void spell_effect(int prof)
+{
+    string myname, yourname;
+    int mylevel;
+    
+    myname = caster->QCN;
+  
+    tell_object(caster, "%^GREEN%^You place a hand upon yourself, enhancing your body with enhanced protection against heat and cold.");
+    tell_room(place, "%^GREEN%^" + sprintf("%s focuses on enhancing %s body with enhanced protection!", myname, caster->query_possessive()), ({ caster }));
+
+    effect(1);
+    spell_successful();
+    addSpellToCaster();
+}
+
+void dest_effect()
+{
+    tell_object(caster, "%^GREEN%^Your protection from the elements seems to dissapate.");
+    effect(-1);
+    ::dest_effect();
+    if(objectp(this_object()))
+        this_object()->remove();
+}
