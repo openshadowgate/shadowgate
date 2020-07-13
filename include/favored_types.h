2b0ac0c105ee3486c73f0420050416f04be359bd
brantlymedders <brantlymedders@gmail.com>
2020-07-12 21:41:27 -0400

    Unifying outsiders.

--- include/favored_types.h
+++ include/favored_types.h
@@ -27,17 +27,16 @@
                          "dragonkin"  : ({ "kobold", "draconian", "dragon", "elven-dragon", "dragonet", "pseudodragon", "drake", "wyvern" }), \
                          "elementals" : ({ "elemental", "genasi", "earth genasi", "air genasi", "water genasi", "air genasi", "efreet", "djinn", "marid", "genie" }), \
                          "elves"      : ({ "elf", "half-elf", "drow", "half-drow", "szarkai" }), \
-                         "evil outsiders"   : ({ "demon", "devil", "imp", "tiefling", "githyanki", "githzerai" }), \
                          "fey"        : ({ "nymph", "satyr", "dryad", "pixie", "brownie", "feytouched", "fey" }), \
                          "giants"     : ({ "voadkyn", "firbolg", "giant", "titan" }), \
                          "goblins"    : ({ "goblin", "bugbear", "hobgoblin", "goblinoid" }), \
-                         "good outsiders" : ({ "deva", "solar", "aasimar", "angel" }), \
                          "humans"     : ({ "human", "shade", "half-elf", "half-orc", "half-ogre", "half-drow", "genasi", "feytouched", "aasimar", "tiefling" }), \
                          "lizardfolk" : ({ "yuan-ti", "saurian", "salamander", "lizardman", "firenewt" }), \
                          "magical beasts" : ({ "owlbear", "grimlock", "jackalwere", "wolfwere", "ooze", "worm", "pegataur", "griffon", "hydra", "shadow", "magical beast" }), \
                          "monstrous humanoids"   : ({ "beastkin", "wemic", "centaur", "ratkin", "minotaur", "gnoll", "kitsune", "werewolf", "wererat", "weretiger", "alaghi", "monstrous humanoid", "troll" }), \
                          "ogres"      : ({ "ogre", "ogre-mage", "half-ogre", "ogroid" }), \
                          "orcs"       : ({ "orc", "half-orc", "orog", "tanarukk", "orcoid" }), \
+                         "outsiders"  : ({ "deva", "solar", "aasimar", "angel", "demon", "devil", "imp", "tiefling", "githyanki", "githzerai", "outsider"  }), \
                          "plants" : ({ "shambler", "shambling mound", "tree", "vine", "plant" }), \
                          "smallfolk"  : ({ "gnome", "halfling", "kender", "derro", "pech", }), \
                          "undead"     : ({ "ghost", "skeleton", "lich", "zombie", "ghast", "entity", "spirit", "wraith", "wight" }), \
