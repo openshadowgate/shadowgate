123329a729d3a1ce5e094dfc8d2d5869cb332873
brantlymedders <brantlymedders@gmail.com>
2020-07-14 12:09:11 -0400

    Updates to bad races

--- d/darkwood/camps/mon/elguard.c
+++ d/darkwood/camps/mon/elguard.c
@@ -3,83 +3,97 @@
 
 inherit MONSTER;
 
-void create() {
-   ::create();
-   set_name("guard");
-   set_id(({"elf","guard"}));
-   set_short("Elven guard");
-   set_long(
-       "This elven guard is dedicated to protecting villagers and the"+
-      " paths leading to the elven village of Synoria."
-   );
-   set_class("fighter");
-   set_guild_level("fighter",15);
-   set_mlevel("fighter",15);
-   set_race("elf");
-   set_body_type("humanoid");
-   set_gender("male");
-   set_hd(15,8);
-   set_alignment(7);
-   set_max_hp(145);
-   set_hp(145);
-   set_overall_ac(2);
-   set_languages( ({ "elven" }) );
-   command("speak elven");
-   set_size(2);
+void create()
+{
+    ::create();
+    set_name("guard");
+    set_id(({ "elf", "guard" }));
+    set_short("Elven guard");
+    set_long(
+        "This elven guard is dedicated to protecting villagers and the" +
+        " paths leading to the elven village of Synoria."
+        );
+    set_class("fighter");
+    set_guild_level("fighter", 15);
+    set_mlevel("fighter", 15);
+    set_race("elf");
+    set_body_type("humanoid");
+    set_gender("male");
+    set_hd(15, 8);
+    set_alignment(7);
+    set_max_hp(145);
+    set_hp(145);
+    set_overall_ac(2);
+    set_languages(({ "elven" }));
+    command("speak elven");
+    set_size(2);
     set_exp(2500);
-   set_wielding_limbs(({"right hand","left hand"}));
-   new(WEAP+"longsword")->move(TO);
-   command("wield sword in left hand");
-   new(WEAP+"shortsword")->move(TO);
-   command("wield sword in right hand");
-   set_property("full attacks",1);
-   set_stats("strength",16);
-   set_stats("constitution",16);
-   set_stats("intelligence",15);
-   set_stats("wisdom",13);
-   set_stats("charisma",14);
-   set_stats("dexterity",18);
-   set_func_chance(45);
-   set_funcs( ({ "flashit","rushit","rushit"}) );
-   add_search_path("/cmds/fighter");
-   set("aggressive", "kill_evil");
-   remove_property("swarm");
-   	set_monster_feats(({
-	   "parry",
-       "powerattack",
-	   "shatter",
-	   "sunder",
-	   "rush"
-	}));
+    set_wielding_limbs(({ "right hand", "left hand" }));
+    new(WEAP + "longsword")->move(TO);
+    command("wield sword in left hand");
+    new(WEAP + "shortsword")->move(TO);
+    command("wield sword in right hand");
+    set_property("full attacks", 1);
+    set_stats("strength", 16);
+    set_stats("constitution", 16);
+    set_stats("intelligence", 15);
+    set_stats("wisdom", 13);
+    set_stats("charisma", 14);
+    set_stats("dexterity", 18);
+    set_func_chance(45);
+    set_funcs(({ "flashit", "rushit", "rushit" }));
+    add_search_path("/cmds/fighter");
+    set("aggressive", "kill_evil");
+    remove_property("swarm");
+    set_monster_feats(({
+        "parry",
+        "powerattack",
+        "shatter",
+        "sunder",
+        "rush"
+    }));
 }
 
-void init() {
-   ::init();
-   if( (string)TP->query_name() == "raider") {
-      command("say %^BOLD%^%^GREEN%^Leave our village!%^RESET%^");
-      command("kill raider");
-   }
+void init()
+{
+    ::init();
+    if ((string)TP->query_name() == "raider") {
+        command("say %^BOLD%^%^GREEN%^Leave our village!%^RESET%^");
+        command("kill raider");
+    }
 }
 
-void rushit(object targ) {
-   TO->force_me("rush "+targ->query_name());
+void rushit(object targ)
+{
+    TO->force_me("rush " + targ->query_name());
 }
 
-void flashit(object targ) {
-   TO->force_me("flash "+targ->query_name());
+void flashit(object targ)
+{
+    TO->force_me("flash " + targ->query_name());
 }
 
-void kill_evil() {
-   string race;
-   object shape;
-   race = TP->query_visual_race();
-   if( race == "drow" || race == "half-drow" || race == "goblin" || race == "hobgoblin" ||
-       race == "orc" || race == "half-orc" || race == "ogre" || race == "half-ogre" ||
-       race == "kobold" || race == "ogre-mage" || race == "bugbear" || race == "wemic" ||
-       race == "gnoll") {
-       force_me("say %^BOLD%^%^GREEN%^Be gone of this village you evil scum!%^RESET%^");
-       force_me("kill "+TP->query_name());
-       command("parry");
-       return;
-   }
+void kill_evil()
+{
+    string race;
+    object shape;
+    race = TP->query_race();
+    if (objectp(shape = TP->query_property("shapeshifted"))) {
+        race = (string)shape->query_shape_race();
+    }
+    if (objectp(shape = TP->query_property("altered"))) {
+        race = (string)shape->query_shape_race();
+    }
+    if (race == "drow" || race == "half-drow" || race == "goblin" || race == "hobgoblin" ||
+        race == "orc" || race == "half-orc" || race == "ogre" || race == "half-ogre" ||
+        race == "kobold" || race == "ogre-mage" || race == "bugbear" || race == "wemic" ||
+        race == "gnoll" ||
+        race == "troll" ||
+        race == "ratkin" ||
+        race == "wererat") {
+        force_me("say %^BOLD%^%^GREEN%^Be gone of this village you evil scum!%^RESET%^");
+        force_me("kill " + TP->query_name());
+        command("parry");
+        return;
+    }
 }
--- d/darkwood/camps/mon/laucian.c
+++ d/darkwood/camps/mon/laucian.c
@@ -5,113 +5,127 @@
 inherit MONSTER;
 object ob;
 
-void create() {
-   ::create();
-   set_name("laucian");
-   set_id(({"elf","guard","captain","laurian amakiir"}));
-   set_short("Laucian Amakiir, Captain of the Guard");
-   set_long(
-       "The male high elf before you stands at five and a half feet. His dark"+
-       " black hair sharply contrasts with his pale skin. His deep green eyes"+
-       " show a finely tuned alertness of his surroundings. He is in charge of"+
-       " the daily patrols on the paths to the village and keeping the elves here"+
-       " safe from invasion. By the way he carries himself, you can tell he does"+
-       " that well."
-   );
-   set_class("fighter");
-   set_guild_level("fighter",15);
-   set_class("thief");
-   set_guild_level("thief",15);
-   set_mlevel("fighter",15);
-   set_mlevel("thief",15);
-   set_mob_magic_resistance("average");
-   set_race("elf");
-   set_body_type("humanoid");
-   set_gender("male");
-   set_property("no animate",1);
-   set_hd(25,8);
-   set_alignment(4);
-   set_max_hp(300);
-   set_hp(300);
-   set_overall_ac(-10);
-   set_size(2);
-   set_exp(15000);
-   ob = new(SPEC2+"elfsword");
-   ob->set_property("monsterweapon", 1);
-   ob->move(TO);
-   command("wield longsword");
-   ob = new(SPEC2+"elfsword");
-   ob->set_property("monsterweapon", 1);
-   ob->move(TO);
-   command("wield longsword");
-   ob = new(POW);
-   ob->set_property("monsterweapon", 1);
-   ob->move(TO);
-   set_languages( ({ "elven" }) );
-   command("speak elven");
-   set_property("full attacks",1);
-   set("aggressive", "kill_evil");
-   set_stats("strength",19);
-   set_stats("constitution",16);
-   set_stats("intelligence",15);
-   set_stats("wisdom",17);
-   set_stats("charisma",14);
-   set_stats("dexterity",20);
-   set_func_chance(45);
-   set_funcs( ({ "flashit","rushit","rushit"}) );
-   set_thief_skill("move silently",95);
-   set_thief_skill("hide in shadows",95);
-   add_search_path("/cmds/fighter");
-   add_search_path("/cmds/thief");
-   set_scrambling(1);
-   set_monster_feats(({
-      "parry",
-      "powerattack",
-	  "shatter",
-	  "sunder",
-	  "rush",
-	  "dodge",
-      "mobility",
-	  "evasion",
-	  "scramble",
-	  "combat reflexes"
-    }));     
+void create()
+{
+    ::create();
+    set_name("laucian");
+    set_id(({ "elf", "guard", "captain", "laurian amakiir" }));
+    set_short("Laucian Amakiir, Captain of the Guard");
+    set_long(
+        "The male high elf before you stands at five and a half feet. His dark" +
+        " black hair sharply contrasts with his pale skin. His deep green eyes" +
+        " show a finely tuned alertness of his surroundings. He is in charge of" +
+        " the daily patrols on the paths to the village and keeping the elves here" +
+        " safe from invasion. By the way he carries himself, you can tell he does" +
+        " that well."
+        );
+    set_class("fighter");
+    set_guild_level("fighter", 15);
+    set_class("thief");
+    set_guild_level("thief", 15);
+    set_mlevel("fighter", 15);
+    set_mlevel("thief", 15);
+    set_mob_magic_resistance("average");
+    set_race("elf");
+    set_body_type("humanoid");
+    set_gender("male");
+    set_property("no animate", 1);
+    set_hd(25, 8);
+    set_alignment(4);
+    set_max_hp(300);
+    set_hp(300);
+    set_overall_ac(-10);
+    set_size(2);
+    set_exp(15000);
+    ob = new(SPEC2 + "elfsword");
+    ob->set_property("monsterweapon", 1);
+    ob->move(TO);
+    command("wield longsword");
+    ob = new(SPEC2 + "elfsword");
+    ob->set_property("monsterweapon", 1);
+    ob->move(TO);
+    command("wield longsword");
+    ob = new(POW);
+    ob->set_property("monsterweapon", 1);
+    ob->move(TO);
+    set_languages(({ "elven" }));
+    command("speak elven");
+    set_property("full attacks", 1);
+    set("aggressive", "kill_evil");
+    set_stats("strength", 19);
+    set_stats("constitution", 16);
+    set_stats("intelligence", 15);
+    set_stats("wisdom", 17);
+    set_stats("charisma", 14);
+    set_stats("dexterity", 20);
+    set_func_chance(45);
+    set_funcs(({ "flashit", "rushit", "rushit" }));
+    set_thief_skill("move silently", 95);
+    set_thief_skill("hide in shadows", 95);
+    add_search_path("/cmds/fighter");
+    add_search_path("/cmds/thief");
+    set_scrambling(1);
+    set_monster_feats(({
+        "parry",
+        "powerattack",
+        "shatter",
+        "sunder",
+        "rush",
+        "dodge",
+        "mobility",
+        "evasion",
+        "scramble",
+        "combat reflexes"
+    }));
 }
 
-void init() {
-   string *evil;
-   ::init();
-      if(wizardp(TP)) return 1;
-      evil = KILLING_D->query_evil_bounties();
-       if(member_array(TP->query_name(),evil) != -1) {
-       force_me("say %^BOLD%^%^GREEN%^Your evil acts must be stopped!!%^RESET%^");
-       force_me("emote moves quicker than you anticipated");
-      force_me("stab "+TP->query_name());
-      force_me("toss powder at "+TP->query_name());
-       }
+void init()
+{
+    string* evil;
+    ::init();
+    if (wizardp(TP)) {
+        return 1;
+    }
+    evil = KILLING_D->query_evil_bounties();
+    if (member_array(TP->query_name(), evil) != -1) {
+        force_me("say %^BOLD%^%^GREEN%^Your evil acts must be stopped!!%^RESET%^");
+        force_me("emote moves quicker than you anticipated");
+        force_me("stab " + TP->query_name());
+        force_me("toss powder at " + TP->query_name());
+    }
 }
 
-void rushit(object targ) {
-   TO->force_me("rush "+targ->query_name());
+void rushit(object targ)
+{
+    TO->force_me("rush " + targ->query_name());
 }
 
-void flashit(object targ) {
-   TO->force_me("flash "+targ->query_name());
+void flashit(object targ)
+{
+    TO->force_me("flash " + targ->query_name());
 }
 
-void kill_evil() {
-   string race;
-   object shape;
-   race = TP->query_race();
-   if(objectp(shape = TP->query_property("shapeshifted"))) race = (string)shape->query_shape_race();
-   if(objectp(shape = TP->query_property("altered"))) race = (string)shape->query_shape_race();
-      if( race == "drow" || race == "half-drow" || race == "goblin" || race == "hobgoblin" ||
-      race == "orc" || race == "half-orc" || race == "ogre" || race == "half-ogre" ||
-      race == "kobold" || race == "ogre-mage" || race == "bugbear" || race == "wemic" ||
-      race == "gnoll") {
-   force_me("say %^BOLD%^%^GREEN%^Be gone of this village you evil scum!%^RESET%^");
-   force_me("kill "+TP->query_name());
-   command("parry");
-   return;
-   }
+void kill_evil()
+{
+    string race;
+    object shape;
+    race = TP->query_race();
+    if (objectp(shape = TP->query_property("shapeshifted"))) {
+        race = (string)shape->query_shape_race();
+    }
+    if (objectp(shape = TP->query_property("altered"))) {
+        race = (string)shape->query_shape_race();
+    }
+    if (race == "drow" || race == "half-drow" || race == "goblin" || race == "hobgoblin" ||
+        race == "orc" || race == "half-orc" || race == "ogre" || race == "half-ogre" ||
+        race == "kobold" || race == "ogre-mage" || race == "bugbear" || race == "wemic" ||
+        race == "gnoll" ||
+        race == "troll" ||
+        race == "ratkin" ||
+        race == "wererat") {
+        force_me("say %^BOLD%^%^GREEN%^Be gone of this village you evil scum!%^RESET%^");
+        force_me("kill " + TP->query_name());
+        command("parry");
+        return;
+    }
 }
