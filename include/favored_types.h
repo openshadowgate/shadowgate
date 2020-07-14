e831a331b6e7537a7764b81b2a152abea657a877
shadowgate <law@shadowgate.org>
2020-07-14 20:00:03 +0000

    autocommit 202007140020

--- include/favored_types.h
+++ include/favored_types.h
@@ -20,24 +20,24 @@
                        ])
 
 #define VALID_ENEMY ([ \
-                         "aberrations" : ({ "beholder", "illithid", "mind flayer" }), \
-                         "animals" : ({ "snake", "serpent", "warthog", "jaguar", "wolf", "rat", "tiger", "reptile", "dinosaur", "dino", "spider", "deer", "snake", "horse", "mare", "stallion" }), \
-                         "constructs" : ({ "construct", "golem", "sandman", "sandling" }), \
-                         "dwarves"  : ({ "dwarf", "duergar" }), \
-                         "dragonkin"  : ({ "kobold", "draconian", "dragon", "elven-dragon", "dragonet", "pseudodragon", "drake", "wyvern" }), \
-                         "elementals" : ({ "elemental", "genasi", "earth genasi", "air genasi", "water genasi", "air genasi", "efreet", "djinn", "marid", "genie" }), \
-                         "elves"      : ({ "elf", "half-elf", "drow", "half-drow", "szarkai" }), \
-                         "fey"        : ({ "nymph", "satyr", "dryad", "pixie", "brownie", "feytouched", "fey" }), \
-                         "giants"     : ({ "voadkyn", "firbolg", "giant", "titan", "troll" }), \
-                         "goblins"    : ({ "goblin", "bugbear", "hobgoblin", "goblinoid" }), \
-                         "humans"     : ({ "human", "shade", "half-elf", "half-orc", "half-ogre", "half-drow", "genasi", "feytouched", "aasimar", "tiefling" }), \
-                         "lizardfolk" : ({ "yuan-ti", "saurian", "salamander", "lizardman", "firenewt" }), \
-                         "magical beasts" : ({ "owlbear", "grimlock", "jackalwere", "wolfwere", "ooze", "worm", "pegataur", "griffon", "hydra", "shadow", "magical beast" }), \
-                         "monstrous humanoids"   : ({ "beastkin", "wemic", "centaur", "ratkin", "minotaur", "gnoll", "kitsune", "werewolf", "wererat", "weretiger", "alaghi", "monstrous humanoid", }), \
-                         "ogres"      : ({ "ogre", "ogre-mage", "half-ogre", "ogroid" }), \
-                         "orcs"       : ({ "orc", "half-orc", "orog", "tanarukk", "orcoid" }), \
-                         "outsiders"  : ({ "deva", "solar", "aasimar", "angel", "demon", "devil", "imp", "tiefling", "githyanki", "githzerai", "outsider"  }), \
-                         "plants" : ({ "shambler", "shambling mound", "tree", "vine", "plant" }), \
-                         "smallfolk"  : ({ "gnome", "halfling", "kender", "derro", "pech", }), \
-                         "undead"     : ({ "ghost", "skeleton", "lich", "zombie", "ghast", "entity", "spirit", "wraith", "wight" }), \
+                         "aberrations" : ({ "aberrations", "beholder", "illithid", "mind flayer", }), \
+                         "animals" : ({ "animals", "snake", "serpent", "warthog", "jaguar", "wolf", "rat", "tiger", "reptile", "dinosaur", "dino", "spider", "deer", "snake", "horse", "mare", "stallion" }), \
+                         "constructs" : ({ "constructs", "construct", "golem", "sandman", "sandling" }), \
+                         "dwarves"  : ({ "dwarf", "dwarves", "duergar" }), \
+                         "dragonkin"  : ({ "dragonkin", "kobold", "draconian", "dragon", "elven-dragon", "dragonet", "pseudodragon", "drake", "wyvern" }), \
+                         "elementals" : ({ "elementals", "elemental", "genasi", "earth genasi", "air genasi", "water genasi", "air genasi", "efreet", "djinn", "marid", "genie" }), \
+                         "elves"      : ({ "elf", "elves", "half-elf", "drow", "half-drow", "szarkai" }), \
+                         "fey"        : ({ "fey", "nymph", "satyr", "dryad", "pixie", "brownie", "feytouched", "fey" }), \
+                         "giants"     : ({ "giants", "voadkyn", "firbolg", "giant", "titan", "troll" }), \
+                         "goblins"    : ({ "goblins", "goblin", "bugbear", "hobgoblin", "goblinoid" }), \
+                         "humans"     : ({ "humans", "human", "shade", "half-elf", "half-orc", "half-ogre", "half-drow", "genasi", "feytouched", "aasimar", "tiefling" }), \
+                         "lizardfolk" : ({ "lizardfolk", "yuan-ti", "saurian", "salamander", "lizardman", "firenewt" }), \
+                         "magical beasts" : ({ "magical beasts", "owlbear", "grimlock", "jackalwere", "wolfwere", "ooze", "worm", "pegataur", "griffon", "hydra", "shadow", "magical beast" }), \
+                         "monstrous humanoids"   : ({ "monstrous humanoids", "beastkin", "wemic", "centaur", "ratkin", "minotaur", "gnoll", "kitsune", "werewolf", "wererat", "weretiger", "alaghi", "monstrous humanoid", }), \
+                         "ogres"      : ({ "ogres", "ogre", "ogre-mage", "half-ogre", "ogroid", }), \
+                         "orcs"       : ({ "orcs", "orc", "half-orc", "orog", "tanarukk", "orcoid", }), \
+                         "outsiders"  : ({ "outsiders", "deva", "solar", "aasimar", "angel", "demon", "devil", "imp", "tiefling", "githyanki", "githzerai", "outsider"  }), \
+                         "plants" : ({ "shambler", "shambling mound", "tree", "vine", "plant", "plants", }), \
+                         "smallfolk"  : ({ "gnome", "halfling", "kender", "derro", "pech", "smallfolk", }), \
+                         "undead"     : ({ "ghost", "skeleton", "lich", "zombie", "ghast", "entity", "spirit", "undead", "wraith", "wight" }), \
                      ])
