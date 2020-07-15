696a597fd739742c9d109ee007d6cac3a7e5ad47
brantlymedders <brantlymedders@gmail.com>
2020-07-15 19:27:51 -0400

    Update to dwarf bonus languages as per PFSRD.

--- std/races/dwarf.c
+++ std/races/dwarf.c
@@ -363,12 +363,12 @@ string* query_languages(string subrace)
     switch (subrace) {
     case "duergar":
     case "grey dwarf":
-        return (["required" : ({ "dwarvish", "undercommon" }), "optional" : ({ "gnomish", "goblin", "common" })]);
+        return (["required" : ({ "dwarvish", "undercommon" }), "optional" : ({ "giant", "gnome", "goblin", "orc", "terran", "common" })]);
 
         break;
 
     default:
-        return (["required" : ({ "dwarvish", "common" }), "optional" : ({ "gnomish", "goblin", "orcish", "undercommon" })]);
+        return (["required" : ({ "dwarvish", "common" }), "optional" : ({ "giant", "gnome", "goblin", "orc", "terran", "undercommon" })]);
 
         break;
     }
