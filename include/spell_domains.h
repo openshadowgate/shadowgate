d3c0ff0ce4413bc010823860b2a12e83d0e1068a
nm0i <nm0i@me0w.net>
2020-07-19 06:27:59 +0300

    Moving Kismet to her original slot, moving domains.

--- include/spell_domains.h
+++ include/spell_domains.h
@@ -3,15 +3,16 @@
 // Must correspond to deities.h
 #define SPELL_DOMAINS (([\
     "callamir"         : ({ "good", "charm", "air", "trickery", "travel", "chaos",}), \
+    "cevahir"          : ({ "knowledge", "mentalism", "renewal", "strength", "war" }) , \
+    "dream"            : ({ "chaos", "madness", "void", "stars", "evil", "good"}),\
     "jarmila"          : ({ "good", "renewal", "sun", "protection", "fire", "martyr" }) ,\
-    "kismet"           : ({ "creation", "knowledge", "mentalism", "magic", "law", "stars"}) , \
+    "khyron"           : ({ "fire", "storms", "destruction", "suffering", "chaos", "plague" }) , \
+    "kismet"           : ({ "creation", "knowledge", "mentalism", "magic", "stars"}) , \
     "kreysneothosies"  : ({ "protection", "vigilance", "strength", "good", "war", "law"}) , \
     "lord shadow"      : ({ "evil", "cold", "tyranny", "suffering", "darkness", "law"}) , \
+    "lysara"           : ({ "travel", "fate", "repose", "moon", "water", "void"}) , \
     "nilith"           : ({ "war", "strength", "destruction", "evil", "chaos", "undeath"}), \
-    "nimnavanon"       : ({ "storms", "plant", "animal", "earth", "destruction", "good" }) , \
-    "the faceless one" : ({ "trickery", "illusion", "cavern", "darkness", "evil", "madness"}) , \
-    "dream"            : ({ "chaos", "madness", "void", "stars", "evil", "good"}),\
-    "cevahir"          : ({ "knowledge", "mentalism", "renewal", "strength", "war" }) , \
-    "khyron"           : ({ "fire", "storms", "destruction", "suffering", "evil", "plague" }) , \
+    "nimnavanon"       : ({ "storms", "plant", "animal", "earth", "destruction", "plague" }) , \
     "seija"            : ({ "elements", "sun", "moon", "cavern", "stars" }) , \
+    "the faceless one" : ({ "trickery", "illusion", "cavern", "darkness", "evil", "madness"}) , \
 ]))
