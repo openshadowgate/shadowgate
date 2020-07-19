6f82c485bfd8afa1339a15706dcaa9e28b38b854
nm0i <nm0i@me0w.net>
2020-07-19 11:11:58 +0300

    Method to prevent in-lib cleaning.

--- adm/daemon/cleaning_d.c
+++ adm/daemon/cleaning_d.c
@@ -1,15 +1,15 @@
 /*
-A daemon to clean up unused objects on a more regular basis than the
-driver level clean_up calls.  It cleans fewer objects at a time to
-avoid large lag spikes that are noticable to the player.
-BLOCK_SIZE determins how many objects are checked at one time to
-           see if they are eligible for clean up.  These objects
-           pass through clean_filter and if they meet the requirements
-           they are then set to a call_out which calls their
-           clean_up function.
-TIME_DELAY this is the duration of the call_out between calls to
-           clean_up.
-created by -Ares 01/21/2010
+  A daemon to clean up unused objects on a more regular basis than the
+  driver level clean_up calls.  It cleans fewer objects at a time to
+  avoid large lag spikes that are noticable to the player.
+  BLOCK_SIZE determins how many objects are checked at one time to
+  see if they are eligible for clean up.  These objects
+  pass through clean_filter and if they meet the requirements
+  they are then set to a call_out which calls their
+  clean_up function.
+  TIME_DELAY this is the duration of the call_out between calls to
+  clean_up.
+  created by -Ares 01/21/2010
 */
 
 #include <std.h>
@@ -20,43 +20,55 @@ inherit DAEMON;
 #define BLOCK_SIZE 500  // how many objects it will check at one time
 #define TIME_DELAY 20   // delay between checking if objects can be cleaned
 
-void begin_cleaning();
-int clean_filter(object obj);
-void begin_cleaning();
-void block_objects(object *objs);
-void slow_clean(object *objs,int step);
-void debug_message(string str);
-
-
-int test_counter,cleaning;
+int test_counter, cleaning;
 object debug_object;
 
-void create() 
-{ 
+void create()
+{
     begin_cleaning();
-    ::create(); 
+    ::create();
 }
 
 int clean_filter(object obj)
 {
-    if(!objectp(obj))            { return 0; }
-    if(strsrch(base_name(obj),"/daemon") == 0) { return 0; }
-    if(strsrch(base_name(obj),"/adm/daemon") == 0) { return 0; }
-    if((clonep(obj) && !objectp(environment(obj))) && !obj->query_had_players()) { return 1; }
-    if(!objectp(environment(obj)) && obj->is_monster()) { return 1; }
-    if(obj->is_spell())          { return 1; }
-    if(obj->is_feat())           { return 1; }
-    if(!obj->is_room())          { return 0; }
-    if(obj->query_had_players()) { return 0; }
+    if (!objectp(obj)) {
+        return 0;
+    }
+    if (strsrch(base_name(obj), "/daemon") == 0) {
+        return 0;
+    }
+    if (strsrch(base_name(obj), "/adm/daemon") == 0) {
+        return 0;
+    }
+    if (obj->query_noclean()) {
+        return 0;
+    }
+    if ((clonep(obj) && !objectp(environment(obj))) && !obj->query_had_players()) {
+        return 1;
+    }
+    if (!objectp(environment(obj)) && obj->is_monster()) {
+        return 1;
+    }
+    if (obj->is_spell()) {
+        return 1;
+    }
+    if (obj->is_feat()) {
+        return 1;
+    }
+    if (!obj->is_room()) {
+        return 0;
+    }
+    if (obj->query_had_players()) {
+        return 0;
+    }
     return 1;
 }
 
-// tells the program to start cleaning 
+// tells the program to start cleaning
 void begin_cleaning()
 {
-    if(!cleaning)
-    {
-        object *objs;
+    if (!cleaning) {
+        object* objs;
         objs = objects();
         block_objects(objs);
         cleaning = 1;
@@ -64,12 +76,14 @@ void begin_cleaning()
 }
 
 // breaks the huge object array of all objects on the mud down into little chunks set by BLOCK_SIZE
-void block_objects(object *objs)
+void block_objects(object* objs)
 {
-    object *obj_blocks=({}),*tmp=({});
-    int blocks, remainder,i,j;
+    object* obj_blocks = ({}), * tmp = ({});
+    int blocks, remainder, i, j;
 
-    if(!pointerp(objs) || !sizeof(objs)) { begin_cleaning(); }
+    if (!pointerp(objs) || !sizeof(objs)) {
+        begin_cleaning();
+    }
 
 
     blocks = sizeof(objs) / BLOCK_SIZE;
@@ -77,102 +91,100 @@ void block_objects(object *objs)
 
     // assigns the objects into arrays with BLOCK_SIZE objects each
     j = 0;
-    for(i=0;i<blocks;i++) 
-    {
-        tmp = objs[j..(j+(BLOCK_SIZE - 1))];
+    for (i = 0; i < blocks; i++) {
+        tmp = objs[j..(j + (BLOCK_SIZE - 1))];
         obj_blocks += ({ tmp });
         j += BLOCK_SIZE;
     }
 
-    if(remainder)
-    {
-        if(!blocks) { tmp = objs[0..remainder]; }
-        else { tmp = objs[(BLOCK_SIZE * (blocks-1))..( ((BLOCK_SIZE * (blocks-1)) + remainder) -1)]; }
+    if (remainder) {
+        if (!blocks) {
+            tmp = objs[0..remainder];
+        }else {
+            tmp = objs[(BLOCK_SIZE * (blocks - 1))..(((BLOCK_SIZE * (blocks - 1)) + remainder) - 1)];
+        }
         obj_blocks += ({ tmp });
     }
 
-    debug_message("sizeof obj_blocks"+sizeof(obj_blocks)+"");
-    for(i=0;i<sizeof(obj_blocks);i++)
-    {
-        debug_message("sizeof obj_blocks["+i+"]: "+sizeof(obj_blocks[i])+"");
+    debug_message("sizeof obj_blocks" + sizeof(obj_blocks) + "");
+    for (i = 0; i < sizeof(obj_blocks); i++) {
+        debug_message("sizeof obj_blocks[" + i + "]: " + sizeof(obj_blocks[i]) + "");
     }
 
-    slow_clean(obj_blocks,0);
+    slow_clean(obj_blocks, 0);
     return;
 }
 
-// runs a callout on each block of objects until it's stepped through every block then it begins again 
-void slow_clean(object *objs,int step)
-{   
-    object *block;
+// runs a callout on each block of objects until it's stepped through every block then it begins again
+void slow_clean(object* objs, int step)
+{
+    object* block;
     int i;
 
-    if(!pointerp(objs) || !sizeof(objs)) { return; }
+    if (!pointerp(objs) || !sizeof(objs)) {
+        return;
+    }
 
-    if(step == sizeof(objs))
-    {
+    if (step == sizeof(objs)) {
         debug_message("%^B_BLUE%^%^WHITE%^Finished one cleaning series.%^RESET%^");
-        debug_message("%^B_BLUE%^%^WHITE%^Total objects cleaned: "+test_counter+"");
+        debug_message("%^B_BLUE%^%^WHITE%^Total objects cleaned: " + test_counter + "");
         cleaning = 0;
         begin_cleaning();
         return;
     }
 
-    if(catch(objs[step]))
-    {
+    if (catch(objs[step])) {
         debug_message("invalid index.");
         return;
     }
 
     block = objs[step];
 
-    block = filter_array(block,"clean_filter",TO);
+    block = filter_array(block, "clean_filter", TO);
 
-    call_out("clean_block",TIME_DELAY,objs,block,step);
+    call_out("clean_block", TIME_DELAY, objs, block, step);
     return;
 }
 
 // makes the actual calls to clean_up
-void clean_block(object *objs,object *block,int step)
+void clean_block(object* objs, object* block, int step)
 {
     mixed error;
     int i;
 
     step++;
 
-    if(!sizeof(block)) 
-    { 
-        debug_message("No objects to clean in block "+step+"");
-        slow_clean(objs,step);
+    if (!sizeof(block)) {
+        debug_message("No objects to clean in block " + step + "");
+        slow_clean(objs, step);
         return;
     }
 
-    debug_message("Cleaning block "+step+"");
-    debug_message("Block size is: "+sizeof(block)+"");
-    for(i=0;i<sizeof(block);i++)
-    {
-        if(!objectp(block[i])) { continue; }
+    debug_message("Cleaning block " + step + "");
+    debug_message("Block size is: " + sizeof(block) + "");
+    for (i = 0; i < sizeof(block); i++) {
+        if (!objectp(block[i])) {
+            continue;
+        }
         error = catch(block[i]->clean_up());
-        if(error)
-        {
-            debug_message("Error in clean up: "+error+"");
+        if (error) {
+            debug_message("Error in clean up: " + error + "");
         }
         test_counter++;
     }
-    debug_message("Total objects cleaned: "+test_counter+"");
-    debug_message("Total memory usage: "+memory_info()+"");
+    debug_message("Total objects cleaned: " + test_counter + "");
+    debug_message("Total memory usage: " + memory_info() + "");
 
-    slow_clean(objs,step);
+    slow_clean(objs, step);
     return;
 }
 
 // allows you to set an object to send debug messages to, ie, yourself
 void set_debug_object(object obj)
 {
-    if(!objectp(obj)) 
-    { 
+    if (!objectp(obj)) {
         debug_object = 0;
-        return; 
+        return;
     }
     debug_object = obj;
 }
@@ -180,9 +192,14 @@ void set_debug_object(object obj)
 // sends debug messages to the object specified with set_debug_object
 void debug_message(string str)
 {
-    if(!objectp(debug_object)) { return; }
-    tell_object(debug_object,str);
+    if (!objectp(debug_object)) {
+        return;
+    }
+    tell_object(debug_object, str);
     return;
 }
 
-int clean_up() { return 1; }
+int clean_up()
+{
+    return 1;
+}
