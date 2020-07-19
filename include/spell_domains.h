680942940d2ba6c2b3e5228477a3c253627ce518
nm0i <nm0i@me0w.net>
2020-07-19 07:11:15 +0300

    Domain rebalance, cleaning up for the first stage.

--- include/spell_domains.h
+++ include/spell_domains.h
@@ -10,13 +10,13 @@
     "cevahir"          : ({ "knowledge", "mentalism", "renewal", "strength", "war" }) , \
     "dream"            : ({ "chaos", "madness", "void", "stars", "evil", "good"}),\
     "jarmila"          : ({ "good", "renewal", "sun", "protection", "fire", "martyr" }) ,\
-    "khyron"           : ({ "fire", "storms", "destruction", "suffering", "chaos", "plague" }) , \
-    "kismet"           : ({ "creation", "knowledge", "mentalism", "magic", "stars"}) , \
+    "khyron"           : ({ "fire", "storms", "destruction", "suffering", "chaos", }) , \
+    "kismet"           : ({ "creation", "knowledge", "magic", "stars"}) , \
     "kreysneothosies"  : ({ "protection", "vigilance", "strength", "good", "war", "law"}) , \
     "lord shadow"      : ({ "evil", "cold", "tyranny", "suffering", "darkness", "law"}) , \
     "lysara"           : ({ "travel", "fate", "repose", "moon", "water", "void"}) , \
     "nilith"           : ({ "war", "strength", "destruction", "evil", "chaos", "undeath"}), \
     "nimnavanon"       : ({ "storms", "plant", "animal", "earth", "destruction", "plague" }) , \
-    "seija"            : ({ "elements", "sun", "moon", "cavern", "stars" }) , \
+    "seija"            : ({ "elements", "sun", "moon", "cavern", "law"}) , \
     "the faceless one" : ({ "trickery", "illusion", "cavern", "darkness", "evil", "madness"}) , \
 ]))
