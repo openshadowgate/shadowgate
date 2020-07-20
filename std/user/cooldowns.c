70345a12292deeff17e850bda053f207f72c14f7
nm0i <nm0i@me0w.net>
2020-07-20 19:38:16 +0300

    Formatting in wing, cooldowns.

--- std/user/cooldowns.c
+++ std/user/cooldowns.c
@@ -1,72 +1,86 @@
-/*
-  cooldowns.c
-  
-  This inheritable handles cooldowns on pretty much
-  anything.
-  
-  -- Tlaloc --
-*/
+/**
+ * @file
+ * @brief This inheritable handles cooldowns on pretty much  anything.
+ *
+ */
 
-mapping cooldowns = ([  ]);
+mapping cooldowns = ([]);
 
-//Tells the player when the cooldown is complete
+/**
+ * Tells the player when the cooldown is complete
+ */
 int notify_available(string str)
 {
     tell_object(this_object(), "[%^CYAN%^COOLDOWN%^RESET%^] -- %^YELLOW%^" + capitalize(str) + "%^CYAN%^ is now available.%^RESET%^\n");
     return 1;
 }
 
-//Checks the time left on the cooldown
+/**
+ * Checks the time left on the cooldown
+ */
 int cooldown(string str)
 {
-    if(mapping_member(cooldowns, str))
+    if (mapping_member(cooldowns, str)) {
         return cooldowns[str];
-    
+    }
+
     return 0;
 }
 
-//Adds a cooldown (could really be anything)
+/**
+ * Adds a cooldown (could really be anything)
+ */
 int add_cooldown(string str, int delay)
 {
-    if(mapping_member(cooldowns, str))
+    if (mapping_member(cooldowns, str)) {
         return 0;
-    
-    (mapping)cooldowns += ([ str : (time() + delay), ]);
+    }
+
+    (mapping)cooldowns += ([ str:(time() + delay), ]);
     return 1;
 }
 
-//Allows premature removal of cooldown
+/**
+ * Allows premature removal of cooldown
+ */
 int remove_cooldown(string str)
 {
-    if(!mapping_member(cooldowns, str))
+    if (!mapping_member(cooldowns, str)) {
         return 0;
-    
+    }
+
     map_delete(cooldowns, str);
     notify_available(str);
     return 1;
 }
 
-//Checks cooldowns to see if they're expired
+/**
+ * Checks cooldowns to see if they're expired
+ */
 void process_cooldowns(string key, int delay)
 {
-    if(delay <= time())
+    if (delay <= time()) {
         remove_cooldown(key);
+    }
 }
 
-//Returns the entire list of cooldowns
+/**
+ * Returns the entire list of cooldowns
+ */
 mapping query_cooldowns()
 {
     return cooldowns;
 }
 
-//Checks cooldown every heart beat
+/**
+ * Checks cooldown every heart beat
+ */
 void heart_beat()
 {
-    if(sizeof(cooldowns))
-    {
-        foreach(string key in (keys(cooldowns)))
+    if (sizeof(cooldowns)) {
+        foreach(string key in(keys(cooldowns)))
             process_cooldowns(key, cooldowns[key]);
+    }else {
+        cooldowns = ([]);
     }
-    else
-        cooldowns = ([  ]);
-} 
+}
