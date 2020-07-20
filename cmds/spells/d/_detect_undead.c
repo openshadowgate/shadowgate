57a3b293d0e23072fe551b165333c32d9ca73211
shadowgate <law@shadowgate.org>
2020-07-20 17:33:02 +0000

    autocommit 202007203317

--- cmds/spells/d/_detect_undead.c
+++ cmds/spells/d/_detect_undead.c
@@ -2,31 +2,18 @@
 #include <priest.h>
 #include <daemons.h>
 
-void create() {
+void create()
+{
     ::create();
     set_spell_name("detect undead");
-    set_spell_level(([ "innate" : 1 ]));
+    set_spell_level(([ "innate" : 1, "paladin" : 1, ]));
     set_spell_sphere("divination");
-    set_syntax("cast CLASS detect undead on TARGET || <room>");
-    set_description("Detect undead will detect the presence of undeath in players or monsters.");
+    set_syntax("cast CLASS detect undead");
+    set_description("Detect undead will detect the presence of greater and lesser undead, including vampires and other intelligent undead.");
     set_verbal_comp();
     set_somatic_comp();
     set_arg_needed();
     set_non_living_ok(1);
-}
-
-int preSpell(){
-    object targ;
-    	if(query_arg() != 0) 
-	{
-    		targ = present(query_arg(), query_place());
-	}
-    if(!objectp(targ) && arg != "room") {
-        tell_object(caster,"You need either a target or <room> for this spell.");
-        return 0;
-    }
-    
-    return 1;
 }
 
 string query_cast_string() 
@@ -34,63 +21,29 @@ string query_cast_string()
    return ""+YOU+" focuses on "+MINE+" holy symbol and chants softly.\n";
 }
 
-void spell_effect(int prof) 
+void spell_effect(int prof)
 {
-    string arg;
-    int i, hits = 0;
-    object *alive,ob;
-
-    arg = caster->realName(arg);
-    if(arg == "") arg = ARG;
-    if(arg == "room") {
-        alive = all_living(environment(CASTER));
-        alive -= ({CASTER});
-        for (i=0;i<sizeof(alive);i++) {
-            if(alive[i]->query_invis()) continue;
-            hits++;
-            if (alive[i]->is_undead()) 
-            {
-                tell_object(CASTER,"%^BOLD%^%^RED%^You detect undeath in "+alive[i]->QCN+".");
-            }else {
-                tell_object(CASTER,"%^BOLD%^%^BLUE%^You detect no undeath in "+alive[i]->QCN+".");
-           }
-        }
-        if(!hits)
-        {
-            tell_object(CASTER, "%^BOLD%^%^BLUE%^You detect no undead.%^RESET%^");
-        }
-    } 
-    else if (!ob = present(arg,environment(caster))) 
+    object *targets;
+    int hits;
+    
+    targets = all_living(environment(caster));
+    targets = filter_array(targets, "check_undead");
+    
+    if(!sizeof(targets))
     {
-        tell_object(CASTER,"That is not here.");
-        dest_effect();
+        tell_object(caster, "There are no undead present here.....");
         return;
-    } 
-    else if (!living(ob)) 
-    {
-        tell_object(CASTER,"You can't detect undeath in that.");
-        dest_effect();
-        return;
-    } 
-    else 
-    {
-        if (ob->is_undead()) 
-        {
-            tell_object(CASTER, "%^BOLD%^%^RED%^You detect undeath in "+ob->QCN+".");
-        } 
-        else 
-        {
-            tell_object(CASTER,"%^BOLD%^%^BLUE%^You detect no undeath in "+ob->QCN+".");
-        }
     }
-    spell_successful();
-    dest_effect();
+    
+    foreach(object ob in targets)
+        tell_object(caster, "%^RED%^" + sprintf("You sense undeath in %s!", ob->query_name()));       
 }
 
-
-void dest_effect() 
+int check_undead(object ob)
 {
-    ::dest_effect();
-    if(objectp(TO)) TO->remove();
-
+    if(ob->is_undead() || 
+    ( member_array("undead", ob->query_id()) > -1 ) || 
+    ob->query_property("undead") || 
+    ob->query_race() == "undead")
+        return 1;
 }
