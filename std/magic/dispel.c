71189d0e3ae6dd3070a4ce009a11371fd4997159
shadowgate <law@shadowgate.org>
2020-07-27 21:27:02 +0000

    autocommit 202007272721

--- std/magic/dispel.c
+++ std/magic/dispel.c
@@ -4,7 +4,7 @@
 int checkDispel(object ob, int clevel)
 {
     int roll, level_diff;
-    object mycast;
+    object mycast, yourcast;
 
     if (!objectp(ob)) {
         return 0;
@@ -13,25 +13,30 @@ int checkDispel(object ob, int clevel)
         return 1;
     }
 
-    mycast = ob->query_caster();
-    if (!objectp(mycast)) {
+    yourcast = ob->query_caster();
+    mycast = TO->query_caster();
+    
+    if (!objectp(yourcast) || !objectp(mycast)) {
         return 0;
     }
 
-    level_diff = (int)ob->query_clevel() - clevel;
+    level_diff = clevel - ob->query_clevel();
 
-    roll = 6 - roll_dice(1, 12);
+    roll = roll_dice(1, 12) - 6;
 
-    if (FEATS_D->usable_feat(ob->query_caster(), "elusive spellcraft") &&
-        !FEATS_D->usable_feat(TO->query_caster(), "elusive spellcraft")) {
-        roll += 4;
+    if (FEATS_D->usable_feat(yourcast, "elusive spellcraft") &&
+        !FEATS_D->usable_feat(mycast, "elusive spellcraft")) {
+        roll -= 4;
     }
+    
+    if(FEATS_D->usable_feat(yourcast, "mystic arcana"))
+        roll -= ( ( yourcast->query_stats("charisma") - 10 ) / 5);
 
     if (mycast->query_property("dispelling_buffer") > 0) {
-        roll += mycast->query_property("dispelling_buffer");
+        roll -= mycast->query_property("dispelling_buffer");
     }
 
-    if (roll + level_diff < 0) {
+    if (roll + level_diff > 0) {
         return 1;
     } else {
         return 0;
