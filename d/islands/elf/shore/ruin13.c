f0ea03e17a18e588257765d1e861bcc9d1c2759c
melchi <melchizedek077@gmail.com>
2020-07-26 18:48:00 -0700

    syntax error and typo.

--- d/islands/elf/shore/ruin13.c
+++ d/islands/elf/shore/ruin13.c
@@ -5,7 +5,7 @@ inherit INH"ruin";
 void create(){
    ::create();
    set_exits(([ "west": ROOMS"shore/ruin7", 
-   set_exits(([ "north": ROOMS"shore/ruin16", 
+   "north": ROOMS"shore/ruin16", 
 
      ]));
 
