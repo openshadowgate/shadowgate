cd3bb020ee26a20888dba382383e346b9054c4e2
melchi <melchizedek077@gmail.com>
2020-07-28 18:12:05 -0700

    adding a little camp

--- d/islands/elf/shore/shore6.c
+++ d/islands/elf/shore/shore6.c
@@ -6,7 +6,13 @@ void create(){
    ::create();
    set_exits(([ "northeast": ROOMS"shore/shore7", 
      "west": ROOMS"shore/shore5",
-
+    "camp": ROOMS"shore/camp",
      ]));
-
+  set_door("gate",ROOMS"shore/camp","camp","elfkey");
 }
+void reset(){
+  ::reset();
+  set_locked("gate",0);
+  set_open("gate", 0);
+
+ }
