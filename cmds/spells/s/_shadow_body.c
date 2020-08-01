5a7a11c9acdc642e95f4f610c27d7da602df1ab5
nm0i <nm0i@me0w.net>
2020-08-01 22:23:09 +0300

    Fix to #618

--- cmds/spells/s/_shadow_body.c
+++ cmds/spells/s/_shadow_body.c
@@ -53,7 +53,7 @@ void create()
     set_discipline("egoist");
     set_monk_way("way of the shadow");
     set_syntax("cast CLASS shadow body");
-    set_description("Your body and all your equipment are subsumed by your shadow. As a living shadow, you blend perfectly into any other shadow and vanish in darkness. You appear as an unattached shadow in areas of full light. You shadow body gived you 10 damage reduction, flying, and 50% fire, acid and electricity resistance.");
+    set_description("Your body and all your equipment are subsumed by your shadow. As a living shadow, you blend perfectly into any other shadow and vanish in darkness. You appear as an unattached shadow in areas of full light. You shadow body gives you 10 damage reduction, flying, and 50% fire, acid and electricity resistance.");
     set_helpful_spell(1);
 }
 
