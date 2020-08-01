3c378b6eb7669c9952b495895e2980fe00d36c4a
nm0i <nm0i@me0w.net>
2020-08-01 21:42:04 +0300

    Reported money exploit in CT.

--- d/attaya/obj/goldbrick.c
+++ d/attaya/obj/goldbrick.c
@@ -3,28 +3,33 @@
 
 inherit OBJECT;
 
-void init(){
+void init()
+{
     ::init();
-    add_action("read","read");
+    add_action("read", "read");
 }
 
-void create(){
+void create()
+{
     ::create();
 
     set_name("brick");
-    set_id(({"gold brick","brick"}));
+    set_id(({ "gold brick", "brick" }));
     set_short("%^BOLD%^%^YELLOW%^gold brick%^RESET%^");
     set_long(
-      "%^ORANGE%^A heavy brick of pure gold"
-    );
-    set_weight(10); //three per sack
-    set_value(10000 * (1+ random(3)));
+        "%^ORANGE%^A heavy brick of pure gold"
+        );
+    set_weight(10);
+    set_value(10000 + roll_dice(20, 20));
 }
 
-int read(string str){
-    if(!str || str != "brick") return  0;
+int read(string str)
+{
+    if (!str || str != "brick") {
+        return 0;
+    }
 
-    tell_room(environment(TO),TPQCN+" reads the writing on the top of the gold brick!",TP);
+    tell_room(environment(TO), TPQCN + " reads the writing on the top of the gold brick!", TP);
     write("\n\n\t\%^BOLD%^%^YELLOW%^99% PURE GOLD BY WEIGHT.  Certified by T.G.D.");
     return 1;
 }
--- d/attaya/tower/spec/tower.c
+++ d/attaya/tower/spec/tower.c
@@ -3,43 +3,55 @@
 
 inherit ROOM;
 
-void create(){
-  ::create();
-  set_terrain(STONE_BUILDING);
-  set_travel(DIRT_ROAD);
-   set_property("no teleport",0); //There are a few rooms without TP proof by original design.
- // Some rooms are TPable in the Tower.
-  set_property("used sticks",1);
+void create()
+{
+    ::create();
+    set_terrain(STONE_BUILDING);
+    set_travel(DIRT_ROAD);
+    set_property("no teleport", 1);
+    set_property("used sticks", 1);
 }
 
 int is_junk(object obj)
 {
-    if(obj->id("summoned monster")) { return 0; }
-    if((int)obj->query_weight() > 50) { return 0; }
-    if(obj->id("sign")) { return 0; }
-    if(obj->id("corpse")) { return 0; }
-    if(obj->query_property("died time")) { return 0; }
-    if(!living(obj)) { return 1; }
-    else return 0;
+    if (obj->id("summoned monster")) {
+        return 0;
+    }
+    if ((int)obj->query_weight() > 50) {
+        return 0;
+    }
+    if (obj->id("sign")) {
+        return 0;
+    }
+    if (obj->id("corpse")) {
+        return 0;
+    }
+    if (obj->query_property("died time")) {
+        return 0;
+    }
+    if (!living(obj)) {
+        return 1;
+    }else {
+        return 0;
+    }
 }
 
 void reset()
 {
     int i;
-    object *junk,*living;
+    object* junk, * living;
     ::reset();
 
     living = all_living(TO);
-    living = filter_array(living,"is_non_immortal_player",FILTERS_D);    
+    living = filter_array(living, "is_non_immortal_player", FILTERS_D);
 
-    if(junk=filter_array(all_inventory(TO),"is_junk",TO) && !sizeof(living))
-    {
-        for(i=0;i<sizeof(junk);i++) { junk[i]->remove(); }
+    if (junk = filter_array(all_inventory(TO), "is_junk", TO) && !sizeof(living)) {
+        for (i = 0; i < sizeof(junk); i++) {
+            junk[i]->remove();
+        }
     }
-    if(sizeof(living))
-    {
-        tell_room(TO,"%^BOLD%^Spirits of the fallen swirl all around, searching "
-            "for supplies for Intruder's army.");
+    if (sizeof(living)) {
+        tell_room(TO, "%^BOLD%^Spirits of the fallen swirl all around, searching "
+                  "for supplies for Intruder's army.");
     }
-
 }
