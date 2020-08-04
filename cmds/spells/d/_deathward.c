7bf6bcca97f3377cd8f81b47fbe30bc53cff2f4c
nm0i <nm0i@me0w.net>
2020-08-04 07:38:47 +0300

    Support for targetting in deathward, formatting.

--- cmds/spells/d/_deathward.c
+++ cmds/spells/d/_deathward.c
@@ -12,10 +12,9 @@ void create()
     set_domains(({ "knowledge", "law" }));
     set_discipline("egoist");
     set_spell_sphere("abjuration");
-    set_syntax("cast CLASS deathward");
+    set_syntax("cast CLASS deathward [on TARGET]");
     set_domains("repose");
-    set_description("Those most blessed of the gods can call upon their favor to ward away the very hand of death itself."
-                    "  This spell protects against death spells, as well as many spell-like effects.");
+    set_description("Those most blessed of the gods can call upon their favor to ward away the very hand of death itself. This spell protects against death spells, as well as many spell-like effects.");
     set_verbal_comp();
     set_somatic_comp();
     set_helpful_spell(1);
@@ -23,8 +22,12 @@ void create()
 
 int preSpell()
 {
-    if (caster->query_property("no death")) {
-        tell_object(caster, "You are already under the influence of a similar effect.");
+    if (!objectp(target)) {
+        target = caster;
+    }
+
+    if (target->query_property("no death")) {
+        tell_object(caster, "They are already under the influence of a similar effect.");
         return 0;
     }
     return 1;
@@ -37,14 +40,14 @@ string query_cast_string()
 
 void spell_effect(int prof)
 {
-    if (!objectp(caster) || !objectp(place)) {
+    if (!objectp(target) || !objectp(place)) {
         dest_effect();
         return;
     }
-    tell_object(caster, "%^BOLD%^%^WHITE%^The blessings of your god fill you with co%^YELLOW%^n%^WHITE%^fid%^YELLOW%^e%^WHITE%^nce!");
-    tell_room(place, "%^BOLD%^%^WHITE%^" + caster->QCN + "'s eyes light with %^YELLOW%^fe%^WHITE%^rv%^YELLOW%^or %^WHITE%^as "
-              + caster->QP + " prayer is answered!", caster);
-    caster->set_property("no death", 1);
+
+    tell_object(target, "%^BOLD%^%^WHITE%^The blessings of your god fill you with co%^YELLOW%^n%^WHITE%^fid%^YELLOW%^e%^WHITE%^nce!");
+    tell_room(place, "%^BOLD%^%^WHITE%^" + target->QCN + "'s eyes light with %^YELLOW%^fe%^WHITE%^rv%^YELLOW%^or %^WHITE%^as " + caster->QP + " prayer is answered!", target);
+    target->set_property("no death", 1);
     spell_successful();
     addSpellToCaster();
     call_out("dest_effect", (ROUND_LENGTH * 12 + roll_dice(1, 20)) * clevel);
@@ -52,9 +55,9 @@ void spell_effect(int prof)
 
 void dest_effect()
 {
-    if (objectp(caster)) {
-        tell_object(caster, "%^BOLD%^%^WHITE%^You feel the protection of your god fade from you.%^RESET%^");
-        caster->set_property("no death", -1);
+    if (objectp(target)) {
+        tell_object(target, "%^BOLD%^%^WHITE%^You feel the protection of your god fade from you.%^RESET%^");
+        target->set_property("no death", -1);
     }
     ::dest_effect();
     if (objectp(TO)) {
