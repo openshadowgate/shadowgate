bbfb6a5c95ae71dd4fd83ed7bf118465f79c6d3e
melchi <melchizedek077@gmail.com>
2020-08-09 09:52:04 -0700

    cosmetic changes

--- d/islands/elf/shore/castle1.c
+++ d/islands/elf/shore/castle1.c
@@ -1,19 +1,17 @@
 #include <std.h>
 #include "../elf.h"
-
-
 inherit WATER;
 
 void create(){
    ::create();
-    set_short("%^BOLD%^BLUE%^Castle Ruins%^RESET%^");
-    set_long(query_short()+"\nThis is the underwater ruins"+
+   set_short("%^BOLD%^BLUE%^Castle Ruins%^RESET%^");
+   set_long(query_short()+"\nThis is the underwater ruins"+
     " inside a %^CYAN%^castle%^RESET%^.  There is a broken %^CYAN%^chair%^RESET%^ behind"+
     " a decaying %^CYAN%^desk%^RESET%^.  The stone walls are falling "+
-    "over the ceiling is barely holding up."+
-    "There looks to be some half burried dead bodies here.");
-    set_listen("default","You hear the swushing of water currents.");
-    set_smell("default","It is hard to smell being underwater.");
+    "over the ceiling is barely holding up.  "+
+    "There looks to be some half burried dead %^CYAN%^bodies%^RESET%^ here.");
+   set_listen("default","You hear the swushing of water currents.");
+   set_smell("default","It is hard to smell being underwater.");
    set_exits(([
       "north" : ROOMS"/shore/castle0",
      
@@ -25,10 +23,10 @@ void create(){
       (({"castle"})) :
         "The castle is just rubble.  There should be hallways and windows but everything has fallen down.",
       (({"desk"})) : "The desk is broken.  It is mostly decayed and laying crooked on the ground.",
+      (({"bodies"})) : "Bits of bodies can be seen partly sticking out of the mud.  Some of the flesh has been picked clean by fish and only bones remain.",
       (({"chair"})) : "This chair is ruined. Three of the four legs are broken it is just leaning against a rock to stand up.",
    ]));
 }
-
 void reset(){
    ::reset();
    if(!present("noble",TO))
