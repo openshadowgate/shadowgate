f254460e1c83a68dff424f34d1cf122b6882e8a0
melchi <melchizedek077@gmail.com>
2020-08-11 23:26:19 -0700

    mislinked (thought I fixed all these)

--- d/islands/elf/shore/ruin14.c
+++ d/islands/elf/shore/ruin14.c
@@ -4,7 +4,7 @@ inherit INH"ruin";
 
 void create(){
    ::create();
-   set_exits(([ "west": ROOMS"shore/ruin2", 
+   set_exits(([ "west": ROOMS"shore/ruin1", 
    "east": ROOMS"shore/ruin17",
 
      ]));
