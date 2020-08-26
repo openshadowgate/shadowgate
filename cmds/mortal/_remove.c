3f1e8762c7cdb6898689e4ffdf717e4a7caab72d
nm0i <nm0i@me0w.net>
2020-08-26 08:30:09 +0300

    | uncrustify

--- cmds/mortal/_remove.c
+++ cmds/mortal/_remove.c
@@ -1,37 +1,39 @@
 #include <std.h>
 inherit DAEMON;
 
-int cmd_remove(string str) 
+int cmd_remove(string str)
 {
-   object ob;
-   if(!str) return notify_fail("Remove what?\n");
+    object ob;
+    if (!str) {
+        return notify_fail("Remove what?\n");
+    }
 
-   if (TP->query_bound()) {
-      TP->send_paralyzed_message("info",TP);
-      return 1;
-   }
-   if(!(ob=present(str, TP))) {
-      notify_fail("You don't have that!\n");
-      return 0;
-   }
-   if(!ob->query_worn()) {
-      message("my_action", "You are not wearing that!", this_player());
-      return 1;
-   }
+    if (TP->query_bound()) {
+        TP->send_paralyzed_message("info", TP);
+        return 1;
+    }
+    if (!(ob = present(str, TP))) {
+        notify_fail("You don't have that!\n");
+        return 0;
+    }
+    if (!ob->query_worn()) {
+        message("my_action", "You are not wearing that!", this_player());
+        return 1;
+    }
 
-   if(ob->query_property("enchantment") && ob->query_property("enchantment") < 0) {
-      write("The curse on this item prevents you from removing it.");
-      return 1;
-   }
-   ob->unwear(); // item bonuses are here - on armour/pocketarmour inherits. N, 7/12.
-   ob->remove_property("wear_order");
-   return 1;
+    if (ob->query_property("enchantment") && ob->query_property("enchantment") < 0) {
+        write("The curse on this item prevents you from removing it.");
+        return 1;
+    }
+    ob->unwear(); // item bonuses are here - on armour/pocketarmour inherits. N, 7/12.
+    ob->remove_property("wear_order");
+    return 1;
 }
 
 void help()
 {
     write(
-"
+        "
 %^CYAN%^NAME%^RESET%^
 
 remove - undress *blush*
@@ -49,4 +51,3 @@ This command will remove ITEM from whatever limb it is currently being worn on.
 wear, wield, unwield, inv, limbs, eq
 ");
 }
-
