aecafb97b0a3f2a4b069d9bdc51948ff5b6c50fd
nm0i <nm0i@me0w.net>
2020-10-07 15:13:00 +0300

    Enervation spell

---
+++ cmds/spells/e/_enervation.c
@@ -0,0 +1,36 @@
+#include <std.h>
+#include <magic.h>
+
+inherit SPELL;
+
+int power;
+
+void create() {
+    ::create();
+    set_spell_name("enervation");
+    set_spell_level(([ "mage" : 4 ]));
+    set_spell_sphere("necromancy");
+    set_syntax("cast CLASS enervation on TARGET");
+    set_damage_desc("1d4 negative levels, clevel additional hp to undead");
+    set_description("You point your finger and fire a black ray of negative energy that suppresses the life force of any living creature it strikes..");
+    set_verbal_comp();
+    set_somatic_comp();
+    set_target_required(1);
+}
+
+void spell_effect(int prof)
+{
+    int round_duration;
+    int power;
+    int i;
+
+    power = roll_dice(1, 4);
+    round_duration = roll_dice(1, 20);
+
+    tell_object(caster,"%^BOLD%^%^BLACK%^A black ray of energy hits " + target->QCN + "%^RESET%^");
+    tell_object(target,"%^BOLD%^BLACK%^A black ray, emanating from " + caster->QCN + "'s finger, hits you.");
+    tell_room(place, "%^BOLD%^%^BLACK%^A black ray, emanating from " + caster->QCN + "'s finger hits " + target->QCN + ".", ({target, caster}));
+
+    "/std/effect/status/negative_level"->apply_effect(target, round_duration, power);
+
+}
