1040d7591b7b6fc67239f3ddf4d8c3b24ad216b0
nm0i <nm0i@me0w.net>
2020-10-07 15:18:47 +0300

    Adjustments to enervation

--- cmds/spells/e/_enervation.c
+++ cmds/spells/e/_enervation.c
@@ -11,7 +11,7 @@ void create() {
     set_spell_level(([ "mage" : 4 ]));
     set_spell_sphere("necromancy");
     set_syntax("cast CLASS enervation on TARGET");
-    set_damage_desc("1d4 negative levels, clevel additional hp to undead");
+    set_damage_desc("1d4 negative levels to living for 1d20 rounds, clevel additional hp to undead");
     set_description("You point your finger and fire a black ray of negative energy that suppresses the life force of any living creature it strikes..");
     set_verbal_comp();
     set_somatic_comp();
@@ -31,6 +31,12 @@ void spell_effect(int prof)
     tell_object(target,"%^BOLD%^BLACK%^A black ray, emanating from " + caster->QCN + "'s finger, hits you.");
     tell_room(place, "%^BOLD%^%^BLACK%^A black ray, emanating from " + caster->QCN + "'s finger hits " + target->QCN + ".", ({target, caster}));
 
-    "/std/effect/status/negative_level"->apply_effect(target, round_duration, power);
+    if (target->is_undead()) {
+        target->add_max_hp_bonus(clevel);
+        tell_object(target,"%^BOLD%^As the ray hits you, you feel empowered.");
+    } else {
+        "/std/effect/status/negative_level"->apply_effect(target, round_duration, power);
+    }
+
 
 }
