bbb48fc9e9c707205f87ba7399e25485a37ff99f
nm0i <nm0i@me0w.net>
2020-10-13 02:44:07 +0300

    Implementing #918

--- cmds/spells/m/_modify_memory.c
+++ cmds/spells/m/_modify_memory.c
@@ -2,12 +2,13 @@
 
 inherit SPELL;
 
-create() {
+create()
+{
     ::create();
     set_spell_name("modify memory");
-    set_spell_level(([ "assassin" : 4, "bard": 4, "psion" : 4, "oracle":4]));
+    set_spell_level(([ "assassin" : 4, "bard" : 4, "psion" : 4, "oracle" : 4]));
     set_mystery("lore");
-    set_domains(({"mentalism"}));
+    set_domains(({ "mentalism" }));
     set_spell_sphere("enchantment_charm");
     set_syntax("cast CLASS modify memory on TARGET");
     set_description("This spell alters victim's memory so they won't remember who you are. Should their will falter they will forget your name and will be momentarily dazed.");
@@ -21,37 +22,39 @@ create() {
 
 string query_cast_string()
 {
-    tell_room("%^RESET%^%^BLUE%^"+caster->QCN+"%^RESET%^%^BLUE%^ looks at "+target->QCN+"%^RESET%^%^BLUE%^ intently.");
+    tell_room("%^RESET%^%^BLUE%^" + caster->QCN + "%^RESET%^%^BLUE%^ looks at " + target->QCN + "%^RESET%^%^BLUE%^ intently.");
     return "display";
 }
 
 void spell_effect(int prof)
 {
-    if(!present(target,place)){
-        tell_object(caster,"Your target has left.");
+    if (!present(target, place)) {
+        tell_object(caster, "Your target has left.");
         dest_effect();
         return;
     }
 
-    if(!userp(target)){
-        tell_object(caster,"Your target is weird.");
+    if (!userp(target)) {
+        tell_object(caster, "Your target is weird.");
         dest_effect();
         return;
     }
 
-    if(!(do_save(target,-4)&&mind_immunity_damage(target))){
+    if (!(do_save(target, -4) && mind_immunity_damage(target))) {
         spell_successful();
-        tell_object(caster,"%^BLUE%^You sense your memory attempt succeeded, and your victim is momentarily dazed.%^RESET%^");
-        target->set_paralyzed(roll_dice(1,3)*8,"%^RESET%^%^BLUE%^You feel oblivious and momentarily distracted.%^RESET%^");
+        tell_object(caster, "%^BLUE%^You sense your memory attempt succeeded, and your victim is momentarily dazed.%^RESET%^");
+        target->set_paralyzed(roll_dice(1, 3) * 8, "%^RESET%^%^BLUE%^You feel oblivious and momentarily distracted... As if you have forgotten something.%^RESET%^");
         target->remove_relationship(caster->query_true_name());
+    }else {
+        tell_object(caster, "%^BLUE%^You sense your memory altering attempt failed.%^RESET%^");
     }
-    else
-        tell_object(caster,"%^BLUE%^You sense your memory altering attempt failed.%^RESET%^");
     dest_effect();
 }
 
 void dest_effect()
 {
     ::dest_effect();
-    if(objectp(TO)) TO->remove();
+    if (objectp(TO)) {
+        TO->remove();
+    }
 }
