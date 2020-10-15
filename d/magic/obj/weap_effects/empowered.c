3185e7be63bd8bc757cea7cb180b25dee8c8167f
shadowgate <law@shadowgate.org>
2020-10-15 22:45:03 +0000

    autocommit 202010154522

---
+++ d/magic/obj/weap_effects/empowered.c
@@ -0,0 +1,76 @@
+/*
+  empowered.c
+  
+  Enhances weapon with mental energy.
+  
+  -- Tlaloc --
+*/
+
+#include <std.h>
+#include <daemons.h>
+
+inherit DAEMON;
+
+int empower_func(object ob)
+{
+    object room, player, spell, enemy;
+    int amount;
+    string pname, ename;
+    mapping info;
+    
+    if(!objectp(ob))
+        return 0;
+    
+    player = environment(ob);
+    
+    if(!userp(player))
+        return 0;
+    
+    room = environment(player);
+    
+    if(!objectp(room))
+        return 0;
+    
+    info = ob->query_property("temp_hit_bonus");
+    spell = info["spell"];
+    
+    if(!objectp(spell))
+        remove_prop(ob);
+    
+    enemy = player->query_current_attacker();
+    
+    if(!objectp(enemy))
+        return 0;
+    
+    pname = capitalize(player->query_name());
+    ename = capitalize(enemy->query_name());
+    //SRD is WC / 2. This scales with level
+    amount = ob->query_wc() * (1 + player->query_guild_level("psywarrior") / 10);
+    
+    if(!random(10))
+    {
+        tell_object(player, "%^GREEN%^Your weapon seems to draw upon " + ename + "'s life force!%^RESET%^");
+        tell_object(enemy, "%^GREEN%^" + pname + "'s weapon seems to draw upon your life force!");
+        tell_room(room, "%^GREEN%^" + pname + "'s weapon seems to draw upon " + ename + "'s life force!", ({ player, enemy }));
+        player->add_hp(amount);
+        enemy->cause_typed_damage(enemy, enemy->return_target_limb(), amount, "negative energy");
+    }
+}
+
+void remove_prop(object ob)
+{
+    object player;
+    
+    ob->remove_property_value("added short", ({ "%^CYAN%^BOLD%^ {empowered}%^RESET%^" }) );
+    ob->remove_property("temp_hit_bonus");
+    
+    player = environment(this_object());
+    player && tell_object(player, "%^%^The vampiric energies fade from your weapon.%^RESET%^");
+}
+    
+    
+    
+    
+    
+    
+    
