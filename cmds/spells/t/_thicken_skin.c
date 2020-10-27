56f919fe1b60ace508c1e67115a9caa96844f26e
nm0i <nm0i@me0w.net>
2020-10-28 01:52:01 +0300

    Implementing #960

--- cmds/spells/t/_thicken_skin.c
+++ cmds/spells/t/_thicken_skin.c
@@ -11,9 +11,7 @@ void create() {
     set_discipline("egoist");
     set_spell_sphere("conjuration_summoning");
     set_syntax("cast CLASS thicken skin");
-    set_description("An egoist who chooses can focus his power to thicken his skin, granting him extra protection in "
-"combat.  This power works much like inertial armor, but unlike other armor abilities, it can be used with inertial armor "
-"to grant an additional bonus.");
+    set_description("An egoist who chooses can focus his power to thicken his skin, granting him extra protection in combat. This power works much like inertial armor, but unlike other armor abilities, it can be used with inertial armor to grant an additional bonus. This spell will not work while you're wearing armour.");
     set_verbal_comp();
     set_somatic_comp();
     set_property("keywords", ({ "defensive", "targeted", "personal" }));
