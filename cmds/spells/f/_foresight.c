c58199bc9335fc6476d6932e5097f10d7691e66e
shadowgate <law@shadowgate.org>
2020-11-12 01:45:02 +0000

    autocommit 202011124501

--- cmds/spells/f/_foresight.c
+++ cmds/spells/f/_foresight.c
@@ -11,7 +11,7 @@ int duration;
 void create() {
     ::create();
     set_spell_name("foresight");
-    set_spell_level(([ "mage" : 7, "bard" : 4,"oracle":7, "druid":9, "psion" : 9 ]));
+    set_spell_level(([ "mage" : 7, "bard" : 4,"oracle":7, "druid":9, "psion" : 9, "magus" : 6 ]));
     set_spell_sphere("divination");
     set_discipline("seer");
     set_mystery("battle");
