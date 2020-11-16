b00c9337cc5d0d7711c93e0d9187ea503076f7af
nm0i <nm0i@me0w.net>
2020-11-16 18:34:33 +0300

    Eldritch weapons duration fix.

--- cmds/spells/e/_eldritch_bow.c
+++ cmds/spells/e/_eldritch_bow.c
@@ -102,7 +102,7 @@ void spell_effect(int prof){
     caster->force_me("wield eldritch bow");
 
     spell_successful();
-    spell_duration = (clevel + roll_dice(1, 20)) * ROUND_LENGTH;
+    spell_duration = (clevel + roll_dice(1, 20)) * ROUND_LENGTH * 10;
     set_end_time();
     call_out("dest_effect",spell_duration);
     return;
