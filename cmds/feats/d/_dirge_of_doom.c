611decf929ada43747ca2bbf13b46f0f099a48a9
nm0i <nm0i@me0w.net>
2020-12-13 22:55:14 +0300

    Dirge of doom prototype

---
+++ cmds/feats/d/_dirge_of_doom.c
@@ -0,0 +1,136 @@
+#include <std.h>
+#include <daemons.h>
+#include <magic.h>
+inherit FEAT;
+
+#define FEATTIMER 15
+
+void create()
+{
+    ::create();
+    feat_type("instant");
+    feat_category("Performance");
+    feat_name("psionicweapon");
+    feat_prereq("Bard L8");
+    feat_classes("bard");
+    feat_desc(".");
+    feat_syntax("dirge_of_doom");
+
+    set_save("will");
+}
+
+int allow_shifted()
+{
+    return 0;
+}
+
+int prerequisites(object ob)
+{
+    if (!objectp(ob)) {
+        return 0;
+    }
+    if (ob->query_class_level("bard") < 8) {
+        dest_effect();
+        return 0;
+    }
+    return ::prerequisites(ob);
+}
+
+int cmd_psionicweapon(string str)
+{
+    object feat;
+    if (!objectp(TP)) {
+        return 0;
+    }
+    feat = new(base_name(TO));
+    feat->setup_feat(TP, "");
+    return 1;
+}
+
+void execute_feat()
+{
+    mapping tempmap;
+    int delay;
+    ::execute_feat();
+
+    if ((int)caster->query_property("using dirge of doom") > time()) {
+        tell_object(caster, "You are not prepared to sing the dirge of doom so soon!");
+        dest_effect();
+        return;
+    }
+    if ((int)caster->query_property("using instant feat")) {
+        tell_object(caster, "You are already in the middle of using a feat!");
+        dest_effect();
+        return;
+    }
+    if (!sizeof(caster->query_attackers())) {
+        tell_object(caster, "You're not under attack!");
+        dest_effect();
+        return;
+    }
+
+    delay = time() + FEATTIMER;
+    delay_messid_msg(FEATTIMER, "%^BOLD%^%^WHITE%^You can ding %^CYAN%^dirge of doom%^WHITE%^ again.%^RESET%^");
+    caster->set_property("using instant feat", 1);
+    caster->remove_property("using dirge of doom");
+    caster->set_property("using dirge of doom", delay);
+    return;
+}
+
+void execute_attack()
+{
+    int die, i;
+    object* targets;
+
+    if (!objectp(caster)) {
+        dest_effect();
+        return;
+    }
+
+    caster->remove_property("using instant feat");
+    ::execute_attack();
+
+    if (caster->query_unconscious()) {
+        dest_effect();
+        return;
+    }
+
+    targets = caster->query_attackers();
+
+    if (!sizeof(targets)) {
+        tell_object(caster, "You are not under attack!");
+        dest_effect();
+        return;
+    }
+
+    targets += ({ caster });
+
+    targets -= ({ caster });
+
+    targets = shuffle(targets);
+
+    for (i = 0; i < sizeof(targets) && i < 8; i++) {
+        if (targets[i] == caster) {
+            continue;
+        }
+
+        if (!objectp(targets[i])) {
+            continue;
+        }
+
+        "/std/effect/status/shaken"->apply_effect(targets[i],roll_dice(1, 4));
+
+        caster->add_attacker(targets[i]);
+        targets[i]->add_attacker(caster);
+    }
+
+    dest_effect();
+    return;
+}
+
+void dest_effect()
+{
+    ::dest_effect();
+    remove_feat(TO);
+    return;
+}
