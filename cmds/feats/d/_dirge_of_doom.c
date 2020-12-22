b417d28fcaa81c95903e9cc758128d6d18de0bb7
nm0i <nm0i@me0w.net>
2020-12-22 23:15:06 +0300

    Helpfile corrections

--- cmds/feats/d/_dirge_of_doom.c
+++ cmds/feats/d/_dirge_of_doom.c
@@ -13,7 +13,9 @@ void create()
     feat_name("dirge of doom");
     feat_prereq("Bard L8");
     feat_classes("bard");
-    feat_desc(".");
+    feat_desc("Your play a melody that inspires a sense of doom in your opponens. If they fail a save, they'll become momentarily shaken with fear.
+
+%^BOLD%^%^WHITE%^See also:%^RESET%^ status effects..");
     feat_syntax("dirge_of_doom");
 
     set_save("will");
