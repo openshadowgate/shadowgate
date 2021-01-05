59b77bd8752eb4d43b7f7eea99994d9bb6f0ab27
nm0i <nm0i@me0w.net>
2021-01-05 18:47:29 +0300

    Ryorik reference in Nim's temple, color adjustments

--- d/magic/temples/nimnavanon/nimnavanon.c
+++ d/magic/temples/nimnavanon/nimnavanon.c
@@ -19,7 +19,7 @@ void create()
 
     switch (season) {
     case "spring":
-        col = "%^CYAN%^";
+        col = "%^GREEN%^";
         break;
     case "winter":
         col = "%^WHITE%^";
@@ -32,7 +32,7 @@ void create()
         break;
     }
 
-    set_long("%^BOLD%^" + col + "The Temple of Nim'navanon%^RESET%^\n%^RESET%^" + col + "You have entered a large clearing deep in the heart of the forest, where a single ancient %^RESET%^%^ORANGE%^oak tree %^RESET%^" + col + "rises to incredible heights at its heart, its gnarled limbs clawing eternally skyward. %^BOLD%^%^CYAN%^L%^RESET%^%^CYAN%^ig%^BOLD%^%^CYAN%^ht %^RESET%^" + col + "dapples the clearing, cast between its leaves as they wave overhead in the breeze. At the base of the tree stands a rough-hewn altar, a single piece of jade with %^RESET%^%^ORANGE%^du%^RED%^l%^ORANGE%^l %^RESET%^" + col + "and rugged sides. Its surface has been polished smooth, displaying the letters of Nim'navanon's charge in g%^YELLOW%^l%^RESET%^" + col + "ow%^YELLOW%^i%^RESET%^" + col + "ng amber against the %^RESET%^" + col + "%^BOLD%^rich mossy hue %^RESET%^" + col + "of the stone. Around the oak, much of the rest of this area looks to be new growth, with %^RESET%^" + col + "%^BOLD%^young grass %^RESET%^" + col + "underfoot that is relatively clear of brush and leaves. A circle of new %^BOLD%^" + col + "sa%^YELLOW%^p%^BOLD%^" + col + "lings %^RESET%^" + col + "questing upwards from the soil at the clearing's edges, beneath the eaves of the older trees that lie beyond. Occasionally, a hint of company is suggested by the %^RESET%^%^MAGENTA%^rustling %^RESET%^" + col + "of undergrowth within their depths, a t%^BOLD%^%^CYAN%^r%^RESET%^" + col + "ill of birdsong, or a distant %^RESET%^%^BLUE%^howl %^RESET%^" + col + "carried upon the wind. \n%^RESET%^");
+    set_long("%^BOLD%^" + col + "The Temple of Nim'navanon%^RESET%^\n%^RESET%^" + col + "You have entered a large clearing deep in the heart of the forest, where a single ancient %^RESET%^%^ORANGE%^oak tree %^RESET%^" + col + "rises to incredible heights at its heart, its gnarled limbs clawing eternally skyward. %^BOLD%^%^CYAN%^L%^RESET%^%^CYAN%^ig%^BOLD%^%^CYAN%^ht %^RESET%^" + col + "dapples the clearing, cast between its leaves as they wave overhead in the breeze. At the base of the tree stands a rough-hewn altar, a single piece of jade with %^RESET%^%^ORANGE%^du%^RED%^l%^ORANGE%^l %^RESET%^" + col + "and rugged sides. Its surface has been polished smooth, displaying the letters of Nim'navanon's charge in g%^YELLOW%^l%^RESET%^" + col + "ow%^YELLOW%^i%^RESET%^" + col + "ng amber against the %^RESET%^%^GREEN%^%^BOLD%^rich mossy hue %^RESET%^" + col + "of the stone. Around the oak, much of the rest of this area looks to be new growth, with %^RESET%^%^GREEN%^%^BOLD%^young grass %^RESET%^" + col + "underfoot that is relatively clear of brush and leaves. A circle of new %^BOLD%^%^GREEN%^sa%^YELLOW%^p%^BOLD%^%^GREEN%^lings %^RESET%^" + col + "questing upwards from the soil at the clearing's edges, beneath the eaves of the older trees that lie beyond. Occasionally, a hint of company is suggested by the %^RESET%^%^MAGENTA%^rustling %^RESET%^" + col + "of undergrowth within their depths, a t%^BOLD%^%^CYAN%^r%^RESET%^" + col + "ill of birdsong, or a distant %^RESET%^%^BLUE%^howl %^RESET%^" + col + "carried upon the wind. \n%^RESET%^");
 
 
     set_smell("default", "The air is filled with the smell of the forest that surrounds you.");
@@ -53,19 +53,12 @@ int read_charge(string str)
     if (!str || str != "charge") {
         return __Read_me(str);
     }
-    write (
-        @CHARGE
-        % ^ BOLD % ^ Nim 'navanon charges his followers with this message:%^RESET%^%^CYAN%^
+    write(
+        "%^BOLD%^Nim'navanon charges his followers with this message:%^RESET%^%^CYAN%^
 Strive to live in harmony with the wilds, for we are each part of the eternal cycle.  Seek to perceive the greater balance in all things, and do not fear the forces of nature, for where destruction, decay and death clear a passage, life springs forth anew.  Intervene where you must, whether it be to cull or to plant anew.  Work actively against those who would disturb the natural order, and teach the ways of the wilderness to any who will listen.  Though civilisations may rise and fall, in the end nature will always reclaim its own.
 
-%^RESET%^---
-
 %^BOLD%^Nim'navanon has developed the following relationships:
-        % ^ RESET % ^ % ^ CYAN % ^ Allies: % ^ RESET % ^ Ryorik
-        % ^ CYAN % ^ Enemies: % ^ RESET % ^ Nilith
-
-        % ^ CYAN % ^ *NOTE:% ^ WHITE % ^ Being an ally or enemy does not necessarily mean you must aid or kill on sight.Determine the best course of action for your character based on your character 's beliefs, the other individual(s) in question, and past actions/interactions with the individual(s) and/or their faith.
-CHARGE
-   );
-   return 1;
+%^RESET%^%^CYAN%^Allies: %^RESET%^Seija
+%^CYAN%^Enemies: %^RESET%^Nilith, Khyron");
+    return 1;
 }
