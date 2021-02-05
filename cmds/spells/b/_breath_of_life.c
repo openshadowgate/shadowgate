bd2e0a13e29cef89c378cac5c14c40390522e1cf
tlaloc <tlaloc@noreply.%(DOMAIN)s>
2021-02-05 03:11:09 +0000

    Update 'cmds/spells/b/_breath_of_life.c'

--- cmds/spells/b/_breath_of_life.c
+++ cmds/spells/b/_breath_of_life.c
@@ -8,6 +8,7 @@ create() {
     set_spell_name("breath of life");
     set_spell_level(([ "cleric" : 5,"oracle":5, "psion" : 5 ]));
     set_mystery("life");
+    set_domain("renewal");
     set_affixed_spell_level(5);
     set_spell_sphere("conjuration_summoning");
     set_discipline("egoist");
