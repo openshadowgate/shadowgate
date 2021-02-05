9ff9e438a89671a7118bf67e1bf7d61735f6cac2
tlaloc <tlaloc@noreply.%(DOMAIN)s>
2021-02-05 18:21:55 +0000

    Update 'daemon/saving_throw_d.c'

--- daemon/saving_throw_d.c
+++ daemon/saving_throw_d.c
@@ -126,7 +126,7 @@ varargs void do_save(object ob, int dc, string type, raw_save)
             num += 2;
         }
         if(member_array("madness", ob->query_divine_domain()) >= 0)
-            num -= 2;
+            num -= roll_dice(1, 4);
 
         save_info["misc_modifiers"] = mod;
         {
