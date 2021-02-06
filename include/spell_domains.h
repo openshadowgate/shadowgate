b8f662c808f20cd0991ed6b7fa29c7a9d7243d2c
nm0i <nm0i@me0w.net>
2021-02-06 04:37:03 +0300

    - edea, ashra, draem

--- include/spell_domains.h
+++ include/spell_domains.h
@@ -3,11 +3,8 @@
 // Occult gods are in this file for multiclassing only. People that MC
 // with cleric will have to deal with lack of domains
 #define SPELL_DOMAINS (([\
- "ashra":({"illusion", "void", "darkness", "evil",}), \
  "callamir" : ({ "charm", "air", "trickery", "travel", "good", "chaos"}), \
  "cevahir" : ({ "mentalism", "renewal", "strength", "war", "air", }) , \
- "dream" : ({ "madness", "void", "stars", "chaos", }), \
- "edea":({"renewal", "void", "protection", "good",}), \
  "jarmila" : ({ "renewal", "sun", "fire", "martyr", "good", }) , \
  "khyron" : ({ "fire", "storms", "destruction", "suffering", "chaos", }) , \
  "kismet" : ({ "creation", "knowledge", "magic", "stars", "fate"}) , \
