9c5e4b41abe938b4c84f129d4323ff28ea0fac52
shadowgate <law@shadowgate.org>
2021-02-07 23:00:01 +0000

    autocommit 202102070023

--- cmds/spells/r/_ray_of_enfeeblement.c
+++ cmds/spells/r/_ray_of_enfeeblement.c
@@ -47,7 +47,7 @@ int preSpell()
     {
         if(!(int)USER_D->spend_pool(this_player(), 1, "grace"))
         {
-            tell_object(caster, "You don't have the Divine Grace to cast Touch of Chaos!");
+            tell_object(caster, "You don't have the Divine Grace to cast Ray of Enfeeblement!");
             return 0;
         }
     }
