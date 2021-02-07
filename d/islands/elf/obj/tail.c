7a1a873c8ffce96969e7b91fbb13a8218c5a4285
melchi <melchizedek077@gmail.com>
2021-02-06 19:45:03 -0800

    figured I'd make this a little nicer.

--- d/islands/elf/obj/tail.c
+++ d/islands/elf/obj/tail.c
@@ -24,4 +24,24 @@ void create(){
    set_item_bonus("thievery",2);
    set_item_bonus("wisdom",-1);
    set_item_bonus("slashing resistance",5);
+   set_struck((:TO,"strikeme":));
+}
+int strikeme(int damage, object what, object who){
+    if(!random(4)){
+  	tell_object(ETO,"%^BOLD%^%^BLUE%^Your cat tail lunghes"+
+		" "+who->QCN+", rying to trip them!");
+   	tell_object(who,"%^BOLD%^%^BLUE%^"+ETO->QCN+"'s tail "+
+		"lunges at you and tries to trip you");
+    tell_room(environment(ETO),"%^BOLD%^"+ETO->QCN+"'s tail tries to trip "+who->QCN+" with"+
+		" a quick motion!",({who,ETO}));
+    if(!"/daemon/saving_throw_d.c"->reflex_save(targ,-30))
+     	who->set_tripped(1,"%^BOLD%^You feel over from a tail trip!");
+  }
+  return damage;
+}
+int wearme(){
+  	write("%^BOLD%^%^BLUE%^You fasten the tail belt around you and go 'Nyah'.");
+   	say("%^BOLD%^%^BLUE%^The tail "+TPQCN+"'s belt flicks back and forth"+
+		" like it is alive.");
+  return 1;
 }
