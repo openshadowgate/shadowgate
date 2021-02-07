ef9806b339d754497e27e6503ed5626f94b7ce36
tlaloc <tlaloc@noreply.%(DOMAIN)s>
2021-02-07 14:50:28 +0000

    Update 'cmds/spells/d/_dispelling_touch.c'

--- cmds/spells/d/_dispelling_touch.c
+++ cmds/spells/d/_dispelling_touch.c
@@ -45,7 +45,7 @@ int preSpell()
     
     if(!(int)USER_D->spend_pool(this_player(), 1, "grace"))
     {
-        tell_object(caster, "You don't have the Divine Grace to cast Fire Bolt!");
+        tell_object(caster, "You don't have the Divine Grace to cast Dispelling Touch!");
         return 0;
     }
 
