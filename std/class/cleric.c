88ecd14fa353628acabcbfc44f11c27d7ef84459
tlaloc <tlaloc@noreply.%(DOMAIN)s>
2021-02-08 19:51:31 +0000

    Update 'std/class/cleric.c'

--- std/class/cleric.c
+++ std/class/cleric.c
@@ -169,6 +169,8 @@ mapping query_innate_spells(object player)
         innate_spells += ([ "touch of chaos" : ([ "type" : "spell", "daily uses" : -1, "level required" : 0 ]), ]);
     if(member_array("suffering", player->query_divine_domain()) >= 0)
         innate_spells += ([ "ray of enfeeblement" : ([ "type" : "spell", "daily uses" : -1, "level required" : 0 ]), ]);
+    if(member_array("law", player->query_divine_domain()) >= 0)
+        innate_spells += ([ "touch of law" : ([ "type" : "spell", "daily uses" : -1, "level required" : 0 ]), ]);
 
     return innate_spells;
 }
