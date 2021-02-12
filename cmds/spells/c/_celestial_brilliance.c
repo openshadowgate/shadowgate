d706b52077d72799eba4b3ad41a50b61b5ec3702
chernobog <chernobog@noreply.%(DOMAIN)s>
2021-02-12 00:32:11 +0000

    updated spell messages

--- cmds/spells/c/_celestial_brilliance.c
+++ cmds/spells/c/_celestial_brilliance.c
@@ -22,18 +22,18 @@ void create()
     set_somatic_comp();
     set_save("reflex");
     aoe_spell(1);
-    set_aoe_message("%^RESET%^%^%^BLUE%^(%^BOLD%^%^WHITE%^engulfed in white swirling lights%^RESET%^%^BLUE%^)%^RESET%^");
+    set_aoe_message("%^BOLD%^%^CYAN%^(%^RESET%^%^CYAN%^c%^BOLD%^a%^GREEN%^s%^CYAN%^c%^RESET%^%^CYAN%^a%^BOLD%^d%^GREEN%^i%^CYAN%^n%^RESET%^%^CYAN%^g w%^BOLD%^a%^GREEN%^v%^CYAN%^e%^RESET%^%^CYAN%^s %^BOLD%^o%^RESET%^%^CYAN%^f %^BOLD%^%^WHITE%^l%^ORANGE%^i%^WHITE%^ght %^CYAN%^f%^GREEN%^i%^CYAN%^l%^RESET%^%^CYAN%^l t%^BOLD%^h%^RESET%^%^CYAN%^e %^BOLD%^a%^GREEN%^r%^CYAN%^e%^RESET%^%^CYAN%^a%^BOLD%^)%^RESET%^");
 }
 
 string query_cast_string()
 {
-    tell_room(place,"%^RESET%^%^WHITE%^As "+caster->QCN+" voices a few syllables, the area begins to swirl with celestial energy.");
+    tell_room(place,"%^RESET%^%^CYAN%^"+caster->QCN+" %^RESET%^%^CYAN%^bows their head with a m%^BOLD%^%^MAGENTA%^u%^CYAN%^r%^RESET%^%^CYAN%^m%^BOLD%^u%^RESET%^%^CYAN%^r%^BOLD%^%^MAGENTA%^r%^RESET%^%^CYAN%^e%^BOLD%^d %^RESET%^%^CYAN%^e%^BOLD%^%^MAGENTA%^n%^CYAN%^t%^RESET%^%^CYAN%^r%^BOLD%^e%^RESET%^%^CYAN%^a%^BOLD%^%^MAGENTA%^t%^CYAN%^y%^RESET%^%^CYAN%^, holding their hands out in %^BOLD%^%^WHITE%^su%^RESET%^p%^BOLD%^p%^RESET%^l%^BOLD%^ica%^RESET%^t%^BOLD%^io%^RESET%^n%^CYAN%^.%^RESET%^");
     return "display";
 }
 
 void spell_effect(int prof)
 {
-    tell_room(place,"%^BOLD%^%^WHITE%^A conduit to the celestial realms is opened and swirling celestial energy engulfs the area!");
+    tell_room(place,"%^RESET%^%^CYAN%^A loud %^BOLD%^t%^GREEN%^r%^CYAN%^um%^GREEN%^p%^CYAN%^et bl%^GREEN%^a%^CYAN%^st %^RESET%^%^CYAN%^sounds out as the area brightens with %^BOLD%^%^WHITE%^v%^RESET%^i%^YELLOW%^b%^WHITE%^ra%^RESET%^n%^BOLD%^t l%^RESET%^u%^BOLD%^m%^ORANGE%^i%^WHITE%^n%^RESET%^e%^BOLD%^s%^ORANGE%^c%^WHITE%^e%^RESET%^n%^BOLD%^ce%^RESET%^%^CYAN%^, %^YELLOW%^p%^WHITE%^u%^ORANGE%^re %^RESET%^%^CYAN%^and %^YELLOW%^wh%^WHITE%^o%^ORANGE%^les%^WHITE%^o%^ORANGE%^me%^RESET%^%^CYAN%^!%^RESET%^");
     prevlight=place->query_light();
     if(prevlight<1)
         place->set_light(1);
@@ -61,7 +61,7 @@ void execute_attack()
         return;
     }
 
-    message("info", "%^BOLD%^%^WHITE%^Swirling incandescent energy engulfs everyone in the room, sparing friends and smiting foes!%^RESET%^", place);
+    message("info","%^RESET%^%^CYAN%^T%^BOLD%^o%^GREEN%^r%^CYAN%^r%^RESET%^%^CYAN%^e%^BOLD%^n%^GREEN%^t%^CYAN%^s %^RESET%^%^CYAN%^of %^WHITE%^g%^BOLD%^l%^CYAN%^i%^RESET%^m%^BOLD%^m%^CYAN%^e%^RESET%^r%^BOLD%^i%^CYAN%^n%^RESET%^g %^BOLD%^p%^ORANGE%^u%^WHITE%^ri%^RESET%^t%^BOLD%^y %^RESET%^%^CYAN%^pulse in waves around the area, and a %^BOLD%^%^WHITE%^c%^RESET%^e%^BOLD%^le%^RESET%^s%^BOLD%^ti%^RESET%^a%^BOLD%^l f%^RESET%^a%^BOLD%^nf%^RESET%^a%^BOLD%^re %^RESET%^%^CYAN%^resounds with a deafening roar.%^RESET%^",place);
     foes = target_selector();
     foes -= ({ caster });
 
@@ -78,7 +78,7 @@ void execute_attack()
         if(foes[i]->is_undead())
         {
             sdamage = (sdamage * 5) / 4;
-            message("info", "%^BOLD%^YELLOW%^" + foes[i]->QCN + "'s very essence struggles as it is consumed!", place);
+            message("info","%^RESET%^%^CYAN%^The %^BOLD%^%^WHITE%^l%^ORANGE%^i%^WHITE%^g%^RESET%^h%^BOLD%^t %^RESET%^%^CYAN%^unmakes %^WHITE%^"+foes[i]->QCN+"%^RESET%^%^CYAN%^, %^BOLD%^%^RED%^tearing %^RESET%^%^CYAN%^apart its %^BOLD%^%^MAGENTA%^p%^RED%^e%^MAGENTA%^rv%^RED%^e%^MAGENTA%^r%^RED%^s%^MAGENTA%^e %^BLACK%^ex%^RESET%^i%^BOLD%^%^BLACK%^st%^RESET%^e%^BOLD%^%^BLACK%^nc%^RESET%^e%^CYAN%^!%^RESET%^",place);
         }
         damage_targ(targ,targ->return_target_limb(),sdamage,"divine");
     }
