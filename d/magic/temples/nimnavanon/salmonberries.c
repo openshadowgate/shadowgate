71fd049eaabb37f1a49b9a900be544cefa361769
shadowgate <law@shadowgate.org>
2021-03-06 20:42:59 +0000

    autocommit 202103064220

--- d/magic/temples/nimnavanon/salmonberries.c
+++ d/magic/temples/nimnavanon/salmonberries.c
@@ -8,11 +8,11 @@ void create()
    set_short("%^GREEN%^A handful of %^RED%^Sa%^BOLD%^lm%^RESET%^%^RED%^o%^BOLD%^n%^RESET%^%^RED%^berr%^BOLD%^ies%^RESET%^");
    set_id(({"salmonberries","berries"}));
    set_long("The %^RED%^Sa%^BOLD%^lm%^RESET%^%^RED%^o%^BOLD%^n%^RESET%^%^RED%^berr%^BOLD%^ies%^RESET%^ are red with a hint of orange and look juicy and ripe.");
-   //set_smell("The %^RED%^Sa%^BOLD%^lm%^RESET%^%^RED%^o%^BOLD%^n%^RESET%^%^RED%^berr%^BOLD%^ies%^RESET%^ have a sweet fragrance.");
+ //  set_smell("The %^RED%^Sa%^BOLD%^lm%^RESET%^%^RED%^o%^BOLD%^n%^RESET%^%^RED%^berr%^BOLD%^ies%^RESET%^ have a sweet fragrance.");
    set_weight(1);
    set_value(0);
    set_destroy();
    set_strength(5);
-   set_my_mess("You eat a handful of %^RED%^Sa%^BOLD%^lm%^RESET%^%^RED%^o%^BOLD%^n%^RESET%^%^RED%^berr%^BOLD%^ies%^RESET%^. They are juicy, floral-sweet and sour in a fresh way.");
+   set_my_mess("You eat a handful of %^RED%^Sa%^BOLD%^lm%^RESET%^%^RED%^o%^BOLD%^n%^RESET%^%^RED%^berr%^BOLD%^ies%^RESET%^. They are juicy and fresh, tasting both %^MAGENTA%^f%^BOLD%^l%^RESET%^%^MAGENTA%^o%^BOLD%^r%^RESET%^%^MAGENTA%^a%^BOLD%^l-%^RESET%^%^MAGENTA%^s%^BOLD%^w%^RESET%^%^MAGENTA%^e%^BOLD%^et %^WHITE%^and %^ORANGE%^sour%^RESET%^.");
    set_your_mess("eats a handful of %^RED%^Sa%^BOLD%^lm%^RESET%^%^RED%^o%^BOLD%^n%^RESET%^%^RED%^berr%^BOLD%^ies%^RESET%^.");
 }
