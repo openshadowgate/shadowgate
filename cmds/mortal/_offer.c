ee9242575ed994d8e973965d8ed84306b560daec
nm0i <nm0i@me0w.net>
2021-03-08 23:49:05 +0300

    Implementing #1560

--- cmds/mortal/_offer.c
+++ cmds/mortal/_offer.c
@@ -5,7 +5,8 @@ inherit DAEMON;
 
 int help();
 
-int cmd_offer(string str) {
+int cmd_offer(string str)
+{
     object ob;
     string wht, god;
     if (!str) {
@@ -17,7 +18,7 @@ int cmd_offer(string str) {
     wht = "";
 
     if (TP->query_bound()) {
-        TP->send_paralyzed_message("info",TP);
+        TP->send_paralyzed_message("info", TP);
         return 1;
     }
 
@@ -29,57 +30,67 @@ int cmd_offer(string str) {
             return notify_fail("You can't offer that to a non-existant god.\n");
         }
     }
-    if (!(ob = present(str,this_player()))) {
-        write("The "+str+" %^RESET%^must be within your inventory.");
+    if (!(ob = present(str, this_player()))) {
+        write("The " + str + " %^RESET%^must be within your inventory.");
         return 1;
     }
-    if (str=="coins") {
+    if (str == "coins") {
         write("%^RED%^The gods are rich enough already!");
-        tell_room(ETP,TPQCN+" is trying desperately to throw their money away...  mabye you should ask for some..",TP);
+        tell_room(ETP, TPQCN + " is trying desperately to throw their money away...  mabye you should ask for some..", TP);
         return 1;
     }
-    if (ob->query_worn())
+    if (ob->query_property("nosell")) {
+        return notify_fail("You can't offer kept objects.");
+    }
+
+    if (ob->query_worn()) {
         return notify_fail("Please remove that before offering it\n");
-    if (ob->query_wielded())
+    }
+    if (ob->query_wielded()) {
         return notify_fail("Please unwield that first.\n");
-    if (ob->query_property("plot_item") || ob->query_property("soulbound"))
+    }
+    if (ob->query_property("plot_item") || ob->query_property("soulbound")) {
         return notify_fail("The gods do not want that.\n");
-    if (ob->query_property("no offer"))
+    }
+    if (ob->query_property("no offer")) {
         return notify_fail("The gods will not accept your offering at this time.\n");
+    }
     if ((time() - (int)ob->query_property("died time") < 600) && !TP->query_property("monsteroffer")) {
         return notify_fail("That object has recently been through a player death. You can not offer it yet.\n");
     }
     if (ob && objectp(ob)) {
         if (TP->query_verbose_combat()) {
             if (god != "") {
-                write("You raise "+ob->query_short()+" %^RESET%^high in the air and a bolt of lightning crashes down upon it, sending it to "+god+".");
+                write("You raise " + ob->query_short() + " %^RESET%^high in the air and a bolt of lightning crashes down upon it, sending it to " + god + ".");
 
-                tell_room(ETP,TPQCN+" offers "+ob->query_short()+" to "+god+".", TP);
+                tell_room(ETP, TPQCN + " offers " + ob->query_short() + " to " + god + ".", TP);
             } else {
-                write("You raise "+ob->query_short()+" %^RESET%^high in the air and a bolt of lightning crashes down upon it, destroying it.");
-                tell_room(ETP,TPQCN+" offers "+ob->query_short()+".",TP);
+                write("You raise " + ob->query_short() + " %^RESET%^high in the air and a bolt of lightning crashes down upon it, destroying it.");
+                tell_room(ETP, TPQCN + " offers " + ob->query_short() + ".", TP);
             }
         } else {
             if (god != "") {
-                write("You offer "+ob->query_short()+" to "+god+".");
-                tell_room(ETP,TPQCN+" offers "+ob->query_short()+" to "+god+".", TP);
+                write("You offer " + ob->query_short() + " to " + god + ".");
+                tell_room(ETP, TPQCN + " offers " + ob->query_short() + " to " + god + ".", TP);
             } else {
-                write("You offer "+ob->query_short()+".");
-                tell_room(ETP,TPQCN+" offers "+ob->query_short()+".",TP);
+                write("You offer " + ob->query_short() + ".");
+                tell_room(ETP, TPQCN + " offers " + ob->query_short() + ".", TP);
             }
         }
         if (ob->query_value()) {
             ob->remove();
             TP->add_hp(random(5));
-        } else
+        } else {
             ob->remove();
+        }
         return 1;
     }
 }
 
-int help() {
+int help()
+{
     write(
-"
+        "
 %^CYAN%^NAME%^RESET%^
 
 offer - dispose of something
--- cmds/mortal/_offerall.c
+++ cmds/mortal/_offerall.c
@@ -2,58 +2,66 @@
 
 inherit DAEMON;
 
-cmd_offerall() {
-   object *inv, *living;
-   int i,s,wt;
+cmd_offerall()
+{
+    object* inv, * living;
+    int i, s, wt;
 
-   inv = all_inventory(ETP);
-   living = all_living(ETP);
-   inv = inv - living;
-   s = sizeof(inv);
+    inv = all_inventory(ETP);
+    living = all_living(ETP);
+    inv = inv - living;
+    s = sizeof(inv);
 
-   if (TP->query_bound()) {
-      TP->send_paralyzed_message("info",TP);
-      return 1;
-   }
-   for(i=0;i<s;i++) {
-      wt = TP->query_max_internal_encumbrance();
-      if(inv[i]->query_property("monsterweapon")) {
-         inv[i]->remove();
-         continue;
-      }
-      if (!inv[i]->get()) {
-          continue;
-      }
-      if(inv[i]->query_weight() > wt) {
-         tell_object(TP,"You can't lift the "+inv[i]->query_name()+".");
-         continue;
-      }
-      if(inv[i]->query_property("no offer") || inv[i]->query("not living")) {
-         tell_object(TP, "The gods will not accept that gift.\n");
-         continue;
-      }
-
-      if(inv[i]->query_property("plot_item")) {
-         tell_object(TP, "The gods will not accept that gift.\n");
-         continue;
-      }
-
-    if( (time() - (int)inv[i]->query_property("died time") < 600) && !TP->query_property("monsteroffer")) {
-         tell_object(TP,"That object has recently been through a player death. You can not offer it yet.\n");
-          continue;
+    if (TP->query_bound()) {
+        TP->send_paralyzed_message("info", TP);
+        return 1;
+    }
+    for (i = 0; i < s; i++) {
+        wt = TP->query_max_internal_encumbrance();
+        if (inv[i]->query_property("monsterweapon")) {
+            inv[i]->remove();
+            continue;
         }
-      if(inv[i]->query_value())
-         TP->add_hp(random(2));
-      inv[i]->remove();
-   }
-   tell_object(TP,"You offer everything in the room and bolts of lightning crash down around you.");
-   tell_room(ETP,"Lightning crashes down around you as "+TPQCN+" offers everything in the room.",TP);
-   return 1;
+        if (!inv[i]->get()) {
+            continue;
+        }
+        if (inv[i]->query_weight() > wt) {
+            tell_object(TP, "You can't lift the " + inv[i]->query_name() + ".");
+            continue;
+        }
+        if (inv[i]->query_property("nosell")) {
+            tell_object(TP, "You can't offer" + inv[i]->query_name() + ", as it is kept object.");
+            continue;
+        }
+
+        if (inv[i]->query_property("no offer") || inv[i]->query("not living")) {
+            tell_object(TP, "The gods will not accept that gift.\n");
+            continue;
+        }
+
+        if (inv[i]->query_property("plot_item")) {
+            tell_object(TP, "The gods will not accept that gift.\n");
+            continue;
+        }
+
+        if ((time() - (int)inv[i]->query_property("died time") < 600) && !TP->query_property("monsteroffer")) {
+            tell_object(TP, "That object has recently been through a player death. You can not offer it yet.\n");
+            continue;
+        }
+        if (inv[i]->query_value()) {
+            TP->add_hp(random(2));
+        }
+        inv[i]->remove();
+    }
+    tell_object(TP, "You offer everything in the room and bolts of lightning crash down around you.");
+    tell_room(ETP, "Lightning crashes down around you as " + TPQCN + " offers everything in the room.", TP);
+    return 1;
 }
 
-int help() {
-   write(
-"
+int help()
+{
+    write(
+        "
 %^CYAN%^NAME%^RESET%^
 
 offerall - clean the room
@@ -66,5 +74,5 @@ This command will offer everything that's on the ground in front of you.
 
 offer, pkilling, get, drop
 ");
-   return 1;
+    return 1;
 }
