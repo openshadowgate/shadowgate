5b66963a9c8740850e17647d163c1bc2329504e1
shadowgate <law@shadowgate.org>
2021-04-09 08:45:02 +0000

    .

--- cmds/spells/d/_detect_magic.c
+++ cmds/spells/d/_detect_magic.c
@@ -48,10 +48,10 @@ void spell_effect(int prof) {
 void dest_effect() {
     if (objectp(caster)) {
         caster->set_detecting_magic(0);
+       tell_object(caster,"%^BOLD%^You no longer can detect magic. Your view of the world returns to normal.");             
     }
     ::dest_effect();
-    tell_object(caster,"%^BOLD%^You no longer can detect magic. Your view of the world returns to normal.");    
-    if(objectp(TO)) TO->remove();
+   if(objectp(TO)) TO->remove();
 
 
 }
