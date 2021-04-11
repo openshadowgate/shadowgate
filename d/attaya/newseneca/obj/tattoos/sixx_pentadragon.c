6157585fdad8c2e8ce1eed7c408f08c6aa8c8dcc
shadowgate <law@shadowgate.org>
2021-04-11 02:00:02 +0000

    .

--- d/attaya/newseneca/obj/tattoos/sixx_pentadragon.c
+++ d/attaya/newseneca/obj/tattoos/sixx_pentadragon.c
@@ -12,3 +12,27 @@ void create(){
 	set_value(20000);
 	set_id(({ "tattoo", "tat", "crue", "pentagram", "dragon" }));
 	set_short("a medium pentadragon tattoo");
+	set_long(
+@MYLONG
+This is a tattoo of a pentagram.  The pentagram is done in blood-black ink.  In the center of the pentagram is a RED dragoN?s head.  Over the top of the pentagram circle is a rocker with the word ?CRUE? on it.
+
+MYLONG
+	);
+	set_limbs(({"right arm"}));
+}
+
+int get(){ return 1; }
+
+int drop(){ return 1; }
+
+void unwear(){ return;}
+
+int query_mysize(){ return "medium"; }
+
+int is_tattoo(){ return 1; }
+
+int wear_me(){
+	"daemon/hide_d"->hideItem(TO,ETO,1);
+return 1;
+}
+
