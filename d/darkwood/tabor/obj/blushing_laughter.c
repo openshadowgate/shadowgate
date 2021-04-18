c54b113f02d94561a0c64283a08aa68d5cfc1a55
rheyn <rheyn@noreply.%(DOMAIN)s>
2021-04-18 16:49:43 +0000

    Update 'd/darkwood/tabor/obj/blushing_laughter.c'
    #1679

--- d/darkwood/tabor/obj/blushing_laughter.c
+++ d/darkwood/tabor/obj/blushing_laughter.c
@@ -29,7 +29,7 @@ void init(){
 int wear_per(string str){
 string long;
    if(str == "perfume" || str == "blushing laughter"){
-      if(count > 4){
+      if(count > 24){
          tell_object(TP,"The bottle of perfume is empty.");
          return 1;
       }
