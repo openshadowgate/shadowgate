cc220f8cf1dc8579dc70ca23e0503d7d0b82ca45
rheyn <rheyn@noreply.%(DOMAIN)s>
2021-04-18 16:45:09 +0000

    Update 'd/tharis/obj/ishy_rains.c'
    #1679

--- d/tharis/obj/ishy_rains.c
+++ d/tharis/obj/ishy_rains.c
@@ -26,7 +26,7 @@ void init(){
 
 int wear_per(string str){
    if(str == "perfume" || str == "istishia's refreshing rains"){
-      if(count > 4){
+      if(count > 24){
          tell_object(TP,"The bottle of perfume is empty.");
          return 1;
       }
