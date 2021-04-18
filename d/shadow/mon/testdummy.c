cd145cebb65e3f175696c02d56759839a9bb7221
draem <nm0i@me0w.net>
2021-04-18 17:28:29 +0300

    DPR calcs for test dummy

--- d/shadow/mon/testdummy.c
+++ d/shadow/mon/testdummy.c
@@ -1,9 +1,8 @@
-//adapted from Ares' testdummy for new Seneca
-//used in the mage guild
-//~Circe~ 1/9/08
 #include <std.h>
 
 inherit "/std/monster.c";
+
+int last_hp = 0;
 
 void create()
 {
@@ -18,10 +17,25 @@ void create()
     set_class("fighter");
     set_mlevel("fighter", 1);
     set_hp(10000000);
+    last_hp = query_hp();
 }
 
 int do_typed_damage_effects(victim, limb, damage, damage_type)
 {
     tell_room(ETO, "%^MAGENTA%^Dummy says%^RESET%^: I got hit to " + limb + " for " + damage + " " + damage_type + " damage!");
     return damage;
+}
+
+pre_round()
+{
+    ::pre_round();
+
+    if (last_hp == query_hp()) {
+        return;
+    }
+
+    tell_room(ETO, "\n %^BOLD%^%^RED%^--=< %^ORANGE%^DING! New round%^RED%^ >=--\n");
+
+    tell_room(ETO, "%^MAGENTA%^Dummy says%^RESET%^: Your DPR is " + (last_hp - query_hp()));
+    last_hp = query_hp();
 }
