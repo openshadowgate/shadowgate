3bcb83590ca248aa9cd0aae45f392b407dab1437
draem <nm0i@me0w.net>
2021-04-18 20:00:17 +0300

    Typoes...

--- cmds/mortal/_stab.c
+++ cmds/mortal/_stab.c
@@ -1,9 +1,9 @@
 #include <std.h>
 
-inherit "/cmd/mortal/_kill";
+inherit "/cmds/mortal/_kill";
 
 int cmd_stab(string args)
 {
     tell_object(TP,"%^BOLD%^%^WHITE%^You jump from shadows to deliver a deadly strike!");
-    return cmd_kill(string args);
+    return cmd_kill(args);
 }
